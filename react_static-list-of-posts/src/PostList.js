a4c781688f29603771519a859aeb5eacd43b28f3
Batsenko <48134243+Batsenko@users.noreply.github.com>
2019-06-07 16:14:39 +0300

    Update react_static-list-of-posts/src/PostList.js
    Co-Authored-By: Остап П'ятковський <44294945+gestap@users.noreply.github.com>

--- react_static-list-of-posts/src/PostList.js
+++ react_static-list-of-posts/src/PostList.js
@@ -5,7 +5,7 @@ import Post from "./Post.js";
 function PostList(props) {
   return props.posts.map((value) => {
   let userIndex = props.users.findIndex((user) => user.id === value.userId);
-  let comments = props.comments.filter((comment) => comment.postId === value.id);
+  let comments = props.comments.filter(comment => comment.postId === value.id);
   
   return <Post key={value.id} post={value} user={props.users[userIndex]} comments={comments}/>
   });
