f4c69e158f704c49a1f781375272591afec29364
VitaliyTregubovKh <tregubovvakharkov@gmail.com>
2019-06-11 19:55:33 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PostsListHandler from './PostsListHandler';
+
+function App() {
+  return (
+    <div className="App">
+      <PostsListHandler />
+    </div>
+  );
+}
+
+export default App;
+
