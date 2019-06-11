8b40086ce057f3c67d8cc2b4ffda2cfa96e801b3
VitaliyTregubovKh <tregubovvakharkov@gmail.com>
2019-06-11 19:58:44 +0300

    Create CommentHandler.js

---
+++ CommentHandler.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+function CommentHandler(props) {
+
+  return (
+    <div className="comment-item">
+      <div className="comment-body">{props.commentBody}</div>
+      <div className="comment-author">{props.commentAuthor}</div>
+    </div>
+  );
+}
+
+export default CommentHandler;
+
