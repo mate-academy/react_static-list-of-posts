59334d0050b49ee0a1a2c66a5e2c9967ace37658
VitaliyTregubovKh <tregubovvakharkov@gmail.com>
2019-06-12 01:11:53 +0300

    Update CommentHandler.js

--- CommentHandler.js
+++ CommentHandler.js
@@ -1,10 +1,14 @@
 import React from 'react'; 
 
 function CommentHandler(props) {
+  const {
+    commentBody: commentBody,
+    commentAuthor: commentAuthor
+  } = props;
   return (
     <div className="comment-item">
-      <div className="comment-body">{props.commentBody}</div>
-      <div className="comment-author">{props.commentAuthor}</div>
+      <div className="comment-body">{commentBody}</div>
+      <div className="comment-author">{commentAuthor}</div>
     </div>
   );
 }
