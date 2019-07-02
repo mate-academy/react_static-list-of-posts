d458ef44f2d1f8c48713d0b3a48dd3d6309c8ae5
Darya Vyshniakova <vishnya054@gmail.com>
2019-07-02 18:20:07 +0300

    added sort by title and by body

---
+++ build/precache-manifest.a977e704d8fc695865c26a0ad68f998c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d254784025120f34f99741f688eb805a",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "c0343f60937cf813efb2",
+    "url": "/react_static-list-of-posts/static/css/main.4ec3559b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_static-list-of-posts/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c0343f60937cf813efb2",
+    "url": "/react_static-list-of-posts/static/js/main.edbf2071.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
