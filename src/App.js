8d614a80ced83ca16e010e90c629d86d09b44a72
mpvitalik <mpvitalik@gmail.com>
2019-07-04 00:19:00 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -6,24 +6,49 @@ import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
 
+const preparedPostsList = posts.map(post => ({
+  post,
+  user: users.find(user => user.id === post.userId),
+  comments: comments.filter(comment => comment.postId === post.id),
+}));
+
 const App = () => (
   <div className="App">
     <h1>Static list of posts</h1>
+    <ListOfPosts postsList={preparedPostsList} />
+  </div>
+);
 
-    <p>
-      <span>posts: </span>
-      {posts.length}
-    </p>
+const ListOfPosts = props => (
+  <div>
+    {props.postsList.map(post => <PostItem postItem={post} />)}
+  </div>
+);
 
-    <p>
-      <span>comments: </span>
-      {comments.length}
-    </p>
+const PostItem = props => (
+  <div className="post">
+    <h2>{props.postItem.post.title}</h2>
+    <p>{props.postItem.post.body}</p>
+    <User user={props.postItem.user} />
+    <div>
+      <h3>Comments:</h3>
+      {props.postItem.comments.map(comment => <Comment comment={comment} />)}
+    </div>
+  </div>
+);
 
-    <p>
-      <span>Users: </span>
-      {users.length}
-    </p>
+const User = props => (
+  <div className="user">
+    <span>{props.user.name}</span>
+    <span>{props.user.email}</span>
+  </div>
+);
+
+const Comment = props => (
+  <div className="comment">
+    <p>{props.comment.body}</p>
+    <span>{props.comment.name}</span>
+    <span>{props.comment.email}</span>
   </div>
 );
 
