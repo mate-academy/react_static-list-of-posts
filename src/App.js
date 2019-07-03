9fd7cbc5adf989557660ea0c7964136af57474db
Taras Khalak <tarasulo@gmail.com>
2019-07-03 20:11:08 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,30 +1,87 @@
 import React from 'react';
-
 import './App.css';
-
 import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
 
+const postWithUser = posts.map(post => ({
+  ...post,
+  user: users.find(user => post.userId === user.id),
+  comments: comments.filter(comment => comment.postId === post.id),
+}));
+
+console.log(postWithUser);
 const App = () => (
-  <div className="App">
+  <>
     <h1>Static list of posts</h1>
+    <PostList posts={postWithUser} />
+  </>
+);
 
-    <p>
-      <span>posts: </span>
-      {posts.length}
-    </p>
+const PostList = (props) => (
+  <>
+    {props.posts.map(post => (
+      <Post post={post} />
+    ))}
+  </>
+);
 
-    <p>
-      <span>comments: </span>
-      {comments.length}
-    </p>
+const Post = (props) => (
+  <section>
+    <h2>
+      {props.post.title}
+    </h2>
+    <p>{props.post.body} </p>
+    <User user={props.post.user} />
+    <CommentList comments={props.post.comments} />
+  </section>
+);
 
-    <p>
-      <span>Users: </span>
-      {users.length}
-    </p>
-  </div>
+const User = (props) => (
+  <>
+    <div>
+      author:
+      {props.user.name}
+    </div>
+    <div>
+      write me:
+      {props.user.email}
+    </div>
+    <div>
+      best regards from:
+      {props.user.address.city}
+      ,  street:
+      {props.user.address.street}
+      ,  flat:
+      {props.user.address.suite}
+      ,  zipcode:
+      {props.user.address.zipcode}
+    </div>
+  </>
+);
+
+const CommentList = (props) => (
+  <>
+    <h5> comments:</h5>
+    {props.comments.map(comment => (
+      <Comment comment={comment} />
+    ))}
+  </>
+);
+
+const Comment = (props) => (
+  <>
+    <div>{props.comment.body}</div>
+    <div>
+      name:
+      {props.comment.name}
+    </div>
+    <div>
+      mail:
+      {props.comment.email}
+    </div>
+    <h7> - - - - - - - -</h7>
+  </>
 );
 
 export default App;
