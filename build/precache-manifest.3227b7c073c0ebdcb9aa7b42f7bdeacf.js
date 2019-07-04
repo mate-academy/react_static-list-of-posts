7c9a57f7d8cfaa5759f47d129ca2bf300c138066
Darya Vyshniakova <vishnya054@gmail.com>
2019-07-04 23:00:29 +0300

    all done

---
+++ build/precache-manifest.3227b7c073c0ebdcb9aa7b42f7bdeacf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d3eacd4a3e9cd62710f7b96b142a32",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "8b8ecd7803f7808bfe40",
+    "url": "/react_static-list-of-posts/static/css/main.4ec3559b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_static-list-of-posts/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8b8ecd7803f7808bfe40",
+    "url": "/react_static-list-of-posts/static/js/main.122cc5e7.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
