10c5771115fe925290178905e43ba5365876988f
tearfulmerlin <t380937709275@gmail.com>
2019-07-04 16:35:13 +0300

    solution without props validation

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,19 +2,17 @@ import React from 'react';
 import './post-list.scss';
 
 import Post from './Post';
-import CommentList from "./CommentList";
+import CommentList from './CommentList';
 
-const PostList = (props) => {
-  return (
-    <section className="post-list">
-      {props.posts.map(post => (
-        <div className="post-wraper" key={post.id}>
-          <Post post={post} />
-          <CommentList comment={post.commentsToPost} />
-        </div>
-      ))}
-    </section>
-  );
-};
+const PostList = props => (
+  <section className="post-list">
+    {props.posts.map(post => (
+      <div className="post-wraper" key={post.id}>
+        <Post post={post} />
+        <CommentList comment={post.commentsToPost} />
+      </div>
+    ))}
+  </section>
+);
 
 export default PostList;
