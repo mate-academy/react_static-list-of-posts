e9202fc083ef425eff913ce2fdc592b818763f40
tearfulmerlin <t380937709275@gmail.com>
2019-07-04 17:19:01 +0300

    solution

--- src/components/User.js
+++ src/components/User.js
@@ -6,6 +6,7 @@ const User = ({ user }) => (
   <div className="user">
     <div className="user__photo"></div>
     <span className="user__name">{user.name}</span>
+    <span className="user__address">City: {user.address.city}</span>
   </div>
 );
 export default User;
