6e948e88c5b6024539c5c5f421e74ccc37274ddd
SerGolub89 <k3210389gsi@gmail.com>
2019-07-18 09:37:47 +0300

    'solution'

--- src/Post.js
+++ src/Post.js
@@ -6,8 +6,8 @@ import users from './api/users';
 import comments from './api/comments';
 
 function Post({ post }) {
-  const filteredUsers = users.find(user => user.id === post.userId);
-  const Users = filteredUsers.map(user => <User key={user.id} user={user} />);
+  const findUser = users.find(user => user.id === post.userId);
+  const user = <User key={findUser.id} user={findUser} />;
 
   const filterCom = comments.filter(com => com.postId === post.id);
   const Comments = filterCom.map(com => <Comment key={com.id} comment={com} />);
@@ -16,7 +16,7 @@ function Post({ post }) {
     <div className="post">
       <h3>{post.title}</h3>
       <p>{post.body}</p>
-      {Users}
+      {user}
       <h3>Comments</h3>
       <hr />
       {Comments}
@@ -26,7 +26,7 @@ function Post({ post }) {
 
 Post.propTypes = {
   post: PropTypes.object,
-  usrId: PropTypes.number,
+  userId: PropTypes.number,
   id: PropTypes.number,
   title: PropTypes.string,
   body: PropTypes.string,
