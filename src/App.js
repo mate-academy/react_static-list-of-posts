8b8d347308085966c48b9e5d0324a622d8fff445
yevhen-sahach <antitop04@gmail.com>
2019-09-09 20:24:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,26 +5,27 @@ import './App.css';
 import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
+import Postlist from './PostList/PostList';
 
 const App = () => (
   <div className="App">
     <h1>Static list of posts</h1>
 
-    <p>
-      <span>posts: </span>
-      {posts.length}
-    </p>
-
-    <p>
-      <span>comments: </span>
-      {comments.length}
-    </p>
-
-    <p>
-      <span>Users: </span>
-      {users.length}
-    </p>
+    <Postlist posts={preparedPosts} />
   </div>
 );
+
+const usersMap = users.reduce((acc, item) => ({ ...acc, [item.id]: item }), {});
+
+const getPosts = (postsArr, commentsArr, usersArr) => (
+  postsArr
+    .map(post => ({
+      ...post,
+      comments: commentsArr.filter(comment => comment.postId === post.id),
+      user: usersMap[post.userId],
+    }))
+);
+
+const preparedPosts = getPosts(posts, comments, users);
 
 export default App;
