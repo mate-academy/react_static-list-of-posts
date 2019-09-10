f89607db888e7f45908096dbcbfad9e61dc5ee65
KseniiaMazan <ksusamazan@gmail.com>
2019-09-10 14:51:10 +0300

    more comments are fixed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './App.css';
 
-import apiPosts from './api/ApiPosts';
-import apiComments from './api/ApiComments';
-import apiUsers from './api/ApiUsers';
+import apiPosts from './api/apiPosts';
+import apiComments from './api/apiComments';
+import apiUsers from './api/apiUsers';
 import PostList from './Components/PostList/PostList';
 import Header from './Components/Header/Header';
 
