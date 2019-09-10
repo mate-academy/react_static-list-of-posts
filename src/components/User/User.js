d86a1e52d8bf62daa390bad468a599f1835472ce
vladyslav223 <vladyslav223@gmail.com>
2019-09-11 00:17:07 +0300

    Done

---
+++ src/components/User/User.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './User.scss';
+const User = ({ user }) => {
+  let {email,name,address}  = user;
+  let {street,suite,city,zipcode} = address;
+  return (
+  <div className='user'>
+    <div className='user__name'>
+    {name}
+    </div>
+    <div className='user__email'>
+    {email}
+    </div>
+    <div className='user__address'>
+      <div className='user__address-street'>{street}</div>
+      <div className='user__address-suite'>{suite}</div>
+      <div className='user__address-city'>{city}</div>
+      <div className='user__address-zipcode'>{zipcode}</div>
+    </div>
+  </div>
+  );
+}
+
+
+
+
+
+export default User;
