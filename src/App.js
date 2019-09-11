202f65f1b651bddb39e048161ccf6bba61616ac3
amserhii11 <amelin.serhii@gmail.com>
2019-09-12 00:16:49 +0300

    Add solution

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React from 'react';
-
 import './App.css';
-
 import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
+import PostList from './components/PostList/PostList';
 
-const App = () => (
-  <div className="App">
-    <h1>Static list of posts</h1>
+const preparedPosts = UserToPosts(posts, users);
 
-    <p>
-      <span>posts: </span>
-      {posts.length}
-    </p>
+function App() {
+  return (
+    <div className="App">
+      <h1>Static list of posts</h1>
 
-    <p>
-      <span>comments: </span>
-      {comments.length}
-    </p>
+      <p>
+        <span>posts: </span>
+        {posts.length}
+      </p>
 
-    <p>
-      <span>Users: </span>
-      {users.length}
-    </p>
-  </div>
-);
+      <p>
+        <span>comments: </span>
+        {comments.length}
+      </p>
+
+      <p>
+        <span>Users: </span>
+        {users.length}
+      </p>
+      <PostList posts={preparedPosts} comments={comments} />
+    </div>
+  );
+}
+
+function UserToPosts(listPost, listUser) {
+  return listPost.map(post => ({
+    ...post,
+    user: listUser.find(user => user.id === post.userId),
+  }));
+}
 
 export default App;
