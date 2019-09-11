20761304618dcec411baffaf467853f25844fd06
unknown <shklyarigor2015@gmail.com>
2019-09-11 13:35:50 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -24,6 +24,18 @@ const App = () => {
   return (
     <div className="App">
       <h1>Static list of posts</h1>
+      <p>
+        <span>posts: </span>
+        {posts.length}
+      </p>
+      <p>
+        <span>comments: </span>
+        {comments.length}
+      </p>
+      <p>
+        <span>Users: </span>
+        {users.length}
+      </p>
       <PostList posts={preparedPosts} />
     </div>
   );
