aba472bbd93a3b2da5cdcf63750ac71ce7c00a29
Rafler <amuronyk@gmail.com>
2019-09-11 14:10:39 +0300

    Fixing comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './App.css';
 import PostsList from './components/PostsList/PostsList';
+import Header from './components/Header/Header';
 
 import posts from './api/posts';
 import comments from './api/comments';
@@ -20,41 +21,7 @@ const preparedPosts = getPostsWithData(posts, users, comments);
 
 const App = () => (
   <div className="App">
-    <h1>
-      <span className="ui red header">Static</span>
-      {' '}
-      <span className="ui green header">list</span>
-      {' '}
-      <span className="ui yellow header">of</span>
-      {' '}
-      <span className="ui blue header">posts</span>
-    </h1>
-    <div className="ui statistics">
-      <div className="teal statistic">
-        <div className="value">
-          {posts.length}
-        </div>
-        <div className="label">
-          POSTS
-        </div>
-      </div>
-      <div className="orange statistic">
-        <div className="value">
-          {users.length}
-        </div>
-        <div className="label">
-          USERS
-        </div>
-      </div>
-      <div className="purple statistic">
-        <div className="value">
-          {comments.length}
-        </div>
-        <div className="label">
-          COMMENTS
-        </div>
-      </div>
-    </div>
+    <Header />
     <PostsList posts={preparedPosts} />
   </div>
 );
