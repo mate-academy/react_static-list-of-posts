ca1f4856890007d7235aaf82eb1c4ff30d3feefc
Vyacheslav Piskun <webdev.pv@gmail.com>
2019-09-11 08:03:40 +0300

    add functions get, function Header and some styles

--- src/App.js
+++ src/App.js
@@ -1,52 +1,44 @@
+/* eslint-disable arrow-body-style */
 import React from 'react';
 import './App.css';
 import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
-import User from './components/User/User';
-import Comment from './components/Comment/Comment';
-import Post from './components/Post/Post';
-import Postlist from './components/PostList/PostList';
-import CommentList from './components/CommentList/CommentList';
+// import User from './components/User/User';
+// import Comment from './components/Comment/Comment';
+// import Post from './components/Post/Post';
+// import Postlist from './components/PostList/PostList';
+// import CommentList from './components/CommentList/CommentList';
+// import getPostsWithUsers from './getPostsWithUsers';
+// import getPostWithComments from './getPostWithComments';
 
-const App = () => (
-  <div className="App">
-    <h1>Static list of posts</h1>
-    <p>
-      <span>posts: </span>
-      {posts.length}
-    </p>
-    <p>
-      <span>comments: </span>
-      {comments.length}
-    </p>
-    <p>
-      <span>Users: </span>
-      {users.length}
-    </p>
-    <TestComponents />
-  </div>
-);
+const App = () => {
+  // const postsWithUsers = getPostsWithUsers(posts, users);
+  // const postList = getPostWithComments(postsWithUsers, comments);
 
-function TestComponents() {
   return (
-    <div>
-      <hr />
-      user
-      <User user={users[0]} />
-      <hr />
-      comment
-      <Comment comment={comments[0]} />
-      <hr />
-      post
-      <Post post={posts[0]} />
-      <hr />
-      posts
-      <Postlist posts={posts} />
-      <hr />
-      comments
-      <CommentList comments={comments} />
-      <hr />
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Static list of posts</h1>
+      <p>
+        <span>posts: </span>
+        {posts.length}
+      </p>
+      <p>
+        <span>comments: </span>
+        {comments.length}
+      </p>
+      <p>
+        <span>Users: </span>
+        {users.length}
+      </p>
     </div>
   );
 }
