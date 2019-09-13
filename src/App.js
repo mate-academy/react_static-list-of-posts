ece19ad82f4a56111d4d27eda50f78abcabf04cd
Volodsher <volodsher@gmail.com>
2019-09-13 09:09:13 +0300

    100%

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
+import PostList from './components/PostList/PostList';
 import './App.css';
-
 import posts from './api/posts';
+import comments from './api/comments';
 import users from './api/users';
 
-function App() {
-  return (
-    <div className="posts-list">
-      {posts.map((post) => {
-        const postUser = users.find(user => user.id === post.userId);
+const preparedPosts = posts.map(post => ({
+  ...post,
+  user: users.find(user => post.userId === user.id),
+  comments: comments.filter(comment => comment.postId === post.id),
+}));
 
-        return (
-          <div className="post">
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            <div className="user-info">
-              <span>{postUser.name}</span>
-              <a href={`mailto:${postUser.email}`}>{postUser.email}</a>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1 className="title">Static list of posts</h1>
+    <PostList posts={preparedPosts} />
+  </div>
+);
 
 export default App;
