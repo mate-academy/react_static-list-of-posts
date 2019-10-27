a6628bd8930b415ca57b6ee108aadb43692e4011
VadimK <djvadya16@gmail.com>
2019-10-27 18:33:06 +0200

    add random photo

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -6,8 +6,8 @@ function Post(props) {
     <div className="post">
       <>
         <div className="authorName"><strong>{props.name}</strong></div>
-        <div className="photo"><img src="https://source.unsplash.com/collection/190727/444x400
-        /?food,game,car,nature,animal" className="main-image" alt='post' /></div>
+        <div className="photo"><img src={"https://source.unsplash.com/collection/190727/" + Math.round(Math.random() * (450 - 400) + 400) + "x" + Math.round(Math.random() * (350 - 300) + 300) +
+        "/?food,game,car,nature,animal"} className="main-image" alt='post' /></div>
         <div className="title"><strong>{props.title}</strong></div>
         <div className="bodyText">{props.body}</div>
         <div className="commentsVisible"><strong>Watch comments ({props.comments.length})</strong></div>
