41b6a96dbdcef08db54ce903d5efdea308f53b97
Veronika D <donets.veronika@yahoo.com>
2019-10-29 13:32:37 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React from 'react';
-
 import './App.css';
 
 import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
+import PostList from './components/PostList';
+
+function getAllPosts(postList, usersList, commentList) {
+  postList.forEach((post) => {
+    post.user = usersList.find(user => user.id === post.userId);
+    post.comments = commentList.filter(comment => comment.postId === post.id);
+  });
+
+  return postList;
+}
+
+const AllPosts = getAllPosts(
+  posts, users, comments
+);
 
 const App = () => (
-  <div className="App">
-    <h1>Static list of posts</h1>
-
-    <p>
-      <span>posts: </span>
-      {posts.length}
-    </p>
-
-    <p>
-      <span>comments: </span>
-      {comments.length}
-    </p>
-
-    <p>
-      <span>Users: </span>
-      {users.length}
-    </p>
-  </div>
+  <>
+    <div className="App">
+      <h1>Static list of posts</h1>
+    </div>
+    <PostList posts={AllPosts} />
+  </>
 );
 
 export default App;
