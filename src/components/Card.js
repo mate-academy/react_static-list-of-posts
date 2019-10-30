8fecfc6cec21b4bfec07225bd4291f12f0c023e9
Dmitriymush <powder322@gmail.com>
2019-10-30 14:30:48 +0200

    fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,29 +2,35 @@ import React from 'react';
 import DropDown from "./DropDown";
 import User from "./User";
 
-function Card({ post }) {
+function Card({ post: {
+  user,
+  title,
+  body,
+  comment,
+  id,
+} }) {
   const wide = Math.round(Math.random() * (410 - 390) + 800);
   const height = Math.round(Math.random() * (410 - 390) + 900);
   return (
     <div className="ui card">
       <div className="content">
-        <div className="right floated meta"> {post.id} h ago</div>
+        <div className="right floated meta"> {id} h ago</div>
         <img className="ui avatar image" src={`https://source.unsplash.com/${wide}x${height}/?man,girl,blog,posts,notes,fashion`} />
-        <User user={post.user} />
+        <User user={user} />
       </div>
       <div className="image">
         <img className="big_image" src={`https://source.unsplash.com/${wide}x${height}/?man,girl,blog,posts,notes,fashion`} />
       </div>
       <div className="content">
-        <h3>{post.title}</h3>
-        <p>{post.body}</p>
+        <h3>{title}</h3>
+        <p>{body}</p>
         <span className="right floated">
           <i className="heart outline like icon" />
           {} likes
         </span>
         <div className="click">
           <i className="comment icon" />
-          {post.comment.length} comments
+          {comment.length} comments
         </div>
       </div>
       <DropDown post={post} />
