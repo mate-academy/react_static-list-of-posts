49f4ac529691b7edab173508c058b7d4c950b416
Chepkyi Mykola <chepkiyn@gmail.com>
2019-10-31 17:14:51 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -6,24 +6,57 @@ import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
 
+import PostList from './components/PostList/PostList';
+
+function getPostsWithUsersAndComments() {
+  const usersMap = new Map();
+  const commentsMap = new Map();
+
+  users.forEach((user) => {
+    usersMap.set(user.id, user);
+  });
+  comments.forEach((comment) => {
+    const indicator = commentsMap.get(comment.postId);
+
+    if (!indicator) {
+      const commentMapValue = [];
+
+      commentMapValue.push(comment);
+      commentsMap.set(comment.postId, commentMapValue);
+    } else {
+      const commentMapValue = [...indicator];
+
+      commentMapValue.push(comment);
+      commentsMap.set(comment.postId, commentMapValue);
+    }
+  });
+
+  return posts.map((post) => {
+    const postsWithUsersAndComments = Object.create(post);
+
+    postsWithUsersAndComments.user = usersMap.get(post.userId);
+    postsWithUsersAndComments.comments = commentsMap.get(post.id);
+
+    return postsWithUsersAndComments;
+  });
+}
+
 const App = () => (
   <div className="App">
     <h1>Static list of posts</h1>
-
     <p>
       <span>posts: </span>
       {posts.length}
     </p>
-
     <p>
       <span>comments: </span>
       {comments.length}
     </p>
-
     <p>
       <span>Users: </span>
       {users.length}
     </p>
+    <PostList posts={getPostsWithUsersAndComments()} />
   </div>
 );
 
