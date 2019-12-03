076cc22a429c00f8cd7baf6aee457d1b6809980c
AnnaGerdii <55839769+AnnaGerdii@users.noreply.github.com>
2019-12-03 20:27:36 +0200

    Update src/PostList.js
    Co-Authored-By: Kryvytskyy Volodymyr <38065883+vkryvytskyy@users.noreply.github.com>

--- src/PostList.js
+++ src/PostList.js
@@ -5,7 +5,7 @@ import Post from './Post';
 const PostList = props => (
   <dl>
     {props.posts.map(currentPost => (
-      <Post post={post} key={post.id} />
+      <Post post={currentPost} key={post.id} />
     ))}
   </dl>
 );
