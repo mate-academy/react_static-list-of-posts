7e4738904f4243b96a35f009ca1e4e7c671e8c65
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-12-04 18:27:47 +0200

    style fix

--- src/User.js
+++ src/User.js
@@ -5,24 +5,21 @@ const User = ({ user }) => (
 
   <>
     <p>
-     Name:
-      {' '}
-      {user.name}
-      {'  '}
-     E-mail:
-      {' '}
-      {user.email}
+      <span className="author__name">{`Name: ${user.name}`}</span>
+      <span className="author__mail">{`E-mail: ${user.email}`}</span>
     </p>
     <p>
-    Address:
-      {' '}
-      {user.address.city}
-      {user.address.street}
-      {user.address.suite}
-      {'  '}
-    Zipcode:
-      {' '}
-      {user.address.zipcode}
+      <span className="author__address">
+        {
+          `Address:
+         ${user.address.city}
+        ${user.address.street}
+        ${user.address.suite}`
+        }
+      </span>
+      <span className="author__zipcode">
+        {`Zipcode:  ${user.address.zipcode}`}
+      </span>
     </p>
 
   </>
