08be118dba535a1357d98a47ab18174726537e98
vadim-os <osinsky_vadim@yahoo.com>
2019-12-05 16:06:27 +0200

    solution

---
+++ src/Comment.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Each Comment should show the name, body and email.
+
+const Comment = ({ comment }) => (
+  <div className="comment">
+    <h4 className="comment--name">
+      {comment.name}
+    </h4>
+    <div className="comment--email">
+      {comment.email}
+    </div>
+    <p className="comment--body">{comment.body}</p>
+  </div>
+);
+
+Comment.propTypes = {
+  comment: PropTypes.shape({
+    name: PropTypes.string,
+    body: PropTypes.string,
+    email: PropTypes.string,
+  }).isRequired,
+};
+
+export default Comment;
---
+++ src/User.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const User = ({ user }) => (
+  <div className="user">
+    <b>Post by:</b>
+    <div className="user--name">{user.name}</div>
+    <div className="user--email">{user.email}</div>
+  </div>
+);
+
+User.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+  }).isRequired,
+};
+
+export default User;
