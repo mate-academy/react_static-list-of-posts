32e4f3261fc231c01dea7913973202c2c1a1c1eb
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-02-12 15:23:00 +0200

    fix2

--- src/CommentList.js
+++ src/CommentList.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import Comment from './Comment';
 
 const CommentList = ({ comments }) => (
-  <React.Fragment>
+  <>
     {comments.map(comment => (
       <Comment key={comment.id} oneComment={comment} />
     ))}
-  </React.Fragment>
+  </>
 );
 
 CommentList.propTypes = {
--- src/User.js
+++ src/User.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const User = ({ user }) => (
-  <React.Fragment>
+  <>
     <p>{`👳${user.name}`}</p>
     <p>{`🏠 ${user.address.city}`}</p>
     <a href={user.email}>{`📧 ${user.email}`}</a>
-  </React.Fragment>
+  </>
 );
 
 User.propTypes = {
