77666f71142cff63e1b00b77d76f52b7c4699180
Actireon <eheieiei37@gmail.com>
2020-02-13 22:02:00 +0200

    bold text

--- src/Comment/Comment.js
+++ src/Comment/Comment.js
@@ -7,7 +7,7 @@ const Comment = ({ name, body, email }) => (
   <li className="comments">
     <p>{name}</p>
     <p>{body}</p>
-    <p>
+    <p className="comments__email">
       Email:
       {' '}
       {email}
