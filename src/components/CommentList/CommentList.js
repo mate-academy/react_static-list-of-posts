604b7bcdf1684aa698d4547c84dcee662d3b9b51
Natalia Lypovyk <cardinnata@gmail.com>
2020-02-14 01:33:57 +0200

    List of Posts

---
+++ src/components/CommentList/CommentList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Comment } from '../Comment/Comment';
+
+export const CommentList = ({ comments }) => (
+  <ul className="comments">
+    {comments.map(comment => (
+      <li className="comment" key={comment.id}>
+        <Comment {...comment} />
+      </li>
+    ))}
+  </ul>
+);
+
+CommentList.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.shape(Comment)).isRequired,
+};
---
+++ src/components/PostList/PostList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Post } from '../Post/Post';
+
+export const PostList = ({ posts }) => (
+  <div className="post-list">
+    {posts.map(post => (
+      <Post key={post.id} post={post} />
+    ))}
+  </div>
+);
+
+PostList.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
