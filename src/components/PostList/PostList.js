6e1871a15e200161c99396384fee52b27cdafb81
Natalia Lypovyk <cardinnata@gmail.com>
2020-02-14 11:25:16 +0200

    PropTypes described

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -11,5 +11,17 @@ export const PostList = ({ posts }) => (
 );
 
 PostList.propTypes = {
-  posts: PropTypes.arrayOf(PropTypes.shape).isRequired,
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    body: PropTypes.string,
+    id: PropTypes.number,
+    comments: PropTypes.arrayOf(PropTypes.object).isRequired,
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      address: PropTypes.shape({
+        city: PropTypes.string,
+      }).isRequired,
+    }),
+  })).isRequired,
 };
