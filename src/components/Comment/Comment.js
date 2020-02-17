e00eea94bfbbe93d0cbaa32ba0a4d5d3b5fea4af
Stanislav Mochyr <delenjer@gmail.com>
2020-02-17 20:10:30 +0200

    destruction

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Comment = ({ comment }) => (
-  <div className="comment">
-    <h4 className="comment__name">
-      {comment.name}
-    </h4>
-    <div className="comment__email">
-      {comment.email}
+const Comment = ({ comment }) => {
+  const { name, email, body } = comment;
+
+  return (
+    <div className="comment">
+      <h4 className="comment__name">
+        {name}
+      </h4>
+      <div className="comment__email">
+        {email}
+      </div>
+      <p className="comment__body">{body}</p>
     </div>
-    <p className="comment__body">{comment.body}</p>
-  </div>
-);
+  );
+};
 
 Comment.propTypes = {
   comment: PropTypes.shape({
