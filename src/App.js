a88c2725e25397bc715b7b79bd4d224b7ab70ced
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-04-24 13:28:29 +0300

    only App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
 
+const preparedPosts = posts.map((post) => {
+  const user = users.find(curUser => curUser.id === post.userId);
+  // eslint-disable-next-line max-len
+  const userComments = comments.filter(curComent => (curComent.postId === post.id));
+
+  return {
+    ...post,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      address: user.address,
+    },
+    userComments,
+  };
+});
+
 const App = () => (
   <div className="App">
     <h1>Static list of posts</h1>
@@ -24,6 +41,35 @@ const App = () => (
       <span>Users: </span>
       {users.length}
     </p>
+    <div className="container">
+      {preparedPosts.map(post => (
+        <div className="post" key="post.id">
+          <h2 className="post__title">
+            {post.title}
+          </h2>
+          <p className="post__body">
+            {post.body}
+          </p>
+
+          <div className="post__user user">
+            <p>{post.user.name}</p>
+            <p>{post.user.email}</p>
+            <p>{post.user.address.city}</p>
+            <p>{post.user.address.street}</p>
+            <p>{post.user.address.suite}</p>
+          </div>
+          <div className="post__coments coments">
+            {post.userComments.map(comment => (
+              <div key={comment.id} className="comment">
+                <p>{comment.name}</p>
+                <p>{comment.body}</p>
+                <p>{comment.email}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 );
 
