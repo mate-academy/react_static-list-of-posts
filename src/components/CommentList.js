0059eff08414a0ac294cf8fe0d3969df6fec15c8
ZhupanovOlexii <zhupanovolexii@gmail.com>
2020-04-26 21:54:13 +0300

    solution

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Comment from './Comment';
-import './CommentList.scss';
 
 const CommentList = ({ comments }) => (
   <div className="comments">
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Post from './Post';
-import './PostList.scss';
 
 const PostList = ({ posts }) => (
   <>
