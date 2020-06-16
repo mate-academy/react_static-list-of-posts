e2ee3378f415736cf25582653ed426a931764f3d
aleksey-10 <alexcare1337@gmail.com>
2020-06-16 14:19:10 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import './App.css';
 import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
+import { PostList } from './components/PostList/PostList';
+
+const preparedPosts = posts.map(post => (
+  {
+    ...post,
+    comments: comments.filter(comment => post.id === comment.postId),
+    user: users.find(user => post.userId === user.id),
+  }
+));
 
 const App = () => (
   <div className="App">
@@ -24,6 +33,8 @@ const App = () => (
       <span>Users: </span>
       {users.length}
     </p>
+
+    <PostList posts={preparedPosts} />
   </div>
 );
 
