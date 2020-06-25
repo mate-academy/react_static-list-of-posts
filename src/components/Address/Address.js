ad52cf5421dafb4d51539b18d160c1e3e95abd57
LimonovInsp <bis.limonov@gmail.com>
2020-06-25 09:53:42 +0300

    refactoring

--- src/components/Address/Address.js
+++ src/components/Address/Address.js
@@ -2,19 +2,12 @@ import React from 'react';
 import './Address.css';
 import { addressShape } from '../Shapes/AddressShape';
 
-const Address = (props) => {
-  const geo = {
-    lat: props.geo.lat,
-    lng: props.geo.lng,
-  };
-
-  return (
-    <div className="address">
-      <p>{getAddress(props)}</p>
-      <p>{`Lat: ${geo.lat}, Lng: ${geo.lng}`}</p>
-    </div>
-  );
-};
+const Address = props => (
+  <div className="address">
+    <p>{getAddress(props)}</p>
+    <p>{`Lat: ${props.geo.lat}, Lng: ${props.geo.lng}`}</p>
+  </div>
+);
 
 const getAddress = (props) => {
   const address = Object.assign({}, props);
