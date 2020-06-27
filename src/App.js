2a94fa8b05e9d701c3bc1b705a471d4afc59eaf7
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-27 10:44:34 +0300

    refactor init prj state as  bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
+import Alert from 'react-bootstrap/Alert';
 
 import './App.css';
 
@@ -7,24 +12,29 @@ import comments from './api/comments';
 import users from './api/users';
 
 const App = () => (
-  <div className="App">
+  <Container className="App">
     <h1>Static list of posts</h1>
-
-    <p>
-      <span>posts: </span>
-      {posts.length}
-    </p>
-
-    <p>
-      <span>comments: </span>
-      {comments.length}
-    </p>
-
-    <p>
-      <span>Users: </span>
-      {users.length}
-    </p>
-  </div>
+    <Row>
+      <Col sm>
+        <Alert variant="primary">
+          posts:
+          <Badge variant="secondary">{posts.length}</Badge>
+        </Alert>
+      </Col>
+      <Col sm>
+        <Alert variant="primary">
+          comments:
+          <Badge variant="secondary">{comments.length}</Badge>
+        </Alert>
+      </Col>
+      <Col sm>
+        <Alert variant="primary">
+          Users:
+          <Badge variant="light">{users.length}</Badge>
+        </Alert>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default App;
