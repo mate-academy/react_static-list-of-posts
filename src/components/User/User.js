d580ba7cc9751ad5fbda76589a9a75f9041faff9
martakupka <marta-k2008@ukr.net>
2020-09-02 18:58:28 +0300

    Solution

---
+++ src/components/User/User.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './User.scss';
+import PropTypes from 'prop-types';
+
+export const User = ({ name, email, address }) => (
+  <div className="posts__author">
+    <p>
+      <strong>{'Author: '}</strong>
+      {name}
+    </p>
+
+    <div>
+      <p>
+        <strong>Contacts:</strong>
+      </p>
+      <p>{email}</p>
+      <p>
+        {Object.values(address)
+          .filter(item => typeof item === 'string')
+          .join(', ')}
+      </p>
+    </div>
+  </div>
+);
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  address: PropTypes.shape({
+    street: PropTypes.string.isRequired,
+    suite: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    zipcode: PropTypes.string.isRequired,
+  }).isRequired,
+};
