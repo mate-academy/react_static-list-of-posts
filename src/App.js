28cf53f60d53af36d4d805979a1632f67eb35087
Vlad Savustianenko <vladsavustianenko@gmail.com>
2020-09-02 20:20:23 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
 
-import './App.scss';
-
-import posts from './api/posts';
-import comments from './api/comments';
-import users from './api/users';
+import './styles.css';
+import PostList from './components/PostList';
 
 const App = () => (
   <div className="App">
-    <h1>Static list of posts</h1>
-
-    <p>
-      <span>posts: </span>
-      {posts.length}
-    </p>
-
-    <p>
-      <span>comments: </span>
-      {comments.length}
-    </p>
-
-    <p>
-      <span>Users: </span>
-      {users.length}
-    </p>
+    <PostList />
   </div>
 );
 
