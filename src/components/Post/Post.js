24c964fc67b4d7e4ed0fa7798eda98a8658db4ae
dmitriy <dmitriy.bondarenko.it@gmail.com>
2020-10-26 17:23:58 +0200

    fix variable name in post title

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -11,7 +11,7 @@ export const Post = ({ title, body, user, comments }) => (
       {title}
     </h1>
     <p className="post__body">
-      {body.replace(/^[a-z]/, leter => leter.toUpperCase())}
+      {body.replace(/^[a-z]/, letter => letter.toUpperCase())}
       {'.'}
     </p>
     <User {...user} />
