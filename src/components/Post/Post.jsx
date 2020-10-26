3908e001b189bedccec134f5c60b9b0d3e2de451
Olena kuzii <okuziy@gmail.com>
2020-10-26 16:57:48 +0200

    add margin

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -8,13 +8,13 @@ import { PostShape } from '../../shapes/PostShape';
 export const Post = ({ title, body, user, comments }) => (
   <li className="post">
     <div>
-      <User {...user} />
       <p className="post__title">
         {title}
       </p>
       <p className="post__body">
         {body}
       </p>
+      <User {...user} />
     </div>
     <CommentList comments={comments} />
   </li>
