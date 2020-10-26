4c43a0b21baef2d3a38a7d46b2457b05565844a8
KirillLutsenko <mnrovski68@gmail.com>
2020-10-26 12:24:30 +0200

    take out adress shape into a separate constant

--- src/components/shapes/UserShape.js
+++ src/components/shapes/UserShape.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 
+const AdressShape = {
+  street: PropTypes.string.isRequired,
+  suite: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  zipcode: PropTypes.string.isRequired,
+};
+
 export const UserShape = {
   name: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
-  adress: PropTypes.shape({
-    street: PropTypes.string.isRequired,
-    suite: PropTypes.string.isRequired,
-    city: PropTypes.string.isRequired,
-    zipcode: PropTypes.string.isRequired,
-  }).isRequired,
+  adress: PropTypes.shape(AdressShape).isRequired,
 };
