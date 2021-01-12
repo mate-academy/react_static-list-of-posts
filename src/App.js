2f4627086d4e425870eb26d68e6154ee53a94d2f
Andrii Haidai <haidai.andriy@gmail.com>
2021-01-12 17:20:31 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import './App.scss';
 import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
+import PostList from './components/PostList/PostList';
 
 const App = () => (
   <div className="App">
     <h1>Static list of posts</h1>
-
+    <PostList
+      posts={posts}
+      users={users}
+      comments={comments}
+    />
     <p>
       <span>posts: </span>
       {posts.length}
