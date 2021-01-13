8dbc762d30c3e9a789cf5edfdf2ab50c4562d611
RioDron <riopositivniy@gmail.com>
2021-01-13 15:54:52 +0200

    eddited PropTypes

--- src/types.js
+++ src/types.js
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
 
-export const userType = {
+export const userType = PropTypes.shape({
   name: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
   address: PropTypes.shape({
     street: PropTypes.string.isRequired,
   }).isRequired,
-};
+});
 
-export const commentType = {
-  name: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-};
+export const commentType = PropTypes.arrayOf(
+  PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  }),
+);
