46913e6a47ea9c124f3cbd92a47099ce55d724c4
Nikita Puhovskii <cheliojul@gmail.com>
2021-03-17 15:52:40 +0200

    develop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PostList } from './components/PostList';
 
 import './App.scss';
 
@@ -6,24 +7,21 @@ import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
 
+const preparedPosts = posts.map((post) => {
+  const postCopy = Object.assign({}, post);
+
+  postCopy.user = users.find(user => postCopy.userId === user.id);
+  postCopy.comments = comments.filter(coment => postCopy.id === coment.postId);
+
+  return postCopy;
+});
+
 const App = () => (
   <div className="App">
     <h1>Static list of posts</h1>
-
-    <p>
-      <span>posts: </span>
-      {posts.length}
-    </p>
-
-    <p>
-      <span>comments: </span>
-      {comments.length}
-    </p>
-
-    <p>
-      <span>Users: </span>
-      {users.length}
-    </p>
+    <div>
+      <PostList posts={preparedPosts} />
+    </div>
   </div>
 );
 
