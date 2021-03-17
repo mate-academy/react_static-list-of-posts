57a824c04e9af22f7ff55698c747479460bb5b4c
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-17 09:22:02 +0200

    Fixes

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -21,6 +21,7 @@ CommentList.propTypes = {
   comments: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
       email: PropTypes.string.isRequired,
       body: PropTypes.string.isRequired,
     }),
