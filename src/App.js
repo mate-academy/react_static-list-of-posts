d82c5ff3e8a23ea5c3af2ddc7fece1aca38fa9c5
Inna Sorochynska <innasoro@gmail.com>
2021-03-31 21:41:01 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React from 'react';
-
-import './App.scss';
-
 import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
+import { PostList } from './components/PostList';
 
 const App = () => (
-  <div className="App">
-    <h1>Static list of posts</h1>
+  <>
+    <div className="App">
+      <h1>Static list of posts</h1>
 
-    <p>
-      <span>posts: </span>
-      {posts.length}
-    </p>
+      <p>
+        <span>posts: </span>
+        {posts.length}
+      </p>
 
-    <p>
-      <span>comments: </span>
-      {comments.length}
-    </p>
+      <p>
+        <span>comments: </span>
+        {comments.length}
+      </p>
 
-    <p>
-      <span>Users: </span>
-      {users.length}
-    </p>
-  </div>
+      <p>
+        <span>Users: </span>
+        {users.length}
+      </p>
+    </div>
+    <PostList postList={preparedPosts} />
+  </>
 );
+
+export const preparedPosts = posts.map((post) => {
+  const commentsList = comments.filter(comment => comment.postId === post.id);
+
+  return {
+    ...post,
+    user: users.find(user => user.id === post.userId),
+    commentsList,
+  };
+});
 
 export default App;
