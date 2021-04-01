0bf0e349a682e9add3ee4134bc132a99c0b49faf
Onishchuk <onishchuk.danil@gmail.com>
2021-04-01 12:16:50 +0300

    fix

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import { Comments } from '../Comments';
+import { CommentList } from '../CommentList';
 import './post.scss';
 import PropTypes from 'prop-types';
+import { User } from '../User'
+import { userType} from '../User'
+import { commentType } from '../Comment'
 
 export const Post = ({author, title, body, comments}) => (
   <>
     <div className="Post">
       <h2>{title}</h2>
       <p>{body}</p>
-      <p>{author.name} / {author.email} / {author.address.city} {author.address.street} {author.address.suite}</p>
+      <div>
+        <User {...author}/>
+      </div>
     </div>
-    <div className="Comments">
-    {comments.map(item => (
-        <div key={item.id}>
-          <Comments {...item} />
-        </div>
-      ))}
-    </div>
+    <CommentList allComments={comments} />
   </>
 );
 
 Post.propTypes = {
-  author: PropTypes.object.isRequired,
+  author: userType,
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
-  comments: PropTypes.array.isRequired,
+  comments: PropTypes.arrayOf(commentType),
 }
