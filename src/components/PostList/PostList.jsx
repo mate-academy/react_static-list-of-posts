7fbfed52f79e9c27b56d1f4bd4167ebc7bb0dffe
scarlettjohanssons <wolverinelop@gmail.com>
2021-05-19 12:37:10 +0300

    second fix

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -5,15 +5,13 @@ import { Post } from '../Post';
 import './PostList.scss';
 
 export const PostList = ({ listOfPosts }) => (
-  <>
-    <ul className="post__list">
-      {listOfPosts.map(post => (
-        <li className="post__item" key={post.id}>
-          <Post {...post} />
-        </li>
-      ))}
-    </ul>
-  </>
+  <ul className="post__list">
+    {listOfPosts.map(post => (
+      <li className="post__item" key={post.id}>
+        <Post {...post} />
+      </li>
+    ))}
+  </ul>
 );
 
 PostList.propTypes = {
