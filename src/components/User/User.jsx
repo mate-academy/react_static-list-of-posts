e877bd8e7cd36ef5f411f9042758b7e6e079e339
Olga Tsarova <alita.jet@gmail.com>
2021-05-19 19:02:37 +0300

    Solution

---
+++ src/components/User/User.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './User.scss';
+
+export const User = ({ name, email, address }) => (
+  <div className="user">
+    <span className="user__name">{name}</span>
+    <a className="user__email" href="mailto:example@gmail.com">{email}</a>
+    <a className="user__address" href="google.com">
+      {`${address.city}, ${address.zipcode}`}
+    </a>
+  </div>
+);
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  address: PropTypes.shape({
+    city: PropTypes.string.isRequired,
+    zipcode: PropTypes.string.isRequired,
+  }).isRequired,
+};
