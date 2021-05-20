caccba484a0d7c08c1bd7efbc670fac003ffcaca
Bulaev Denis <bulaevdenis7@gmail.com>
2021-05-20 14:30:12 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -5,10 +5,26 @@ import './App.scss';
 import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
+import { PostList } from './components/PostList/PostList';
+
+const postsWithComments = posts.map(post => ({
+  ...post,
+  comments: comments.filter(comment => comment.postId === post.id),
+}));
+
+const usersWithPosts = users.map(user => ({
+  id: user.id,
+  name: user.name,
+  email: user.email,
+  address: user.address,
+  posts: postsWithComments.filter(post => post.userId === user.id),
+}));
 
 const App = () => (
   <div className="App">
     <h1>Static list of posts</h1>
+
+    <PostList users={usersWithPosts} />
 
     <p>
       <span>posts: </span>
