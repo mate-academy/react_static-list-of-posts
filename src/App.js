8fe70a29fa3597b4f5a521a55130c9425289d804
Sasha Chornyi <sasha-202@inbox.ru>
2021-05-21 15:58:21 +0300

    done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import posts from './api/posts';
 import comments from './api/comments';
 import users from './api/users';
 
+import { PostList } from './components/PostList';
+
+const preparedPosts = posts.map(post => ({
+  ...post,
+  user: users.find(user => user.id === post.userId),
+  comments: comments.filter(comment => comment.postId === post.id),
+}));
+
 const App = () => (
   <div className="App">
     <h1>Static list of posts</h1>
@@ -24,6 +32,8 @@ const App = () => (
       <span>Users: </span>
       {users.length}
     </p>
+
+    <PostList preparedPosts={preparedPosts} key={preparedPosts.id} />
   </div>
 );
 
