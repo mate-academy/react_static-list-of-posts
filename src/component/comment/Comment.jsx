68187924eeccd24265d19e04ee9ab7fa0e53cb97
romandubovskyi <rdubowskij@gmail.com>
2021-06-01 18:34:17 +0300

    removed redundant code

--- src/component/comment/Comment.jsx
+++ src/component/comment/Comment.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { CommentType } from './CommentType';
 import './Comment.css';
--- src/component/postlist/PostList.jsx
+++ src/component/postlist/PostList.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import { Post } from '../post';
 import './PostList.css';
 
-export const PostList = ({ list }) => {
-
-  return (
-    <ul className="posts">
-      {list.map(postContent => (
-        <li key={postContent.id}>
-          <Post {...postContent} />
-        </li>
-      ))}
-    </ul>
-  );
-};
+export const PostList = ({ list }) => (
+  <ul className="posts">
+    {list.map(postContent => (
+      <li key={postContent.id}>
+        <Post {...postContent} />
+      </li>
+    ))}
+  </ul>
+);
 
 PostList.propTypes = {
   list: PropTypes.arrayOf(PropTypes.shape({
