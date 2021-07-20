c220fa6fcdc4987bf047e29ee9520e6b5ab3ef3c
Artem <temaqwerty15@gmail.com>
2021-07-20 18:22:46 +0300

    fin4

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -6,7 +6,15 @@ import { Post } from '../Post';
 
 export const PostList = ({ posts }) => (
   <List className="list">
-    {posts.map(post => <ListItem key={post.id}><Post post={post} /></ListItem>)}
+    {
+      posts.map(
+        post => (
+          <ListItem key={post.id}>
+            <Post post={post} />
+          </ListItem>
+        ),
+      )
+    }
   </List>
 );
 
