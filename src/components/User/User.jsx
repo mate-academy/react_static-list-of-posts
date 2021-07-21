5389bcd15b9a908025faa4a93d9a756ee289449d
Ruslan Vyvdyuk <84183046+Vivdyuk@users.noreply.github.com>
2021-07-21 19:07:21 +0300

    address was added

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -4,12 +4,17 @@ import './User.scss';
 import { userPropTypes } from '../TypesOfProp';
 
 export const User = ({ user }) => {
-  const { name, email } = user;
+  const { name, email, address } = user;
+  const { street, suite, city, zipcode } = address;
 
   return (
     <>
       <p className="post__author">{name}</p>
       <p className="post__author-email">{email}</p>
+      <span className="post__author-address">
+        {`${street}, ${suite}, ${city}, ${zipcode}`}
+      </span>
+
     </>
   );
 };
