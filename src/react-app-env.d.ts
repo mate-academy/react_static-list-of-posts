8f105e156dc3a2e746b70e9ee27e8ade4be0b4f8
Dmitriy Pleshakov <dmitriypleshakov1@gmail.com>
2021-09-02 19:14:37 +0300

    React_Static-list-of-posts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="react-scripts" />
+
+interface Post {
+  userId: numer;
+  id: number;
+  title: string;
+  body: string;
+  user: User | null;
+  comment: Comment[];
+}
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Comment {
+  postId: number;
+  name: string;
+  body: string;
+  email: string;
+  id: number;
+}
