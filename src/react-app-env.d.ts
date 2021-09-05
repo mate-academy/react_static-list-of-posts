46ee4f1b46fd67c8f67631e4a2bd06b7f0c271fc
Andriy Nikorych <andrei,nikorich@gmail.com>
2021-09-05 21:34:04 +0300

    solution without CSS

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,48 @@
 /// <reference types="react-scripts" />
+
+interface Comments {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+  user: User | undefined;
+  comments: Comments[];
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
