e6d2e4a6b909578bb433c46b14c73e3660be1163
Oleksandr Zhitenko <geetonio28@gmail.com>
2021-09-05 20:59:47 +0300

    task id done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
 import './App.scss';
+import { PostList } from './components/PostList/PostList';
+import { Post } from './types/Post';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+import { Comment } from './types/Comment';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+const preparedPosts: Post[] = posts.map(post => ({
+  ...post,
+  user: users.find(userObj => userObj.id === post.userId) || null,
+  comments: comments.filter(comment => comment.postId === post.id) as Comment[],
+}));
 
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of posts</h1>
+    <PostList content={preparedPosts} />
   </div>
 );
 
