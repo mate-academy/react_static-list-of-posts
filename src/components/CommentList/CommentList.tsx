f0b097f24d039ec3bfd9d1ccdc745ab5a95714de
Volodymyr Khimiak <vv.himiak@gmail.com>
2021-09-06 18:54:20 +0300

    commit after changes

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -5,16 +5,14 @@ type Props = {
   comments: Comment[];
 };
 
-export const CommentList: React.FC<Props> = (props) => {
-  return (
-    <>
-      {props.comments.map(comment => {
-        return (
-          <ul key={comment.id}>
-            <CommentInfo comment={comment} />
-          </ul>
-        );
-      })}
-    </>
-  );
-};
+export const CommentList: React.FC<Props> = (props) => (
+  <>
+    {props.comments.map(comment => {
+      return (
+        <ul key={comment.id}>
+          <CommentInfo comment={comment} />
+        </ul>
+      );
+    })}
+  </>
+);
