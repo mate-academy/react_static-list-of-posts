0acdb114630c7ad3cb8f592640dcc1e20ccef13d
Kostiantyn Besedin <besconstantine@gmail.com>
2021-10-11 22:26:43 +0300

    fixes after student code review

--- src/api/components/CommentInfo/CommentInfo.tsx
+++ src/api/components/CommentInfo/CommentInfo.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { Comment } from '../../../types/Comment';
 import './CommentInfo.scss';
 
-export const CommentInfo: React.FC<{ comment: Comment }> = ({ comment }) => (
-  <div className="CommentInfo">
-    <h3 className="CommentInfo-Name">{comment.name}</h3>
-    <p className="CommentInfo-Body">{comment.body}</p>
-    <div className="CommentInfo-Email">{comment.email.toLocaleLowerCase()}</div>
-  </div>
-);
+export const CommentInfo: React.FC<{ comment: Comment }> = ({ comment }) => {
+  const {
+    name,
+    body,
+    email,
+  } = comment;
+
+  return (
+    <div className="CommentInfo">
+      <h3 className="CommentInfo-Name">{name}</h3>
+      <p className="CommentInfo-Body">{body}</p>
+      <div className="CommentInfo-Email">{email.toLocaleLowerCase()}</div>
+    </div>
+  );
+};
