4e2f45d22b11c1e94656951a4afb07741ca0ede4
Kostiantyn Besedin <besconstantine@gmail.com>
2021-10-12 08:29:09 +0300

    more fixes

--- src/api/components/PostInfo/PostInfo.tsx
+++ src/api/components/PostInfo/PostInfo.tsx
@@ -4,19 +4,28 @@ import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo: React.FC<{ post: Post }> = ({ post }) => (
-  <div className="PostInfo">
-    {post.user && (
-      <div className="PostInfo-User">
-        <UserInfo user={post.user} />
-      </div>
-    )}
-    <h2 className="PostInfo-Title">{post.title}</h2>
-    <p className="PostInfo-Body">{post.body}</p>
-    {post.comments.length && (
-      <div className="PostInfo-Comments">
-        <CommentList comments={post.comments} />
-      </div>
-    )}
-  </div>
-);
+export const PostInfo: React.FC<{ post: Post }> = ({ post }) => {
+  const {
+    user: userFromPost,
+    title,
+    body,
+    comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      {userFromPost && (
+        <div className="PostInfo-User">
+          <UserInfo user={userFromPost} />
+        </div>
+      )}
+      <h2 className="PostInfo-Title">{title}</h2>
+      <p className="PostInfo-Body">{body}</p>
+      {comments.length && (
+        <div className="PostInfo-Comments">
+          <CommentList comments={comments} />
+        </div>
+      )}
+    </div>
+  );
+};
--- src/api/components/UserInfo/UserInfo.tsx
+++ src/api/components/UserInfo/UserInfo.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { User } from '../../../types/User';
 import './UserInfo.scss';
 
-export const UserInfo: React.FC<{ user: User }> = ({ user }) => (
-  <div className="UserInfo">
-    <img
-      className="UserInfo-Foto"
-      src="https://cdn3.iconfinder.com/data/icons/user-icon-4-1/100/user_2-07-512.png"
-      alt="User foto"
-    />
-    <h3 className="UserInfo-Nickname">{user.username}</h3>
-    <p className="UserInfo-Name">{`(${user.name})`}</p>
-    <a className="UserInfo-Email" href={`mailto:${user.email.toLocaleLowerCase()}`}>send e-mail to User</a>
-  </div>
-);
+export const UserInfo: React.FC<{ user: User }> = ({ user }) => {
+  const {
+    name,
+    username,
+    email,
+  } = user;
+
+  return (
+    <div className="UserInfo">
+      <img
+        className="UserInfo-Foto"
+        src="https://cdn3.iconfinder.com/data/icons/user-icon-4-1/100/user_2-07-512.png"
+        alt="User foto"
+      />
+      <h3 className="UserInfo-Nickname">{username}</h3>
+      <p className="UserInfo-Name">{`(${name})`}</p>
+      <a className="UserInfo-Email" href={`mailto:${email.toLocaleLowerCase()}`}>send e-mail to User</a>
+    </div>
+  );
+};
