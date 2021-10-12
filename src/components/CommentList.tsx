f9a857d3a671120412a64c70fc583c336e8da8bd
Maksym Vikolash <vikolashmaxim@gmail.com>
2021-10-12 13:23:48 +0300

    solution

---
+++ src/components/CommentList.tsx
@@ -0,0 +1,17 @@
+import { CommentInfo } from './CommentInfo';
+import { Comment } from '../types/Comment';
+
+export const CommentList: React.FC<{ comments: Comment[] }> = ({ comments }) => (
+  <>
+    {comments.length > 0 ? (
+      <ul className="comments">
+        {comments.map(comment => (
+          <li key={comment.id} className="commentsItem">
+            <CommentInfo comment={comment} />
+          </li>
+        ))}
+      </ul>
+    )
+      : <p>No comments</p>}
+  </>
+);
