054b2cc2bfdec6a4e6a57c38947f4700d296f8ed
Aleksey Senchenko <sav45731@gmail.com>
2021-11-08 23:13:14 +0300

    created list of posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-
+import { PostList } from './components/PostList';
 import './App.scss';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
 
-const App: React.FC = () => (
+const preparedPosts = posts.map(post => ({
+  ...post,
+  user: users.find(person => person.id === post.userId) || null,
+  comments: comments.filter(comment => comment.postId === post.id),
+}));
+
+export const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of posts</h1>
+    <h1 className="App__title">Static list of posts</h1>
+    <PostList posts={preparedPosts} />
   </div>
 );
-
-export default App;
