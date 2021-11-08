72056d856ffd1f2cc54ef2e27702944df1c8fcf4
Ivarovskiy <iva7art@gmail.com>
2021-11-08 10:46:49 +0400

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
 import './App.scss';
+import { PostList } from './components/PostList';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+
+const preparedPosts = posts.map(post => ({
+  ...post,
+  user: users.find(user => post.userId === user.id),
+  comments: comments.filter(comment => comment.postId === post.id),
+}));
 
 const App: React.FC = () => (
-  <div className="App">
-    <h1>Static list of posts</h1>
-  </div>
+  <>
+    <div className="App">
+      <PostList posts={preparedPosts} />
+    </div>
+  </>
 );
 
 export default App;
---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './UserInfo.scss';
+
+type UserFields = {
+  name: string | undefined,
+  email: string | undefined
+};
+
+export const UserInfo: React.FC<UserFields> = ({ name, email }) => (
+  <>
+    <div className="ccc">
+      <div className="name">
+        {name}
+      </div>
+      <div className="user-info">
+        {email}
+      </div>
+    </div>
+  </>
+);
