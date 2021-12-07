8d8f5444bc24eee873ab7ef6a62692b5122ff7c2
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-12-07 12:16:10 +0200

    minor fixes

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -13,8 +13,8 @@ export const PostInfo: React.FC<Props> = ({ post }) => {
     <>
       <h3 className="fullPost__title">{post.title}</h3>
       <p className="fullPost__body">{post.body}</p>
-      {post.user && (<UserInfo user={post.user} /> || null)}
-      {post.comments && (<CommentInfo comments={post.comments} /> || null)}
+      {post.user && <UserInfo user={post.user} />}
+      {post.comments && <CommentInfo comments={post.comments} />}
     </>
   );
 };
