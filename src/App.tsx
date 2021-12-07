d31c31ed96a33afaec096cf8bd3ac739fd45de17
Daryna Dudar <keks9ra1286@gmail.com>
2021-12-08 00:03:24 +0200

    Feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { PostList } from './components/PostList/PostList';
 
 import './App.scss';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+
+import { Post } from './types/Post';
+
+const preparedPosts: Post[] = posts.map(post => {
+  return {
+    ...post,
+    user: users.find(user => user.id === post.userId) || null,
+    comments: comments.filter((comment) => post.id === comment.postId),
+  };
+});
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of posts</h1>
+    <h1 className="App__title">Your feed</h1>
+    <PostList posts={preparedPosts} />
   </div>
 );
 
