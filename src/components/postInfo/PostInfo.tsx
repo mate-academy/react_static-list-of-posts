37e84982092fa7d8ceb364fdf62b64bec0d64c18
Veronika Korona <Korona.vero@gmail.com>
2021-12-08 17:05:29 +0100

    Fixed

--- src/components/postInfo/PostInfo.tsx
+++ src/components/postInfo/PostInfo.tsx
@@ -12,12 +12,10 @@ export const PostInfo: React.FC<Props> = ({ post }) => (
   <div className="post">
     <div className="post__title">{post.title}</div>
     <div className="post__body">{post.body}</div>
-    <div className="post__user">
-      {post.user && <UserInfo user={post.user} />}
-    </div>
+    {post.user && <UserInfo user={post.user} />}
     <div className="post__comments">
       <div>Comments:</div>
-      {post.comment && <CommentList comments={post.comment} />}
+      <CommentList comments={post.comment} />
     </div>
   </div>
 );
