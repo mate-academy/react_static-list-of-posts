53356b648f2a3aef4dc7b6848f339932bae7ea4f
TatianaMorozova04 <morozovatatiana04@gmail.com>
2021-12-08 16:39:53 +0200

    Posts

---
+++ src/components/CommentList/CommentList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './CommentList.scss';
+import { Comment } from '../../types/Comment';
+
+import { CommentInfo } from '../CommentInfo';
+
+type Props = {
+  comments: Comment [];
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <ul className="CommentList">
+    {comments.map((comment) => (
+      <li key={comment.id} className="CommentList__item">
+        <CommentInfo {...comment} />
+      </li>
+    ))}
+  </ul>
+);
