9f90e541eb45b4b3367205131fc2158cbc14a558
lanebx <havchikos@gmail.com>
2021-12-08 13:55:22 +0200

    Moved UserInfo and CommentsList inside PostInfo component

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import { IPost } from '../../types/IPost';
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
 
 import './PostInfo.scss';
 
-type Props = Pick<IPost, 'title' | 'body'>;
+type Props = Omit<IPost, 'userId' | 'id'>;
 
-export const PostInfo: React.FC<Props> = ({ title, body }) => {
+export const PostInfo: React.FC<Props> = ({
+  title,
+  body,
+  user,
+  comments,
+}) => {
   return (
-    <div className="postInfo">
-      <h3 className="postInfo__title">
-        {title}
-      </h3>
-      <p className="postInfo__body">
-        {body}
-      </p>
+    <div className="post">
+      <img
+        className="post__img"
+        src="https://dynaimage.cdn.cnn.com/cnn/q_auto,w_412,c_fill,g_auto,h_232,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F200305114843-01-edge-hudson-yards-observation-deck.jpg"
+        alt=""
+      />
+      <div className="post__body">
+        <div className="postInfo">
+          <h3 className="postInfo__title">
+            {title}
+          </h3>
+          <p className="postInfo__body">
+            {body}
+          </p>
+        </div>
+        <UserInfo
+          name={user === null ? ' ' : user.name}
+          email={user === null ? ' ' : user.email}
+        />
+        <CommentList
+          commentList={comments === null ? [] : comments}
+        />
+      </div>
     </div>
   );
 };
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { IPost } from '../../types/IPost';
-import { CommentList } from '../CommentList/CommentList';
 import { PostInfo } from '../PostInfo/PostInfo';
-import { UserInfo } from '../UserInfo/UserInfo';
 
 import './PostList.scss';
 
@@ -14,23 +12,13 @@ export const PostList: React.FC<Props> = ({ posts }) => {
   return (
     <div className="postList">
       {posts.map(post => (
-        <div className="post" key={post.id}>
-          <img className="post__img" src="https://dynaimage.cdn.cnn.com/cnn/q_auto,w_412,c_fill,g_auto,h_232,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F200305114843-01-edge-hudson-yards-observation-deck.jpg" alt="" />
-          <div className="post__body">
-            <PostInfo
-              title={post.title}
-              body={post.body}
-            />
-            <UserInfo
-              name={post.user === null ? 'no user name' : post.user.name}
-              email={post.user === null ? 'no user email' : post.user.email}
-            />
-            <CommentList
-              commentList={post.comments === null ? [] : post.comments}
-            />
-          </div>
-        </div>
-
+        <PostInfo
+          key={post.id}
+          title={post.title}
+          body={post.body}
+          user={post.user}
+          comments={post.comments}
+        />
       ))}
     </div>
   );
