d48391b482ee8fadb120d49c8783f38b6f837ab0
Aleksandr Prokopovich <aldmpr@gmail.com>
2021-12-08 07:44:29 +0200

    Solution without styling

---
+++ src/components/PostInfo/PostInfo.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+import './PostInfo.scss';
+import { Post } from '../../types/Post';
+
+type Props = Omit<Post, 'id'>;
+
+export const PostInfo: React.FC<Props> = ({
+  title, body, user, comments,
+}) => (
+  <article className="post">
+    <div className="post__author">
+      {user && <UserInfo {...user} />}
+    </div>
+
+    <div className="post__main">
+      <h2 className="post__title">{title}</h2>
+      <p className="post__text">{body}</p>
+
+      <div className="post__comments">
+        <CommentList comments={comments} />
+      </div>
+    </div>
+  </article>
+);
