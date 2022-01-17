9fe54a5bc4fb799a3e55b9fdb47369afe1eb010e
Bohdan Maksymiuk <bohdan.max@ukr.net>
2022-01-17 10:40:54 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import 'bulma/css/bulma.min.css';
 
-import './App.scss';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import { Post } from './types/Post';
+import { PostList } from './components/PostList';
+
+const preparedPosts: Post[] = posts.map(post => ({
+  ...post,
+  user: users.find(user => user.id === post.userId) || null,
+  comments: comments.filter(comment => comment.postId === post.id) || null,
+}));
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of posts</h1>
+    <h1 className="title is-1 is-centered">Static list of posts</h1>
+    <PostList posts={preparedPosts} />
   </div>
 );
 
