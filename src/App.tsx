86ac0e9b96a38e7e5f5f987a23315391dff2d907
Maksym Shymanskij <makskampod@gmail.com>
2022-01-17 20:36:55 +0200

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import './App.scss';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+import { PostList } from './PostList/PostList';
+
+const preparedPosts: Post[] = posts.map(post => ({
+  ...post,
+  user: users.find((user) => user.id === post.userId) || null,
+  comments: comments.filter(comment => comment.postId === post.id),
+}));
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of posts</h1>
+    <PostList preparedPosts={preparedPosts} />
   </div>
 );
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="react-scripts" />
+interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
+  user: User | null,
+  comments: Comments[],
+}
+
+interface User {
+  name: string,
+  email: string,
+
+}
+
+interface Comments {
+  name: string,
+  body: string,
+  email: string,
+}
