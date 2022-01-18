8c03a0a788fc68dfa160228ade7270b4c83922c1
Mikedacheka <mikedacheka@gmail.com>
2022-01-18 19:40:28 +0200

    sm msg

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,26 @@
 /// <reference types="react-scripts" />
+
+interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
+}
+
+interface User {
+  name: string,
+  email: string,
+}
+
+interface Coment {
+  postId: number,
+  id: number,
+  name: string,
+  email: string,
+  body: string,
+}
+
+interface PreparedPost extends Post{
+  user: User | null,
+  comments: Coment[] | null,
+}
