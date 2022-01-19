e42feb658cb5d48c2bce71e7ef3b5d9bf47b93fd
Andrii Koptiev <koptev.andrey87@gmail.com>
2022-01-19 14:10:34 +0200

    fixed2 after mentor review

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const CommentList: React.FC<Props> = ({ commentaries }) => (
   <div className="ui comments">
     <h3 className="ui dividing header">Comments</h3>
-    {commentaries && (
+    {commentaries.length > 0 && (
       <ul className="comment-list">
         {commentaries.map(commentary => (
           <li key={commentary.id} className="comment-list__item">
