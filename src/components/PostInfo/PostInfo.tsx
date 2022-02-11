84de64cd87b3ad39e8e87963274a5a56e42f8aa7
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-02-11 12:35:47 +0200

    files added

---
+++ src/components/PostInfo/PostInfo.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Post } from '../../types';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+type Prop = {
+  props: Post[],
+};
+
+export const PostInfo: React.FC<Prop> = ({ props }) => {
+  return (
+    props.map((prop: Post) => (
+      <div key={prop.id} className="post">
+        <h2>{prop.title}</h2>
+        {prop.user && <UserInfo user={prop.user} />}
+        <div>{prop.body}</div>
+        {prop.comments && <CommentInfo postComments={prop.comments} />}
+      </div>
+    ))
+  );
+};
