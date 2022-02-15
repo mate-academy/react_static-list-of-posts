925873fa6f6da9473720711df2beba13b1845996
mariiaStorozhyk <xmsha722@gmail.com>
2022-02-15 17:22:00 +0200

    tsx - ts

--- src/types/FullPost.tsx
+++
@@ -1,8 +0,0 @@
-import { Comment } from './Comment';
-import { Post } from './Post';
-import { User } from './User';
-
-export interface FullPost extends Post {
-  user?: User;
-  comments: Comment[];
-}
--- src/types/User.tsx
+++
@@ -1,6 +0,0 @@
-export interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-}
