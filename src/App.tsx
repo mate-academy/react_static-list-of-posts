3e2bd81eabfda320d27abc48fed1d5f2bcd7b102
Heorhii Plugoyi <gladiator943work@gmail.com>
2022-02-16 13:53:29 +0200

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import PostList from './components/PostList';
 
 import './App.scss';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+
+const preparedPosts = posts.map(post => {
+  const user = users.find(u => u.id === post.userId);
+  const postComments = comments.filter(c => c.postId === post.id);
+
+  if (!user) {
+    throw new Error('User not found.');
+  }
+
+  return {
+    ...post,
+    user,
+    comments: postComments,
+  };
+});
 
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of posts</h1>
+    <PostList posts={preparedPosts} />
   </div>
 );
 
