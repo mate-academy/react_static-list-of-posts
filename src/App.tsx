f3dad43276e05a5263b8d0f0a236ac59958661ef
Artem Paskal <artem.yogi@gmail.com>
2022-02-16 14:52:18 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-
 import './App.scss';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+
+import { PostList } from './Components/PostList/PostList';
+
+const preparedPosts = posts.map((post) => {
+  return {
+    ...post,
+    user: users.find((user) => user.id === post.userId) || null,
+    comments: comments.filter((comment) => comment.postId === post.id),
+  };
+});
 
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of posts</h1>
+    <PostList preparedPosts={preparedPosts} />
   </div>
 );
 
