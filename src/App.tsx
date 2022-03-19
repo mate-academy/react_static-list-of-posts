46cdc354c3c97c069fe31f050c2512ca829fca2d
Pasechnik Vitaly <pasechnik.vitaly.11@gmail.com>
2022-03-19 02:40:10 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-
 import './App.scss';
+import { IPreparedPost } from './types/Types';
+import { PostList } from './components/PostList/PostList';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+
+const preparedPosts: IPreparedPost[] = posts.map(post => {
+  return {
+    ...post,
+    user: users.find(user => post.userId === user.id) || null,
+    comments: comments.filter(comment => post.id === comment.postId) || null,
+  };
+});
 
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of posts</h1>
+    <PostList posts={preparedPosts} />
   </div>
 );
 
