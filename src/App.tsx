f612cd113ac702bdab2986ab10d58d36cc7bc8fa
Anna Serdechna <anserdechna@gmail.com>
2022-03-28 09:04:30 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import { FC } from 'react';
 
+import { preparedPosts } from './api/data';
+import { PostList } from './components/PostList/PostList';
 import './App.scss';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
-
-const App: React.FC = () => (
+const App: FC = () => (
   <div className="App">
-    <h1>Static list of posts</h1>
+    <PostList posts={preparedPosts} />
   </div>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import './index.scss';
 
 ReactDOM.render(
   <App />,
