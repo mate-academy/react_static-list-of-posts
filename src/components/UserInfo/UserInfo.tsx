69ed370c9ab909abf27dc1df6cd3370c30b6202e
Roma Boreiko <roma.boreiko@gmail.com>
2022-03-28 15:42:14 +0300

    Solution

---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { User } from '../../types/User';
+
+export const UserInfo: React.FC<User> = ({ name, email }) => (
+  <>
+    <div>{name}</div>
+    <div>{email}</div>
+  </>
+);
