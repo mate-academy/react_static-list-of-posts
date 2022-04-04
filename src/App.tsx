c37447c04f7431b3ea4af33bc16fff30aa80b6b0
KristinaTar <kristina.tarashchuk@gmail.com>
2022-04-04 21:44:51 +0300

    version2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-
+import { Post } from './types';
 import './App.scss';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import PostList from './PostList/PostList';
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Static list of posts</h1>
-  </div>
-);
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+
+const App: React.FC = () => {
+  const preparedPosts: Array<Post> = posts.map(e => {
+    return {
+      ...e,
+      user: users.find(user => user.id === e.userId) || null,
+      comments: comments.filter(comment => comment.postId === e.id) || null,
+    };
+  });
+
+  return (
+    <>
+      <div className="App">
+        <h1>Static list of posts</h1>
+      </div>
+      <PostList preparedPosts={preparedPosts} />
+    </>
+  );
+};
 
 export default App;
