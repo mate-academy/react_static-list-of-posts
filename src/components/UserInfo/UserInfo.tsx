abe536eea9dace78283191932de189e788758a72
Yegor Kochetkov <kegor.fx@gmail.com>
2022-05-02 13:16:23 +0300

    Add `anonymous` if username not available

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -12,7 +12,11 @@ export const UserInfo: React.FC<Props> = ({ user }) => (
   <p className="user-info">
     <span className="user-info__title">Author:</span>
     <span data-cy="name">
-      {` ${user?.name}`}
+      {
+        user
+          ? ` ${user.name}`
+          : 'anonymous'
+      }
     </span>
     <a className="user-info__link" href={`mailto:${user?.email}`} data-cy="email">Email</a>
   </p>
