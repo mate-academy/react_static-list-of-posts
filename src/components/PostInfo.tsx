0f5272d0334120a369601984dcf4d22496cbce26
Viacheslav Petrovskyi <v.petrovskyi@icloud.com>
2022-05-12 15:16:21 +0300

    fix

--- src/components/PostInfo.tsx
+++ src/components/PostInfo.tsx
@@ -13,6 +13,6 @@ export const PostInfo: React.FC<Props> = ({ post }) => (
     <div className="postTitle">{post.title}</div>
     <div className="postBody">{post.body}</div>
     {post.user && <UserInfo user={post.user} />}
-    {post.comments?.length && <CommentList comments={post.comments} />}
+    {post.comments.length && <CommentList comments={post.comments} />}
   </>
 );
