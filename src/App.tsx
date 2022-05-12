42a385b8ccca821d3731028f5d15bcaf96a64c10
unknown <avicostov2@gmail.com>
2022-05-12 22:52:09 +0200

    solut

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 
 import './App.scss';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+import { PostList } from './components/PostLits/PostList';
+
+const preparedPosts = posts.map(post => (
+  {
+    ...post,
+    user: users.find(user => user.id === post.userId) || null,
+    comments: comments.filter(comment => comment.postId === post.id) || null
+  }
+));
 
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of posts</h1>
+    <PostList prepPosts={preparedPosts}/>
   </div>
 );
 
