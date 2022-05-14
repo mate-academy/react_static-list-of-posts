fe40de7375d1399cab68438ed3afcad26e77ca8f
Andriy Fesych <andriyhimself@gmail.com>
2022-05-14 21:35:39 +0300

    save_1

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 import './App.scss';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+import { PostList } from './components/PostList';
+
+const preparedPosts = posts.map(singlepost => ({
+  ...singlepost,
+  user: (users.find(user => user.id === singlepost.userId) || null),
+  comments: (comments.filter(comment => comment.postId === singlepost.id)),
+}));
 
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of posts</h1>
+    <PostList posts={preparedPosts} />
   </div>
 );
 
