b113ba173b54099c3f8f4b4e611a49187663623e
Dmytro Semenenko <dmitriy.semenenko@gmail.com>
2022-05-28 22:36:55 +0300

    React Static List Of Posts Solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
-
 import './CommentInfo.scss';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+type Props = {
+  name: string;
+  email: string;
+  body: string;
+};
+
+export const CommentInfo: React.FC<Props> = ({ name, email, body }) => (
+  <div className="comments__item">
+    <span data-cy="comment-name" className="comments__item-name">
+      {'Name: '}
+      {name}
+    </span>
+    <br />
+    <span data-cy="comment-email" className="comments__item-email">
+      {'Email: '}
+      {email}
+    </span>
+    <br />
+    <span data-cy="comment-body" className="comments__item-body">
+      {'Comment: '}
+      {body}
+    </span>
+  </div>
 );
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import './PostList.scss';
+import { Post } from '../../types/Post';
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  postsList: Post[];
+};
+
+export const PostList: React.FC<Props> = ({ postsList }) => (
+  <ul className="post">
+    {postsList.map((postItem) => (
+      <li key={postItem.id}>
+        <PostInfo {...postItem} />
+      </li>
+    ))}
+  </ul>
 );
