af58d53b882a5ca575843297e09b6d86b9844cf0
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2022-06-01 01:09:33 +0300

    solution

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import './PostList.scss';
+import { PreparedPost } from '../../react-app-env';
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+interface Props {
+  posts: PreparedPost[],
+}
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <ul className="posts-list">
+    {posts.map(post => (
+      <li
+        className="posts-item"
+        key={post.id}
+        data-cy="post-info"
+      >
+        <PostInfo post={post} />
+      </li>
+    ))}
+  </ul>
 );
