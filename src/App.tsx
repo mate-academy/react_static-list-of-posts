30b8aa91677cd1a763bdfe0abca0a19d534651b5
Andriy Simyachenko <simyachenko@gmail.com>
2022-06-02 12:58:42 +0300

    commit message

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,38 @@ import React from 'react';
 
 import './App.scss';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import posts from './api/posts';
+import comments from './api/comments';
+import users from './api/users';
+import {
+  Post,
+  User,
+  Comment,
+  FullPost,
+} from './react-app-env';
+
+import { PostList } from './components/PostList/PostList';
+
+const preparingPostsFunc = (
+  postsList: Post[],
+  usersList: User[],
+  commentsList: Comment[],
+) => {
+  return postsList.map(post => ({
+    ...post,
+    user: usersList.find(user => user.id === post.userId),
+    comment: commentsList.filter(comment => comment.postId === post.id),
+  }));
+};
+
+const preparedPosts: FullPost[] = preparingPostsFunc(posts, users, comments);
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of posts</h1>
+    <h1 className="app__title">Static list of posts</h1>
+    <PostList
+      preparedPosts={preparedPosts}
+    />
   </div>
 );
 
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import './PostList.scss';
+import { FullPost } from '../../react-app-env';
+import { PostInfo } from '../PostInfo/PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  preparedPosts: FullPost[],
+};
+
+export const PostList: React.FC<Props> = ({ preparedPosts }) => (
+  <ul className="postList">
+    {preparedPosts.map(post => (
+      <li
+        key={post.id}
+        data-cy="post-info"
+        className="postList__item"
+      >
+        <PostInfo
+          post={post}
+        />
+      </li>
+    ))}
+  </ul>
 );
