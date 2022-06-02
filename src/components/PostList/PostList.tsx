bbc944a6d2f9b9e06a83ec292b0c3d3491c5149b
Andrei Sucman <sucmanandrei@gmail.com>
2022-06-02 22:33:13 +0300

    task completed

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { PostInfo } from '../PostInfo/PostInfo';
 import './PostList.scss';
+import { PreparedPost } from '../../app.typedef';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+interface Props {
+  posts: PreparedPost[];
+}
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <ul className="post-list">
+    {posts.map(post => {
+      return (
+        <li key={post.id} className="post-list__item">
+          <PostInfo post={post} />
+        </li>
+      );
+    })}
+  </ul>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import { User } from '../../app.typedef';
 import './UserInfo.scss';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+interface Props {
+  user: User;
+}
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <div className="user">
+    <h3 data-cy="user-name" className="user__name">{user.name}</h3>
+    <a
+      href="email"
+      data-cy="user-email"
+      className="user__email"
+    >
+      {user.email}
+    </a>
+  </div>
 );
