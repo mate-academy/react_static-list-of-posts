487ddf07284f8c66b25f0d7bcfae809540ddaf38
MabuzaM <mabuzamfano@gmail.com>
2022-06-03 09:14:03 +0200

    Created preparedPosts array

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
-
 import './App.scss';
+import {
+  Post,
+  PreparedPost,
+  User,
+  Comment,
+} from './appTypeDefs';
 
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import postsFromServer from './api/posts';
+import commentsFromServer from './api/comments';
+import usersFromServer from './api/users';
+import { PostList } from './components/PostList/PostList';
+
+export function preparePosts(
+  posts: Post[],
+  comments: Comment[],
+  users: User[],
+): PreparedPost[] {
+  return posts.map(post => ({
+    ...post,
+    user: users.find(user => (
+      user.id === post.userId
+    )) || null || undefined,
+    comment: comments.find(comment => (
+      comment.postId === post.id
+    )) || null || undefined,
+  }));
+}
+
+export const preparedPosts = preparePosts(
+  postsFromServer,
+  commentsFromServer,
+  usersFromServer,
+);
 
 const App: React.FC = () => (
   <div className="App">
+    <PostList posts={preparedPosts} />
     <h1>Static list of posts</h1>
   </div>
 );
