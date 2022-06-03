2bba4b0dd8b0c979eb4f3d6557c773f528a33a73
Lilia Mazur <liliamazur2508@gmail.com>
2022-06-03 21:35:35 +0300

    solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -9,10 +9,10 @@ type Props = {
 
 export const CommentInfo: React.FC<Props> = ({ comment }) => (
   <>
-    <h3 data-cy="comment__name">
+    <h3 data-cy="comment-name">
       {`Comment: ${comment.name}`}
     </h3>
-    <p data-cy="comment__body">{comment.body}</p>
-    <p data-cy="comment__email">{comment.email}</p>
+    <p data-cy="comment-body">{comment.body}</p>
+    <p data-cy="comment-email">{comment.email}</p>
   </>
 );
