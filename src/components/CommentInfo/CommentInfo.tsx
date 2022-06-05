dbdd74d355c12bf1ca7e5c602a0c1af51a254463
Lebogang Mogane <mogane420@gmail.com>
2022-06-05 18:21:30 +0200

    Solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import { Comment } from '../../react-app-env';
 import './CommentInfo.scss';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+interface Props {
+  comment: Comment | undefined;
+}
+
+export const CommentInfo: React.FC<Props> = ({ comment }) => (
+  <div className="commentInfo">
+    <p data-cy="comment-name">
+      <strong>CommentBy: </strong>
+
+      {comment ? comment.name : 'Unknown'}
+    </p>
+
+    <p data-cy="comment-body">
+      Comment:
+      {' '}
+      {comment ? comment.body : 'No comments'}
+    </p>
+
+    <p data-cy="comment-email">
+      Email:
+      {' '}
+      {comment ? comment.email : 'Unknown'}
+    </p>
+  </div>
 );
