8cd8c0a4c8dc2f8186c379bc32a00da9b157c4ba
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-06-17 14:07:32 +0300

    done

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { PostInfo } from '../PostInfo/PostInfo';
+import { Post } from '../../react-app-env';
 import './PostList.scss';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+interface Props {
+  posts: Post[]
+}
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <ul className="posts">
+    {posts.map(post => (
+      <li key={post.id}>
+        <PostInfo post={post} />
+      </li>
+    ))}
+  </ul>
 );
