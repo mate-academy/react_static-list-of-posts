0995969334bd624d9794c230db7794a1441deedb
mira1zu <mira1zu733@gmail.com>
2022-07-03 13:25:42 +0200

    Solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 
 import './CommentInfo.scss';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+type Props = {
+  name: string;
+  body: string;
+  email: string;
+};
+
+export const CommentInfo: React.FC<Props> = ({ name, body, email }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__user">
+      <h3 className="CommentInfo__name" data-cy="comment-name">{`${name}`}</h3>
+      {', '}
+
+      <a className="CommentInfo__email" href={`mailto: ${email}`} data-cy="comment-email">{email}</a>
+    </div>
+
+    <p data-cy="comment-body">{body}</p>
+  </div>
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,6 +1,37 @@
 import React from 'react';
+
 import './PostInfo.scss';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import User from '../../types/User';
+import Comment from '../../types/Comment';
+
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+type Props = {
+  title: string;
+  body: string;
+  user: User;
+  comments: Comment[];
+};
+
+export const PostInfo: React.FC<Props> = ({
+  title,
+  body,
+  user,
+  comments,
+}) => (
+  <>
+    <div className="PostInfo" data-cy="post-info">
+      <h1 data-cy="post-title">{title}</h1>
+
+      <p data-cy="post-body">{body}</p>
+    </div>
+
+    <UserInfo name={user.name} email={user.email} />
+
+    <hr />
+
+    <CommentList comments={comments} />
+  </>
 );
