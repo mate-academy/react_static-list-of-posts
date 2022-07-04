78d453a49c644d783265d7e23574c77166e548d4
Roman Marunkevich <vinnibea@gmail.com>
2022-07-04 22:04:07 +0300

    solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 import './CommentInfo.scss';
+import { Comments } from '../types/Comments';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+export const CommentInfo: React.FC<Comments> = ({ name, body, email }) => (
+  <>
+    <h3 className="posts__comment-title">
+      {name}
+    </h3>
+    <p className="posts__comment-mail">
+      {email}
+    </p>
+    <p className="posts__bottom">
+      {body}
+    </p>
+  </>
 );
--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+import { Comments } from '../types/Comments';
 import './CommentList.scss';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+type Prop = {
+  comments: Comments[];
+};
+export const CommentList: React.FC<Prop> = ({ comments }) => (
+  <ul className="posts__comment">
+    {comments.map(comment => (
+      <li key={comment.id} className="posts__content">
+        <CommentInfo
+          email={comment.email}
+          body={comment.body}
+          name={comment.name}
+        />
+      </li>
+    ))}
+  </ul>
 );
