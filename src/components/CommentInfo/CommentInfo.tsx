ff0a8ea088264b1d5f664fe084016758a1db57a6
Roman Marunkevich <vinnibea@gmail.com>
2022-07-04 22:13:48 +0300

    solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -5,13 +5,13 @@ import { Comments } from '../types/Comments';
 
 export const CommentInfo: React.FC<Comments> = ({ name, body, email }) => (
   <>
-    <h3 className="posts__comment-title">
+    <h3 className="posts__comment-title" data-cy="comment-name">
       {name}
     </h3>
-    <p className="posts__comment-mail">
+    <p className="posts__comment-mail" data-cy="comment-email">
       {email}
     </p>
-    <p className="posts__bottom">
+    <p className="posts__bottom" data-cy="comment-body">
       {body}
     </p>
   </>
