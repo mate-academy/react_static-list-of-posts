c69d5b64f837d7476535e0a0cb06138458dc0762
SpaceHook <maxdumiak39@gmail.com>
2022-07-07 17:38:47 +0300

    React list of pots - ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
+import './reset.scss';
 import './App.scss';
-
-// import posts from './api/posts';
-// import comments from './api/comments';
-// import users from './api/users';
+import { PostList } from './components/PostList/PostList';
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of posts</h1>
+    <PostList />
   </div>
 );
 
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { Post } from '../../types/Post';
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+type Props = {
+  post: Post,
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <>
+    <div
+      className="post"
+      data-cy="post-info"
+      key={post.id}
+    >
+      <h2 className="post__title" data-cy="post-title">{post.title}</h2>
+      <p className="post__body" data-cy="post-body">{post.body}</p>
+      {post.user && <UserInfo user={post.user} />}
+      {post.comments && <CommentList comments={post.comments} />}
+    </div>
+  </>
 );
