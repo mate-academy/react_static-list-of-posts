ce04785fa41797212fbf2727679930ffcb861955
Sasha Skrinnik <sasha.skrinnik@gmail.com>
2022-07-07 20:00:03 +0300

    Solution

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { PostInfo } from '../PostInfo/PostInfo';
 import './PostList.scss';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  posts: Post[],
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <ul className="posts__list">
+    {posts.map(post => (
+      <li className="posts__item" key={post.id}>
+        <PostInfo post={post} />
+      </li>
+    ))}
+  </ul>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 
 import './UserInfo.scss';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+type Props = {
+  user: User,
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <div className="user">
+    <p className="user__name" data-cy="user-name">
+      {user.name}
+    </p>
+
+    <p className="user__email" data-cy="user-email">
+      {user.email}
+    </p>
+  </div>
 );
