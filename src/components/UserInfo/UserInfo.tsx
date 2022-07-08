c99689f7b5411091e671ae5c99b9d891fbb7343e
Ihor Shynkar <aedricson@gmail.com>
2022-07-08 12:47:06 +0300

    added postlist

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { User } from '../../types/User';
 
 import './UserInfo.scss';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+type Props = {
+  user: User,
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <article className="userAbout">
+    <div className="userAbout__name" data-cy="user-name">
+      {user.name}
+    </div>
+
+    <div className="userAbout__email" data-cy="user-email">
+      {user.email}
+    </div>
+  </article>
 );
