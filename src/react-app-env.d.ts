e640e6dcdd801879ff09c513d22bd963b16427aa
2022YEAR <tonnyedgy@gmail.com>
2022-07-08 16:35:24 +0300

    Solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="react-scripts" />
+export interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
+  user: User | null,
+  comments: Comment[],
+}
+
+export interface User {
+  name: string,
+  email: string,
+}
+
+export interface Comment {
+  postId: number,
+  id: number,
+  name: string,
+  email: string,
+  body: string,
+}
