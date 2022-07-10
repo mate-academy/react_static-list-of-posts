2f9d996718ff417d3ae84b0800cc6e60d0b0bedb
Denys Politov <dpolitov2001@gmail.com>
2022-07-10 19:04:57 +0300

    first commit

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,3 +1,20 @@
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import React from 'react';
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
+
+type Props = {
+  comments: Comment[] | null;
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  (comments && comments.length > 0 && (
+    <div className="CommentList">
+      {comments.map(comment => <CommentInfo comment={comment} />)}
+    </div>
+  )) || (
+    <>
+      <hr />
+      <b>No comments yet</b>
+    </>
+  )
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,28 @@
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import React from 'react';
+import { Post } from '../../types/Post';
+import { CommentList } from '../CommentList';
+
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      {' Posted by  '}
+
+      {post.poster
+        && (
+          <a className="UserInfo" href={post.poster.email}>
+            {post.poster.name}
+          </a>
+        )}
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <CommentList comments={post.comments} />
+  </div>
 );
