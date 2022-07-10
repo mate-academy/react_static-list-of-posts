e07e8b8fcc2183dc0737e09284587e1566fa6e53
Khrystyna Sabat <satttinka@gmail.com>
2022-07-10 22:42:38 +0200

    first commit

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,3 +1,16 @@
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../../types/types';
+import './CommentList.scss';
+
+type Props = {
+  comments: Comment[]
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => (
+      <CommentInfo comment={comment} />
+    ))}
+  </div>
 );
