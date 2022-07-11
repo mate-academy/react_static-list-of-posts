e7bf7bbd50a8b794d7528c22569037a7512f12c2
Khrystyna Sabat <satttinka@gmail.com>
2022-07-11 09:19:13 +0200

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,26 @@ import { PostList } from './components/PostList';
 import postsFromServer from './api/posts';
 import commentsFromServer from './api/comments';
 import usersFromServer from './api/users';
+import { Post, User, Comment } from './types/types';
 
-const posts: any = postsFromServer.map(post => ({
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+function getComment(postId: number): Comment[] {
+  const foundComment = commentsFromServer.filter(comment => (
+    comment.postId === postId
+  ));
+
+  return foundComment;
+}
+
+const posts: Post[] = postsFromServer.map(post => ({
   ...post,
-  user: usersFromServer.find(user => user.id === post.userId),
-  comments: commentsFromServer.filter(comment => comment.postId === post.id),
+  user: getUser(post.userId),
+  comments: getComment(post.id),
 }));
 
 export const App: React.FC = () => (
