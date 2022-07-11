01db34eae0f95524d67d9d4dbf9893feffd65543
Lesya Savka <lesyasimple@gmail.com>
2022-07-11 13:41:25 +0300

    ver_01

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,3 +1,21 @@
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+import React from 'react';
+import { Post } from '../../types/Post';
+import { PostInfo } from '../PostInfo/PostInfo';
+import './PostList.scss';
+
+type Props = {
+  posts: Post[];
+};
+
+export const PostList: React.FC<Props> = (
+  { posts },
+) => (
+  <ul className="post-list">
+    {posts.map(post => (
+      <li className="post-list__item" key={post.id}>
+        <PostInfo post={post} />
+      </li>
+    ))}
+  </ul>
+
 );
