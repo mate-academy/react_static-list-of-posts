1406e0df78cc8ed423387d619a4bf867b25ceaf7
Illia Bielichenko <ibelich12@gmail.com>
2022-07-11 14:59:59 +0300

    solution was added

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,3 +1,13 @@
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+import React from 'react';
+import { PostComment } from '../../types/types';
+
+type Props = {
+  comment : PostComment;
+};
+
+export const CommentInfo: React.FC<Props> = ({ comment }) => (
+  <div key={comment.id}>
+    <h2>{comment.name}</h2>
+    <p>{comment.body}</p>
+  </div>
 );
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,3 +1,19 @@
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+import React from 'react';
+import { PreparedPost } from '../../types/types';
+import { PostInfo } from '../PostInfo';
+
+type Props = {
+  preparedPosts: PreparedPost[];
+};
+
+export const PostList: React.FC<Props> = ({ preparedPosts }) => (
+  <ul>
+    {preparedPosts.map(post => (
+      <div className="PostList">
+        <PostInfo post={post} />
+      </div>
+    ))}
+  </ul>
 );
+
+export default PostList;
