79a83108c9771ed10b1660681c7824f1a6e93d38
Dmitrii Pirohov <dmitriy_pirogov@live.com>
2022-07-12 20:00:35 +0300

    third version

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,3 +1,16 @@
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+import { Comment } from '../../types/Comment';
+import './CommentInfo.scss';
+
+type Props = {
+  comment: Comment,
+};
+
+export const CommentInfo: React.FC<Props> = ({ comment }) => (
+  <div className="comment">
+    <div className="comment__container">
+      <h5 className="comment__name">{comment.name}</h5>
+      <p className="comment__email">{comment.email}</p>
+    </div>
+    <p className="comment__body">{comment.body}</p>
+  </div>
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,25 @@
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import React from 'react';
+import { Post } from '../../types/Post';
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+import './PostInfo.scss';
+
+type Props = {
+  post: Post,
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <div className="post" data-cy="post-info">
+    {post.user !== null ? <UserInfo user={post.user} /> : null}
+
+    <p className="post__title" data-cy="post-title">
+      {post.title}
+    </p>
+
+    <p className="post__body" data-cy="post-body">
+      {post.body}
+    </p>
+
+    <CommentList comments={post.comment} />
+  </div>
 );
