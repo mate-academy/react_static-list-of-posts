5310a87d29733edc05be2182a18c799772eb061f
Mikhail Shchuka <mikhail.shchuka@gmail.com>
2022-07-17 15:41:08 +0300

    Solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,34 @@
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import { Comment } from '../../types/comment';
+import { Post } from '../../types/post';
+import { User } from '../../types/user';
+
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+import './PostInfo.scss';
+
+type Props = {
+  post: Post;
+  user: User;
+  comments: Comment[];
+};
+
+export const PostInfo: React.FC<Props> = ({ post, user, comments }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      {' Posted by  '}
+
+      <UserInfo user={user} />
+    </div>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    <CommentList
+      comments={comments.filter(({ postId }) => postId === post.id)}
+    />
+  </div>
 );
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,3 +1,29 @@
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+import { Comment } from '../../types/comment';
+import { Post } from '../../types/post';
+import { User } from '../../types/user';
+
+import { PostInfo } from '../PostInfo';
+
+type Props = {
+  posts: Post[];
+  users: User[];
+  comments: Comment[];
+};
+
+export const PostList: React.FC<Props> = ({ posts, users, comments }) => (
+  <>
+    {posts.map((post => (
+      <PostInfo
+        key={post.id}
+        post={post}
+        user={users.find(({ id }) => id === post.userId) || {
+          id: 0,
+          name: 'unfound',
+          username: 'unfound',
+          email: 'unfound',
+        }}
+        comments={comments}
+      />
+    )))}
+  </>
 );
