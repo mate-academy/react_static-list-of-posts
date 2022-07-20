524bd811c9372beda772b9b438c0906207edd015
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-07-20 17:38:55 +0300

    fix

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -14,7 +14,7 @@ export const PostInfo: React.FC<Props> = ({ post }) => (
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{post.title}</h3>
         {' Posted by  '}
-        <UserInfo user={post.user} />
+        {post.user && <UserInfo user={post.user} />}
       </div>
       <p className="PostInfo__body">
         {post.body}
