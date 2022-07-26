d3f36276c90f7d94f0ea5a902c4a0dc13b3f6817
fledith <barateon023@gmail.com>
2022-07-26 14:27:47 +0300

    Solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,3 +1,30 @@
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+
+import './CommentList.scss';
+
+import { Comment } from '../../types/Comment';
+
+type Props = {
+  comments: Comment[] | null;
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <>
+    {comments
+      ? (
+        <div className="CommentList">
+          {comments.map(comment => (
+            <CommentInfo key={comment.id} comment={comment} />
+          ))}
+        </div>
+      )
+      : (
+        <>
+          <hr />
+
+          <b>No comments yet</b>
+        </>
+      )}
+  </>
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,31 @@
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+import './PostInfo.scss';
+
+import { Post } from '../../types/Post';
+
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">
+        {post.title}
+      </h3>
+
+      {' Posted by  '}
+
+      <UserInfo user={post.user} />
+    </div>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    <CommentList comments={post.comments} />
+  </div>
 );
