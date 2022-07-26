01516709868a1c1c68a7801ab998ccf5f5b70c65
Ksenia Didukh <k.tsviatko@gmail.com>
2022-07-26 14:39:41 +0300

    Sol

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,31 @@
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import { Post } from '../../types/Post';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <>
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        {' Posted by  '}
+
+        {post.user && <UserInfo user={post.user} />}
+      </div>
+
+      <p className="PostInfo__body">
+        {post.body}
+      </p>
+
+      <hr />
+
+      {post.commentList && post.commentList.length > 0
+        ? (<CommentList comments={post.commentList} />)
+        : <b>No comments yet</b>}
+    </div>
+  </>
 );
