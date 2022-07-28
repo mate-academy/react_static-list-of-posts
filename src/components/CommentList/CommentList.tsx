1f1632d37d9d55a51e35ec44e0f8a4d74b11dee8
silchyn <ivan.silchin@gmail.com>
2022-07-28 21:51:38 +0300

    added static list of posts

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,3 +1,13 @@
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import React from 'react';
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
+
+type Props = {
+  comments: Comment[]
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => <CommentInfo comment={comment} />)}
+  </div>
 );
