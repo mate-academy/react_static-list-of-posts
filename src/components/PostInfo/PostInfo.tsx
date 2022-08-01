62bbafb3d45855dcf62147e82cecdd460263c09f
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-08-01 19:18:48 +0300

    posts have been rendered

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,32 @@
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+import { FormattedPost } from '../../Types/FormattedPost';
+
+type Props = {
+  post: FormattedPost;
+};
+
+export const PostInfo: React.FC<Props> = ({
+  post: {
+    title,
+    user,
+    body,
+    comments,
+  },
+}) => (
+  <>
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        {' Posted by  '}
+
+        {user && <UserInfo user={user} />}
+      </div>
+
+      <p className="PostInfo__body">{body}</p>
+
+      {comments.length && <CommentList comments={comments} />}
+    </div>
+  </>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,3 +1,13 @@
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+import { User } from '../../Types/User';
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user: { email, name } }) => (
+  <>
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  </>
 );
