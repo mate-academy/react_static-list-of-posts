3c63abe23b4c9da3126541c3236e3a6de1bab367
Volodymyr Dolishnii <leedeelee3@gmail.com>
2022-08-02 18:41:30 +0300

    Finallyyy, the tests passed

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,3 +1,12 @@
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+import React from 'react';
+import { User } from '../../types/User';
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
 );
