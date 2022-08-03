ec25c30e204e9e4a27ef0db0c54135ccaddd0a1c
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-08-03 10:57:00 +0300

    Key was added

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -14,7 +14,9 @@ export const CommentList: React.FC<Props> = ({ comments }) => {
       <>
         <hr />
         <div className="CommentList">
-          {comments.map(comment => <CommentInfo comment={comment} />)}
+          {comments.map(comment => (
+            <CommentInfo comment={comment} key={comment.id} />
+          ))}
         </div>
       </>
     );
