ac7bf2edab61fd3bcbaaf2cdca34255f94a9731d
Denys Korotkoruchko <denenergii@gmail.com>
2022-08-03 11:38:27 +0300

    ready

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,3 +1,14 @@
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import { Comments } from '../../types/Comments';
+import { CommentInfo } from '../CommentInfo';
+
+type Props = {
+  comments: Comments[];
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <div className="CommentInfo">
+    {comments.map(comment => (
+      <CommentInfo key={comment.id} comment={comment} />
+    ))}
+  </div>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,3 +1,15 @@
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
-);
+import { Users } from '../../types/Users';
+
+type Props = {
+  user: Users
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
