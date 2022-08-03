15f6b6af45b6344a823fa4c29c206446779f6fe5
denis-khristyuk <monsieur.apala@gmail.com>
2022-08-03 14:36:40 +0300

    solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,3 +1,24 @@
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+import React from 'react';
+import { Comment } from '../../types/Comment';
+
+type Props = {
+  comment: Comment
+};
+
+export const CommentInfo: React.FC<Props> = ({ comment }) => (
+  <>
+    <div className="CommentInfo__title">
+      <h3 className="CommentInfo__name">{comment.name}</h3>
+      {' by '}
+      <a
+        className="CommentInfo__email"
+        href={`mailto:${comment.email}`}
+      >
+        {comment.email}
+      </a>
+    </div>
+    <div className="CommentInfo__body">
+      {comment.body}
+    </div>
+  </>
 );
--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,3 +1,17 @@
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../../types/Comment';
+
+type Props = {
+  comments: Comment[];
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => (
+      <div className="CommentInfo" key={comment.id}>
+        <CommentInfo comment={comment} />
+      </div>
+    ))}
+  </div>
 );
