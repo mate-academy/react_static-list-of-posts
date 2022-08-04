5322fcb0d782dbc8d03a17ee3325778dc887858b
Danil Hodytskyi <danil.hodytskyi1105@gmail.com>
2022-08-04 15:26:03 +0300

    create static list of post

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,3 +1,16 @@
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+import React from 'react';
+import { Post } from '../types/Post';
+import { PostInfo } from '../PostInfo';
+import './PostList.scss';
+
+type Props = {
+  posts: Post[],
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+  </div>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,3 +1,13 @@
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+import React from 'react';
+import './UserInfo.scss';
+import { User } from '../types/User';
+
+type Props = {
+  user: User | null,
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user?.email}`}>
+    {user?.name}
+  </a>
 );
