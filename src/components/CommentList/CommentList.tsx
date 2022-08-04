bc130ca666141feb383a557f6c9b718047f95298
Vitaliy <vitmixgt@icloud.com>
2022-08-04 22:57:38 +0300

    solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,3 +1,21 @@
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../../Type/comments';
+import './CommentList.scss';
+
+type Props = {
+  comments: Comment[]
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  return (
+    <>
+      {comments.map(comment => (
+        <div className="CommentInfo">
+          <CommentInfo key={comment.id} comment={comment} />
+        </div>
+      ))}
+    </>
+
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,3 +1,20 @@
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
-);
+import React from 'react';
+import { User } from '../../Type/user';
+import './UserInfo.scss';
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const {
+    name,
+    email,
+  } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
