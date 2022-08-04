dee42c1a04e0d49d727e5929df1ba0d9312263ed
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-08-04 14:29:14 +0300

    firstCommit

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,3 +1,16 @@
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+import { Post } from '../../Types/Posts';
+
+type Props = {
+  posts: Post[]
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <div className="PostInfo">
+        <PostInfo {...post} />
+      </div>
+    ))}
+  </div>
 );
