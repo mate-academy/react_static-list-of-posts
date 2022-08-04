6523c65d3311c2dc16842d7c7cad7326d9fb77d9
Yaroslav Lizogub <lizogub.yar@gmail.com>
2022-08-04 13:20:14 +0300

    added key to CommentInfo component

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const CommentList: React.FC<Props> = ({ comments }) => (
   <div className="CommentList">
     {comments.map(comment => (
-      <CommentInfo comment={comment} />
+      <CommentInfo key={comment.id} comment={comment} />
     ))}
   </div>
 );
