aacfba6fb293fe34490688cb7e6deb451d87e1d7
Max Shylenko <maxshylenko@gmail.com>
2022-08-09 16:43:30 +0200

    react static list of posts done

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,3 +1,30 @@
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+type Props = {
+  name: string,
+  email: string,
+  description: string,
+};
+
+export const CommentInfo: React.FC<Props> = ({
+  name,
+  email,
+  description,
+}) => (
+  <>
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{name}</strong>
+
+      {' by '}
+
+      <a
+        className="CommentInfo__email"
+        href={`mailto:${email}`}
+      >
+        {email}
+      </a>
+    </div>
+
+    <div className="CommentInfo__body">
+      {description}
+    </div>
+  </>
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,41 @@
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import { Post } from '../../types/Post';
+
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <>
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">
+        {post.title}
+      </h3>
+
+      {' Posted by  '}
+
+      {post.user && (
+        <UserInfo
+          name={post.user.name}
+          email={post.user.email}
+        />
+      )}
+    </div>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    {post.comments.length === 0
+      ? (
+        <>
+          <hr />
+
+          <b>No comments yet</b>
+        </>
+      )
+      : <CommentList comments={post.comments} />}
+  </>
 );
