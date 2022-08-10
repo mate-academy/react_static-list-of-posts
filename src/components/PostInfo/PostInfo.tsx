75218b9baade5c9f31264bb3c2f8124f18132cb3
Koba_ <kovbasinskyv@gmail.com>
2022-08-10 12:40:05 +0300

    solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,27 @@
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import { FullPost } from '../../types/fullpost';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+type Post = {
+  post: FullPost,
+};
+
+export const PostInfo: React.FC<Post> = ({ post }) => (
+  <>
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {post.title}
+        </h3>
+
+        <UserInfo user={post.user} />
+      </div>
+
+      <p className="PostInfo__body">
+        {post.body}
+      </p>
+
+      <CommentList comments={post.comment} />
+    </div>
+  </>
 );
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,3 +1,16 @@
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+import { FullPost } from '../../types/fullpost';
+import { PostInfo } from '../PostInfo';
+
+type Postlist = {
+  postlist: FullPost[],
+};
+
+export const PostList: React.FC<Postlist> = ({ postlist }) => (
+  <>
+    {postlist.map((post: FullPost) => (
+      <div key={post.id} className="PostList">
+        <PostInfo post={post} />
+      </div>
+    ))}
+  </>
 );
