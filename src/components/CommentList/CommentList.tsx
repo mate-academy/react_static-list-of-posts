25be11bb7d4cfa71549248c1d94f136b43af14e7
Olha Chebotarova <chebotarovaolha@gmail.com>
2022-08-12 14:18:05 -0400

    List of posts render

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,3 +1,24 @@
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../../types/Comment';
+
+type Props = {
+  comments: Comment[]
+};
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  if (comments.length > 0) {
+    return (
+      <div className="CommentList">
+        {comments.map((com) => (
+          <CommentInfo key={com.id} comment={com} />
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <hr />
+      <b>No comments yet</b>
+    </>
+  );
+};
