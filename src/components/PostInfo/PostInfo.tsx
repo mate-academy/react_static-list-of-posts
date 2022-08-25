881b79e47eca5322ab2b67183ea5390be7f7add7
Nazar Kulchevych <gamebandit38@gmail.com>
2022-08-25 15:05:07 +0300

    done destructurization

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -7,30 +7,39 @@ type Props = {
   post: Post;
 };
 
-export const PostInfo: React.FC<Props> = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">
-        {post.title}
-      </h3>
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    body,
+    user,
+    comments,
+  } = post;
 
-      {' Posted by  '}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        {' Posted by  '}
+        {
+          user
+            ? <UserInfo user={user} />
+            : ''
+        }
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
       {
-        post.user
-          ? <UserInfo user={post.user} />
-          : ''
+        comments
+          ? <CommentList comments={comments} />
+          : <b>No comments yet</b>
       }
+
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-
-    {
-      post.comments
-        ? <CommentList comments={post.comments} />
-        : <b>No comments yet</b>
-    }
-
-  </div>
-);
+  );
+};
