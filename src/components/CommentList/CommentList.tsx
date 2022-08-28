738a91d68f5e2a9fcd3a3eab7bf7c274d6153b17
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-28 20:27:00 +0300

    Added CommentList component & styles

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,3 +1,22 @@
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
+
+import './CommentList.scss';
+
+type Props = {
+  comments: Comment[],
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  if (!comments.length) {
+    return null;
+  }
+
+  return (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo comment={comment} key={comment.id} />
+      ))}
+    </div>
+  );
+};
