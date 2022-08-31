636b4cc86b03a897141e24511e2ca9bdbc990784
Nikita Havryliuk <chinter.games1@gmail.com>
2022-08-31 16:51:34 +0300

    solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { User } from '../types/User';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+import './UserInfo.scss';
+
+type Props = {
+  user: User | null,
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => user && (
+  <p>
+    {' Posted by  '}
+
+    <a className="UserInfo" href={user.email}>
+      {user.name}
+    </a>
+  </p>
 );
