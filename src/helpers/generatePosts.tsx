9a0065b38e2574e69be7d528b446afede907e891
Maks Sobko <tiserettt@gmail.com>
2022-09-04 15:34:37 +0300

    added functions for finding comments and users by id

--- src/helpers/generatePosts.tsx
+++ src/helpers/generatePosts.tsx
@@ -1,18 +1,23 @@
 import { Post } from '../types/Post';
 import { Comment } from '../types/Comment';
+import { User } from '../types/User';
 
 import postsFromServer from '../api/posts';
 import commentsFromServer from '../api/comments';
 import usersFromServer from '../api/users';
 
-function getComments(postId: number): Comment[] {
+function getCommentsById(postId: number): Comment[] {
   return commentsFromServer.filter(comment => (
     comment.postId === postId
   ));
 }
 
+function getUserById(userId: number): User | null {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
 export const posts: Post[] = postsFromServer.map(post => ({
   ...post,
-  comments: getComments(post.id),
-  user: usersFromServer.find(user => user.id === post.userId) || null,
+  comments: getCommentsById(post.id),
+  user: getUserById(post.userId),
 }));
