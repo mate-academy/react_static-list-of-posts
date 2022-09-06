5d1e007ca9e73a1f8dbc9790869ab3cfc0be01fa
Synytsyn Denys <yungdena54@gmail.com>
2022-09-06 13:14:14 +0300

    List of posts(React)

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,44 @@
 import React from 'react';
+import { Posts } from '../../types/Posts';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+type Props = {
+  post: Posts
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    body,
+    user,
+    comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        {' Posted by '}
+        {user
+          ? <UserInfo user={user} />
+          : ''}
+
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+
+      { comments
+        ? <CommentList comments={comments} />
+        : <b data-cy="NoCommentsMessage">No comments yet</b> }
+
+    </div>
+  );
+};
