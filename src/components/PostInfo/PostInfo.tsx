db60d49ceab6ba9eaf8e3e3466f7eef6049ea2c7
Mark Shapka <mark.shapka1@gmail.com>
2022-09-07 20:08:59 +0300

    Task_react-static-list-of-posts

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import { User } from '../../types/User';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+import { Comment } from '../../types/Comment';
+import './PostInfo.scss';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+type Props = {
+  title: string;
+  body: string;
+  user: User | null;
+  comments: Comment[];
+};
+
+export const PostInfo: React.FC<Props> = ({
+  title,
+  body,
+  user,
+  comments,
+}) => (
+  <>
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        {user && <UserInfo name={user.name} email={user.email} />}
+
+      </p>
+    </div>
+
+    <p className="PostInfo__body">
+      {body}
+    </p>
+
+    <hr />
+
+    {comments.length
+      ? <CommentList commentsList={comments} />
+      : <b data-cy="NoCommentsMessage">No comments yet</b>}
+  </>
 );
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Post } from '../../types/Post';
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  posts: Post[];
+};
+
+export const PostList: React.FC<Props> = ({ posts = [] }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <div className="PostInfo" key={post.id}>
+        <PostInfo
+          title={post.title}
+          body={post.body}
+          user={post.user}
+          comments={post.comments}
+        />
+      </div>
+    ))}
+  </div>
 );
