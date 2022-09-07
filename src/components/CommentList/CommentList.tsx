c17e61c28283818e666f2b8870011a0a05395696
Cyril Prue <kir8mir>
2022-09-07 17:44:13 +0300

    Solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import commentsFromServer from '../../api/comments';
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+const comments: Comment[] = commentsFromServer;
+
+interface Props {
+  postId: number;
+}
+
+export const CommentList: React.FC<Props> = ({ postId }) => {
+  const needComments = comments.filter(comment => (
+    postId === comment.postId
+  ));
+
+  return (
+
+    needComments.length > 0
+      ? (
+        <div className="CommentList">
+          {
+            needComments.map(comment => (
+              <CommentInfo comment={comment} key={comment.id} />
+            ))
+          }
+        </div>
+      )
+      : <b data-cy="NoCommentsMessage">No comments yet</b>
+  );
+};
