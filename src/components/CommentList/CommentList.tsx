a143364da6ff8991600178e799ac6ec7fa850e35
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-09-07 18:18:57 +0300

    list-of-posts done and all components were separated

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+import { CommentFromServer } from '../../types/Comments';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = {
+  comments: CommentFromServer[] | null;
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  return comments?.length
+    ? (
+      <div className="CommentList">
+        {comments?.map((comment) => (
+          <CommentInfo comment={comment} key={comment.id} />
+        ))}
+      </div>
+    )
+    : <b data-cy="NoCommentsMessage">No comments yet</b>;
+};
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+import { Post } from '../../types/Posts';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    user,
+    comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {post.title}
+        </h3>
+
+        <p>
+          {' Posted by  '}
+          {post.user && <UserInfo user={user} />}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {post.body}
+      </p>
+
+      <hr />
+
+      {post.comments && <CommentList comments={comments} />}
+    </div>
+  );
+};
