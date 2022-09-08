273e2da0c9f75124b9b1d308e3413a1fa72454fd
Max Kravchenko <max.kravchenko@gmail.com>
2022-09-08 13:52:17 +0300

    add static list of posts before SCSS separation

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,50 @@
 import React from 'react';
+import './PostInfo.scss';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import { PostToRender } from '../../types/PostToRender';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList';
+
+export type Props = {
+  post: PostToRender;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    comments,
+    userId,
+    body,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        <p>
+          {' Posted by  '}
+          <UserInfo
+            key={userId}
+            user={post?.user}
+          />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+      {comments.length
+        ? (
+          <CommentList comments={comments} />
+        )
+        : (
+          <b data-cy="NoCommentsMessage">
+            No comments yet
+          </b>
+        )}
+    </div>
+  );
+};
