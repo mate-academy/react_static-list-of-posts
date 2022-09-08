2155c3eeef9a608e095b368b9ff9933c04d2fb74
roma-shafihullin <lanister0789@gmail.com>
2022-09-08 23:22:30 +0300

    types, function, post added

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,34 @@ import React from 'react';
 
 import './App.scss';
 
-// import postsFromServer from './api/posts';
-// import commentsFromServer from './api/comments';
-// import usersFromServer from './api/users';
+import postsFromServer from './api/posts';
+import commentsFromServer from './api/comments';
+import usersFromServer from './api/users';
+
+import { User } from './types/User';
+import { Post } from './types/Post';
+import { Comment } from './types/Comment';
+
+function getUserById(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+function getCommentsById(postId: number): Comment[] | null {
+  const comments = commentsFromServer
+    .filter(comment => comment.postId === postId);
+
+  return comments.length !== 0
+    ? comments
+    : null;
+}
+
+const posts: Post[] = postsFromServer.map(post => ({
+  ...post,
+  user: getUserById(post.userId),
+  comments: getCommentsById(post.id),
+}));
 
 export const App: React.FC = () => (
   <section className="App">
