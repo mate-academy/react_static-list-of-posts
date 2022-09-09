a28b3d038dc827278844a25913d3667b373b4354
Yurii Kozlov <yurka22870@gmail.com>
2022-09-09 21:20:17 +0300

    list of posts was created

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = {
+  comments: Comment[]
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  const noComment = <b data-cy="NoCommentsMessage">No comments yet</b>;
+  const comment = comments
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    .map((comment) => <CommentInfo key={comment.id} comment={comment} />);
+
+  return (
+    <div className="CommentList">
+      {comments.length > 0 ? comment : noComment}
+    </div>
+  );
+};
