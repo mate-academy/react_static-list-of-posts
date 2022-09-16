e7663e035721df4e34a7fd565d6f08c32b053070
Oles Partyka <oles.partyka.27@gmail.com>
2022-09-16 19:27:36 +0200

    Solution

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import { PostToRender } from '../../types/Post';
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  posts: PostToRender[];
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+  </div>
 );
