13829297ab9cf81f5d2b6d3837062c090d5c6364
Denis Koval <denisdkoval@gmail.com>
2022-09-16 13:56:32 +0300

    solution7

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -11,7 +11,7 @@ export const PostInfo: React.FC<Props> = ({ post }) => (
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{post.title}</h3>
       {' Posted by  '}
-      {post.user && <UserInfo user={post.user} />}
+      <UserInfo user={post.user} />
     </div>
     <p className="PostInfo__body">
       {post.body}
