48a04b1b96201b821d8573a0d37c3e3195cfc542
Bohdan Tron <ikwiczzts@gmail.com>
2022-10-04 12:55:34 +0300

    solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
-);
+import { Comment } from '../../types/Comment';
+
+type Props = {
+  comment: Comment,
+};
+
+export const CommentInfo: React.FC<Props> = ({ comment }, { key }) => {
+  const {
+    name,
+    email,
+    body,
+  } = comment;
+
+  return (
+    <>
+      <div
+        className="CommentInfo"
+        key={key}
+      >
+        <div className="CommentInfo__title">
+          <strong className="CommentInfo__name">{name}</strong>
+
+          {' by '}
+
+          <a
+            className="CommentInfo__email"
+            href={`mailto:${email}`}
+          >
+            {email}
+          </a>
+        </div>
+
+        <div className="CommentInfo__body">
+          {body}
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import './UserInfo.scss';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
-);
+import { User } from '../../types/User';
+
+type Props = {
+  user: User | null;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  return (
+    <>
+      <a
+        className="UserInfo"
+        href={`mailto:${user?.email}`}
+      >
+        {user?.name}
+      </a>
+    </>
+  );
+};
