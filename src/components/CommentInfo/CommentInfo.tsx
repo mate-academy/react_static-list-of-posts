d275fb873167d011a49d9ded9aac35dea843824b
naveed-shoukat <shoukat.naveed@gmail.com>
2022-10-06 13:56:42 +0300

    solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { UserComment } from '../../types/UserComment';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+type CommentInfoProps = {
+  comment: UserComment
+};
+
+export const CommentInfo: React.FC<CommentInfoProps> = ({ comment }) => (
+  <div key={comment.id} className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">
+        {comment.name}
+      </strong>
+
+      {' by '}
+
+      <a
+        className="CommentInfo__email"
+        href={`mailto:${comment.email}`}
+      >
+        {comment.email}
+      </a>
+    </div>
+
+    <div className="CommentInfo__body">
+      {comment.body}
+    </div>
+  </div>
 );
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { PostInfo } from '../PostInfo';
+import { Post } from '../../types/Post';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type PostListProps = {
+  posts: Post[],
+};
+
+export const PostList: React.FC<PostListProps> = ({ posts }) => (
+  <div className="PostList">
+    {
+      posts.map((post) => (
+        <PostInfo
+          key={post.id}
+          post={post}
+        />
+      ))
+    }
+  </div>
 );
