0865da472f2bebb64ba59f7a911852419d03dddd
Killdex <kdcompany2018@gmail.com>
2022-10-11 19:02:56 +0300

    list of post done

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import { Post } from '../../types/Post';
+
+type Props = {
+  post: Post,
+};
+
+const NoCommentsYet = () => (
+  <>
+    <hr />
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  </>
 );
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    userId,
+    title,
+    body,
+    user,
+    comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        <p>
+          {' Posted by  '}
+
+          {user && <UserInfo user={user} key={userId} />}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      {
+        (comments.length > 0 && <CommentList comments={comments} />)
+        || <NoCommentsYet />
+      }
+
+    </div>
+  );
+};
