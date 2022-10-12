4da71d96f3690e33078033ce47394702a96aeffc
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-10-12 15:32:09 +0300

    static list of posts solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import './PostInfo.scss';
+import { PostData } from '../../react-app-env';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+interface Props {
+  post: PostData;
+}
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <UserInfo user={post.user} />
+
+    </div>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    <hr />
+
+    {
+      post.comments?.length === 0
+        ? <b data-cy="NoCommentsMessage">No comments yet</b>
+        : <CommentList comments={post.comments} />
+    }
+  </div>
 );
