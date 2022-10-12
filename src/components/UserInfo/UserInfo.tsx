8d27bac112d35f1c516bec020ac2e484ead213bf
Kamil Witek <kamilprogramuje@gmail.com>
2022-10-12 17:06:33 +0200

    modified return inside UserInfo

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -11,5 +11,9 @@ type Props = {
 export const UserInfo: React.FC<Props> = ({ user }) => {
   const { name, email } = user;
 
-  return <a href={`mailto:${email}`} className="UserInfo">{name}</a>;
+  return (
+    <a href={`mailto:${email}`} className="UserInfo">
+      {name}
+    </a>
+  );
 };
