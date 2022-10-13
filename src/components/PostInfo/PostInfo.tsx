dba125c31ee4228c2a6dc4113a53fbdb9fab0af9
Vlad Harkusha <sivari888@gmail.com>
2022-10-13 17:43:35 +0300

    add conditional rendering to UserInfo and destructive assignment to props

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -9,28 +9,36 @@ type Props = {
   post: PreparedPost;
 };
 
-export const PostInfo: React.FC<Props> = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">
-        {post.title}
-      </h3>
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    user,
+    body,
+    comments,
+  } = post;
 
-      <p>
-        {' Posted by  '}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
 
-        <UserInfo user={post.user} />
+        <p>
+          {' Posted by  '}
+          {user && <UserInfo user={user} />}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
       </p>
+
+      <hr />
+
+      {post.comments.length > 0
+        ? <CommentList comments={comments} />
+        : <b data-cy="NoCommentsMessage">No comments yet</b>}
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-
-    <hr />
-
-    {post.comments.length > 0
-      ? <CommentList comments={post.comments} />
-      : <b data-cy="NoCommentsMessage">No comments yet</b>}
-  </div>
-);
+  );
+}
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -4,11 +4,15 @@ import { User } from '../../types/User';
 import './UserInfo.scss';
 
 type Props = {
-  user: User | null;
+  user: User;
 };
 
-export const UserInfo: React.FC<Props> = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user?.email}`}>
-    {user?.name}
-  </a>
-);
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { name, email } = user;
+
+  return user && (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
