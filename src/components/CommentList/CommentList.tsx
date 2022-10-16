e5ad38c8bb02f968c7c9d1eab08da19fd4c28cca
Roman Starovoit <rstarovoit@gmail.com>
2022-10-16 18:19:57 +0300

    Solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../../react-app-env';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  comments: Comment[],
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <div className="CommentList">
+    {
+      comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))
+    }
+  </div>
 );
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import { PostInfo } from '../PostInfo';
+
+import { User, Comment, Post } from '../../react-app-env';
+
+import postsFromServer from '../../api/posts';
+import commentsFromServer from '../../api/comments';
+import usersFromServer from '../../api/users';
+
+function getUser(userId: number): User | null {
+  const userToFind = usersFromServer.find(user => user.id === userId);
+
+  return userToFind || null;
+}
+
+function getComments(postId: number): Comment[] {
+  const comments = commentsFromServer
+    .filter(comment => postId === comment.postId);
+
+  return comments;
+}
+
+const posts: Post[] = postsFromServer.map(post => ({
+  ...post,
+  user: getUser(post.userId),
+  comments: getComments(post.id),
+}));
 
 export const PostList: React.FC = () => (
-  <>Put the list here</>
+  <div className="PostList">
+
+    {
+      posts.map(post => (
+        <PostInfo key={post.id} post={post} />
+      ))
+    }
+  </div>
 );
