892405c8182e373847760a0150e39ed23a7aa9e8
Andrii Zhabynets <a.zhabynets03@gmail.com>
2022-10-16 15:12:10 +0300

    create static list of posts

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,39 @@ import React from 'react';
 
 import './App.scss';
 
-// import postsFromServer from './api/posts';
-// import commentsFromServer from './api/comments';
-// import usersFromServer from './api/users';
+import postsFromServer from './api/posts';
+import commentsFromServer from './api/comments';
+import usersFromServer from './api/users';
+import { PostList } from './components/PostList';
+import { Post } from './types/Post';
+import { User } from './types/User';
+import { Comment } from './types/Comment';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+function getComments(id: number): Comment[] {
+  const foundComments = commentsFromServer
+    .filter(comment => comment.postId === id);
+
+  return foundComments;
+}
+
+export const posts: Post[] = postsFromServer.map(post => ({
+  ...post,
+  user: getUser(post.userId),
+  comments: getComments(post.id),
+}));
 
 export const App: React.FC = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <div className="PostList">
+    <PostList posts={posts} />
+    {/* <div className="PostList">
       <div className="PostInfo">
         <div className="PostInfo__header">
           <h3 className="PostInfo__title">qui est esse</h3>
@@ -104,6 +128,6 @@ export const App: React.FC = () => (
           </div>
         </div>
       </div>
-    </div>
+    </div> */}
   </section>
 );
