3b1da2bc1aae7226c35a38dd07b1397ae10a974c
Denys Topchyi <miliarder97@gmail.com>
2022-10-19 17:13:59 +0300

    react_static-posts

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Post } from '../../Types/Post';
+import { PostInfo } from '../PostInfo/PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  posts: Post[];
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => <PostInfo key={post.id} post={post} />)}
+  </div>
 );
