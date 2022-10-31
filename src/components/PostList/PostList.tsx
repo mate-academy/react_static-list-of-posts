1c9d84127b08f7f28203a20cde8136f47b3aa447
omerfarukbalik <merbalk2147@gmail.com>
2022-10-31 09:42:47 +0300

    solution

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
 
+import { Post } from '../../types/Post';
+import { User } from '../../types/User';
+import { Comment } from '../../types/Comment';
+
+import { PostInfo } from '../PostInfo';
+
+import postsFromServer from '../../api/posts';
+import commentsFromServer from '../../api/comments';
+import usersFromServer from '../../api/users';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+function getComment(postId: number): Comment[] {
+  return commentsFromServer
+    .filter(post => post.postId === postId);
+}
+
+export const posts: Post[] = postsFromServer.map(post => ({
+  ...post,
+  user: getUser(post.userId),
+  comments: getComment(post.id),
+}));
+
 export const PostList: React.FC = () => (
-  <>Put the list here</>
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+  </div>
 );
