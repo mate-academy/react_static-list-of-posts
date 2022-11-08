8fc3fe7bf91a49167c80587898ba7ba4bf004dd9
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-11-09 01:04:56 +0200

    React static list of posts solution

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import { PostInfo } from '../PostInfo';
+import { Post } from '../../types/post';
+import { User } from '../../types/user';
+import postsFromServer from '../../api/posts';
+import usersFromServer from '../../api/users';
+import commentsFromServer from '../../api/comments';
+import { Comment } from '../../types/comment';
+
+function getUser(userId: number): User | null {
+  const relatedUser = usersFromServer.find(user => user.id === userId);
+
+  return relatedUser || null;
+}
+
+function getComments(postId: number): Comment[] {
+  const relatedComment = commentsFromServer.filter(
+    comment => comment.postId === postId,
+  );
+
+  return relatedComment;
+}
+
+export const posts: Post[] = postsFromServer.map(post => ({
+  ...post,
+  user: getUser(post.userId),
+  comments: getComments(post.id),
+}));
 
 export const PostList: React.FC = () => (
-  <>Put the list here</>
+  <>
+    <div className="PostList">
+      {posts.map(post => (
+        <PostInfo post={post} />
+      ))}
+    </div>
+  </>
 );
