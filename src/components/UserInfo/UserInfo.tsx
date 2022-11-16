73db2c633080f2883c94ec5992ecd46a0c44f8f7
Illia Skladnik <scladnik@gmail.com>
2022-11-16 18:54:51 +0200

    Fix

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -5,11 +5,15 @@ type Props = {
   user: User;
 };
 
-export const UserInfo: React.FC<Props> = ({ user }) => (
-  <p>
-    {' Posted by  '}
-    <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.name}
-    </a>
-  </p>
-);
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { email, name } = user;
+
+  return (
+    <p>
+      Posted by
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
+      </a>
+    </p>
+  );
+};
