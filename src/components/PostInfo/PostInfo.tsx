1527c384905f4641031fda5b1c1436b2948eba96
Ivan Nevmerzhytskyi <110294770+Ivan-Nevmerzhytskyi@users.noreply.github.com>
2022-11-16 12:48:56 +0200

    Update PostInfo.tsx

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -8,27 +8,40 @@ type Props = {
   post: Post;
 };
 
-export const PostInfo: React.FC<Props> = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    body,
+    user,
+    comments,
+  } = post;
 
-      <p>
-        {' Posted by  '}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
 
-        {post.user && <UserInfo user={post.user} />}
+        <p>
+          {' Posted by  '}
+
+          {user && <UserInfo user={user} />}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
       </p>
+
+      {!comments.length
+        ? (
+          <>
+            <hr />
+            <b data-cy="NoCommentsMessage">No comments yet</b>
+          </>
+        )
+        : <CommentList comments={comments} />}
     </div>
-
-    <p className="PostInfo__body">{post.body}</p>
-
-    {!post.comments.length
-      ? (
-        <>
-          <hr />
-          <b data-cy="NoCommentsMessage">No comments yet</b>
-        </>
-      )
-      : <CommentList comments={post.comments} />}
-  </div>
-);
+  );
+};
