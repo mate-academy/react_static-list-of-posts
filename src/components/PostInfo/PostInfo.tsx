9ee7c5c208ae9dd2508a8621313fc98c4c01b4f5
Stepan Prokopyev <stepkapro2002@gmail.com>
2022-11-17 10:34:18 +0200

    Beta-version

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,61 @@
 import React from 'react';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import { Post } from '../../types/Post';
+
+import './PostInfo.scss';
+
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+import commentsFromServer from '../../api/comments';
+import usersFromServer from '../../api/users';
+
+type Props = {
+  post: Post,
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    userId,
+    id,
+    title,
+    body,
+  } = post;
+
+  const neededComments = commentsFromServer.filter(comment => (
+    comment.postId === id
+  ));
+
+  const neededUser = usersFromServer.find(user => (
+    user.id === userId
+  ));
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={neededUser} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+
+      {
+        neededComments.length > 0
+          ? <CommentList comments={neededComments} />
+          : <b data-cy="NoCommentsMessage">No comments yet</b>
+      }
+
+    </div>
+  );
+};
