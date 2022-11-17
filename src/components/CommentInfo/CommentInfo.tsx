57e84ac5d0bc6632f21fb37fac2ec0f95931c84f
Artem66 <melodpi@gmail.com>
2022-11-17 07:33:43 +0000

    splited app, implemented component

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
-);
+type Props = {
+  comment: Comment;
+};
+
+export const CommentInfo: React.FC<Props> = ({ comment }) => {
+  const { name, email, body } = comment;
+
+  return (
+    <div className="CommentInfo comment">
+      <div className="content">
+        <a href={`mailto:${email}`} className="CommentInfo__name author">{name}</a>
+        <div className="metadata">
+          <a href={`mailto:${email}`} className="CommentInfo__email date">{email}</a>
+        </div>
+        <p className="CommentInfo__body">{body}</p>
+      </div>
+    </div>
+  );
+};
