611dd2a97ecea8f93e896b9332d22580dc15711b
Max Fedak <max.fedak@gmail.com>
2022-11-17 21:04:53 +0200

    solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import { User } from '../../types/user';
+import { Comment } from '../../types/comment';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
-);
+type Props = {
+  user: User | Comment,
+  dataType: 'post' | 'comment',
+};
+
+export const UserInfo: React.FC<Props> = ({ user, dataType }) => {
+  const { email, name } = user;
+  const propperText = dataType === 'comment'
+    ? email
+    : name;
+
+  const propperClass = dataType === 'comment'
+    ? 'CommentInfo__email'
+    : 'UserInfo';
+
+  return (
+    <a className={propperClass} href={`mailto:${email}`}>
+      {propperText}
+    </a>
+  );
+};
