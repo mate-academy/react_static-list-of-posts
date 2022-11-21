9ae0beaf650ef25b090cbfc92f53845222388862
viktor-kotik <vikuso1996@gmail.com>
2022-11-21 13:35:36 +0200

    Solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,32 +1,5 @@
 import React from 'react';
 
-type Props = {
-  name: string;
-  email: string;
-  body: string
-};
-
-export const CommentInfo: React.FC<Props> = ({
-  name, email, body,
-}) => (
-  <>
-    <div className="CommentInfo">
-      <div className="CommentInfo__title">
-        <strong className="CommentInfo__name">{name}</strong>
-
-        {' by '}
-
-        <a
-          className="CommentInfo__email"
-          href={`mailto:${email}`}
-        >
-          {email}
-        </a>
-      </div>
-
-      <div className="CommentInfo__body">
-        {body}
-      </div>
-    </div>
-  </>
+export const CommentInfo: React.FC = () => (
+  <></>
 );
