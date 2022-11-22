6e354a25f3422f701ea8a2bc17cb3fa2424af87c
Bohdan Stepanets <number1xy@gmail.com>
2022-11-22 12:58:35 +0200

    Make List of posts v1.0

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Comment } from '../../types/comment';
+import { CommentInfo } from '../CommentInfo';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+import './CommentList.scss';
+
+type Props = {
+  comments: Comment[]
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  return (
+    <div className="CommentList">
+      {
+        comments.map(comment => (
+          <CommentInfo comment={comment} key={comment.id} />
+        ))
+      }
+    </div>
+  );
+};
