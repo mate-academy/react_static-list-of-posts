f0c59779bd89a1b4bbf324042754c64df18e46b4
Serhii Yunnikov <yunnikov@icloud.com>
2022-11-30 09:16:22 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,38 @@ import React from 'react';
 import './App.scss';
 
 import postsFromServer from './api/posts';
-// import commentsFromServer from './api/comments';
-// import usersFromServer from './api/users';
+import commentsFromServer from './api/comments';
+import usersFromServer from './api/users';
 
-// import { Comment } from './types/Comment';
-// import { User } from './types/User';
-// import { Post } from './types/Post';
+import { Comment } from './types/Comment';
+import { User } from './types/User';
+import { Post } from './types/Post';
 
 import { PostList } from './components/PostList';
 
-// function getComments(id: number): Comment[] | null {
-//   const postComments
-//   = commentsFromServer.filter(comment => id === comment.postId);
+function getComments(id: number): Comment[] | null {
+  const postComments
+  = commentsFromServer.filter(comment => id === comment.postId);
 
-//   return postComments || null;
-// }
+  return postComments || null;
+}
 
-// function getUser(id: number): User | null {
-//   const user = usersFromServer.find(userPost => id === userPost.id);
+function getUser(id: number): User | null {
+  const user = usersFromServer.find(userPost => id === userPost.id);
 
-//   return user || null;
-// }
+  return user || null;
+}
 
-// export const postList: Post[] = postsFromServer.map(post => ({
-//   ...post,
-//   comments: getComments(post.id),
-//   user: getUser(post.userId),
-// }));
+export const postList: Post[] = postsFromServer.map(post => ({
+  ...post,
+  comments: getComments(post.id),
+  user: getUser(post.userId),
+}));
 
 export const App: React.FC = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <PostList postList={postsFromServer} />
+    <PostList postList={postList} />
   </section>
 );
