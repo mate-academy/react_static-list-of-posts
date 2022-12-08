718af1eb8ee57feb2dab2f58920fba677881a81a
Demut <uddarendeli1@gmail.com>
2022-12-08 12:30:21 +0300

    Solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import { Post } from '../../types/post';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+export const PostInfo: React.FC<Post> = ({
+  title,
+  body,
+  user,
+  comments,
+}: Post) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{title}</h3>
+
+      <p>
+        {' Posted by  '}
+        <UserInfo {...user} />
+      </p>
+    </div>
+
+    <p className="PostInfo__body">
+      {body}
+    </p>
+
+    <hr />
+
+    { comments.length > 0
+      ? <CommentList comments={comments} />
+      : <b data-cy="NoCommentsMessage">No comments yet</b>}
+  </div>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import { User } from '../../types/user';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+export const UserInfo: React.FC<User> = ({ name, email }: User) => (
+  <a
+    className="UserInfo"
+    href={email}
+  >
+    {name}
+  </a>
 );
