63cc49df3e12effbde377574466d4eecb778d720
Umut Deniz Darendeli <108900524+UmizDemud@users.noreply.github.com>
2022-12-10 22:31:45 +0300

    Update src/components/PostInfo/PostInfo.tsx
    Co-authored-by: Misha Grinko <glukym@gmail.com>

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -31,9 +31,11 @@ export const PostInfo: React.FC<Props> = ({ post }) => {
 
       <hr />
 
-      { comments.length > 0
-        ? <CommentList comments={comments} />
-        : <b data-cy="NoCommentsMessage">No comments yet</b>}
+      { comments.length > 0 ? (
+        <CommentList comments={comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
     </div>
   );
 };
