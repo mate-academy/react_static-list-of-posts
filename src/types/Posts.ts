8ead5d7e2806b2c623a23b187719534ccb4e0baf
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-12-19 20:05:34 +0800

    fixed

--- src/types/Posts.ts
+++ src/types/Posts.ts
@@ -2,9 +2,10 @@ import { Users } from './Users';
 import { Comment } from './Comment';
 
 export interface Posts {
+  userId:number,
   body: string,
   title: string,
   id: number,
-  user: Users[];
+  user: Users | null;
   comments: Comment[];
 }
