3447f4886647328318145b18a46265ce60bcad7e
Anastasiia (Nelly) <116572743+anastasiia-roiek@users.noreply.github.com>
2022-12-27 22:33:03 +0200

    list of posts

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import { Post } from '../../types/post';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import './PostInfo.scss';
+
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    user,
+    body,
+    comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+        <p>
+          {' Posted by  '}
+          {user && (
+            <UserInfo user={user} />
+          )}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{body}</p>
+
+      {
+        comments.length
+          ? <CommentList comments={comments} />
+          : (
+            <>
+              <hr />
+              <b data-cy="NoCommentsMessage">No comments yet</b>
+            </>
+          )
+      }
+    </div>
+  );
+};
