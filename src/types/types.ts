e37cf7be04448503fd884b3780c33de4184d986f
Andrii Petrinko <apetrinko@gmail.com>
2022-12-28 14:50:03 +0200

    solution

---
+++ src/types/types.ts
@@ -0,0 +1,23 @@
+export interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+};
+
+export interface Comment {
+  postId: number,
+  id: number,
+  name: string,
+  email: string,
+  body: string,
+}
+
+export interface Post {
+  id: number,
+  userId: number,
+  title: string,
+  body: string,
+  comments: Comment[] | null,
+  user: User | null,
+}
