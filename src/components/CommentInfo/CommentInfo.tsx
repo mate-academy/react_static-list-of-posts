d1d9b2fbe203d7aa5979839d09e12592e08570ca
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2022-12-28 21:24:09 +0200

    solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -5,18 +5,26 @@ type Props = {
   comment: Comment;
 };
 
-export const CommentInfo:React.FC<Props> = ({ comment }) => (
-  <div className="CommentInfo">
-    <div className="CommentInfo__title">
-      <strong className="CommentInfo__name">{comment.name}</strong>
-      {' by '}
-      <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
-        {comment.email}
-      </a>
-    </div>
+export const CommentInfo:React.FC<Props> = ({ comment }) => {
+  const {
+    name,
+    email,
+    body,
+  } = comment;
 
-    <div className="CommentInfo__body">
-      {comment.body}
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{name}</strong>
+        {' by '}
+        <a className="CommentInfo__email" href={`mailto:${email}`}>
+          {email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">
+        {body}
+      </div>
     </div>
-  </div>
-);
+  );
+};
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -9,30 +9,37 @@ type Props = {
 };
 
 export const PostInfo:React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    user,
+    body,
+    comments,
+  } = post;
+
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+        <h3 className="PostInfo__title">{title}</h3>
         <p>
-          {' Posted by  '}
-          {post.user && (
-            <UserInfo user={post.user} />
+          {' Posted by '}
+          {user && (
+            <UserInfo user={user} />
           )}
         </p>
       </div>
 
       <p className="PostInfo__body">
-        {post.body}
+        {body}
       </p>
 
-      {post.comments.length !== 0 ? (
-        <CommentList comments={post.comments} />
-      ) : (
-        <>
-          <hr />
-          <b data-cy="NoCommentsMessage">No comments yet</b>
-        </>
-      )}
+      {comments.length
+        ? <CommentList comments={comments} />
+        : (
+          <>
+            <hr />
+            <b data-cy="NoCommentsMessage">No comments yet</b>
+          </>
+        )}
     </div>
   );
 };
