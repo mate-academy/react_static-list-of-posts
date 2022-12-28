df4310fc77bd8e15f05a714dc795cc90c6656c92
Ivan Melnychuk <jacksmilereacher@gmail.com>
2022-12-28 12:04:39 +0200

    divides App.scss into separate files

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Post } from '../../types/Post';
 import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
+import './PostInfo.scss';
 
 type Props = {
   post: Post;
