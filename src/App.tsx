4707855cc6dbf6f468d2ea8fc442aafc4e862a60
Kris Planka <kristyaplanka@gmail.com>
2022-12-28 11:38:55 +0200

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,26 @@ import './App.scss';
 import postsFromServer from './api/posts';
 import commentsFromServer from './api/comments';
 import usersFromServer from './api/users';
+import { User } from './types/User';
+import { Comment } from './types/Comment';
 import { PreparedPost } from './types/PreparedPost';
 import { PostList } from './components/PostList/PostList';
 
-const posts: PreparedPost[] = postsFromServer
-  .map(post => {
-    const foundUser = usersFromServer.find(user => post.id === user.id);
-    const foundComments = commentsFromServer
-      .filter(comment => post.id === comment.postId);
+function getUser(id: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === id);
 
-    return {
-      ...post,
-      user: foundUser || null,
-      comments: foundComments,
-    };
-  });
+  return foundUser || null;
+}
+
+function getComments(id: number): Comment[] {
+  return commentsFromServer.filter(comment => comment.postId === id);
+}
+
+const posts: PreparedPost[] = postsFromServer.map(post => ({
+  ...post,
+  user: getUser(post.userId),
+  comments: getComments(post.id),
+}));
 
 export const App: React.FC = () => {
   return (
