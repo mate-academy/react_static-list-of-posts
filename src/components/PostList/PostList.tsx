20cee22e1906a6bf95592b8c65b787b060984e8e
Saleh BUD <saleh.7734@gmail.com>
2022-12-28 14:20:39 +0200

    Complete

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Post } from '../../react-app-env';
+import { PostInfo } from '../PostInfo';
+import './PostList.scss';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = {
+  posts: Post[];
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => {
+  return (
+    <ul className="PostList">
+      {posts.map((post: Post) => (
+        <li key={post.id}>
+          <PostInfo
+            post={post}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,25 @@
 /// <reference types="react-scripts" />
+
+export interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
+  user: User | null,
+  comments: Comment[],
+}
+
+export interface Comment {
+  postId: number,
+  id: number,
+  name: string,
+  email: string,
+  body: string
+}
+
+export interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+}
