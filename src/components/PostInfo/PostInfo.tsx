09c4fd43cc277a77fd5411579be2dcb21e59ec0a
Kyrylo Haiduk <kirillhaiduk0@gmail.com>
2022-12-28 14:38:57 +0200

    fixed ternary operator

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -33,16 +33,14 @@ export const PostInfo: React.FC<Props> = ({ post }) => {
         {body}
       </p>
 
-      {
-        comments.length !== 0
-          ? <CommentList comments={comments} />
-          : (
-            <>
-              <hr />
-              <b data-cy="NoCommentsMessage">No comments yet</b>
-            </>
-          )
-      }
+      {comments.length !== 0
+        ? <CommentList comments={comments} />
+        : (
+          <>
+            <hr />
+            <b data-cy="NoCommentsMessage">No comments yet</b>
+          </>
+        )}
     </div>
   );
 };
