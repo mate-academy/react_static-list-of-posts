f4b8713e337a26823372683bbc98ec639b532656
kreisberg <anastasia.kreisberg@yahoo.com>
2022-12-29 01:00:49 +0200

    post list

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
-);
+import { Comments } from '../../types/Comments';
+
+type Props = {
+  comment: Comments;
+};
+
+export const CommentInfo: React.FC<Props> = ({ comment }) => {
+  const {
+    name,
+    email,
+    body,
+  } = comment;
+
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">
+          {name}
+        </strong>
+
+        {' by '}
+
+        <a
+          className="CommentInfo__email"
+          href={`mailto:${email}`}
+        >
+          {email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">
+        {body}
+      </div>
+    </div>
+  );
+};
---
+++ src/types/Posts.ts
@@ -0,0 +1,11 @@
+import { Users } from './Users';
+import { Comments } from './Comments';
+
+export interface Posts {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+  comments: Comments[] | null;
+  user: Users | null;
+}
