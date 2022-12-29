26f3a437cb1003a046df076f9e1e255cf518dded
Dariia Skoryk <skorykdariia@gmail.com>
2022-12-29 18:17:43 +0200

    changes were added

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -10,24 +10,31 @@ type Props = {
 };
 
 export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    user,
+    body,
+    comments,
+  } = post;
+
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+        <h3 className="PostInfo__title">{title}</h3>
 
         <p>
           {' Posted by  '}
-          {post.user && (<UserInfo user={post.user} />)}
+          {user && (<UserInfo user={user} />)}
         </p>
       </div>
 
-      <p className="PostInfo__body">{post.body}</p>
+      <p className="PostInfo__body">{body}</p>
 
       <hr />
 
-      {post.comments.length === 0
+      {comments.length === 0
         ? <b data-cy="NoCommentsMessage">No comments yet</b>
-        : <CommentList comments={post.comments} />}
+        : <CommentList comments={comments} />}
     </div>
   );
 };
