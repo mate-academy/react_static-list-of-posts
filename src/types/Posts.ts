73eebadbfc8f82da44096515ffb35a43edb8ee10
Levente Krotos <krotos.levi@gmail.com>
2022-12-30 13:18:32 +0100

    solution

---
+++ src/types/Posts.ts
@@ -0,0 +1,11 @@
+import { Comment } from './Comments';
+import { User } from './Users';
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+  user: User | null;
+  comments: Comment[] | null;
+}
