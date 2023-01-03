e47901d214cd8fe8083e10d9e864d9e905e36e67
Gürsel Çakır <hardmatters@outlook.com>
2023-01-03 19:09:21 +0300

    key added

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -10,7 +10,10 @@ export const PostList: React.FC<Props> = ({ posts }) => (
   <div className="PostList">
 
     {posts.map(post => (
-      <PostInfo post={post} />
+      <PostInfo
+        post={post}
+        key={post.id}
+      />
     ))}
   </div>
 );
