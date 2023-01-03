309768e4fff53c6f6b8cf77da85e18ee63a1d4f2
Daryna Zinchenko <zinchenko.daryna@gmail.com>
2023-01-03 13:10:39 +0100

    destructuring added

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -8,34 +8,43 @@ type Props = {
   post: Post;
 };
 
-export const PostInfo: React.FC<Props> = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">
-        {post.title}
-      </h3>
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    user,
+    body,
+    comments,
+  } = post;
 
-      <p>
-        {' Posted by  '}
-        {post.user && (
-          <UserInfo user={post.user} />
-        )}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        <p>
+          {' Posted by  '}
+          {user && (
+            <UserInfo user={user} />
+          )}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
       </p>
+
+      {comments !== null && comments.length > 0
+        ? (<CommentList comments={comments} />)
+        : (
+          <>
+            <hr />
+
+            <b data-cy="NoCommentsMessage">No comments yet</b>
+          </>
+        )}
+
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-
-    {post.comments !== null && post.comments.length > 0
-      ? (<CommentList comments={post.comments} />)
-      : (
-        <>
-          <hr />
-
-          <b data-cy="NoCommentsMessage">No comments yet</b>
-        </>
-      )}
-
-  </div>
-);
+  );
+};
