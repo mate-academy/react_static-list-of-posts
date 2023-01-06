df96282574820e9d57a2cdbe103156fbfa602a00
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2023-01-06 13:49:59 +0200

    fragment deleted

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -8,12 +8,10 @@ type Props = {
 
 export const PostList: React.FC<Props> = ({ posts }) => {
   return (
-    <>
-      <div className="PostList">
-        {posts.map(
-          post => <PostInfo key={post.id} post={post} />,
-        )}
-      </div>
-    </>
+    <div className="PostList">
+      {posts.map(
+        post => <PostInfo key={post.id} post={post} />,
+      )}
+    </div>
   );
 };
