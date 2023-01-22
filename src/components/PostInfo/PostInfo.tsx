b1c7715cfbdcbd3090c3f367b9a77b49ce7545b1
Andrii Medintsev <medintsev.andrew@gmail.com>
2023-01-23 00:55:36 +0200

    Static list of posts was implemented

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,47 @@
 import React from 'react';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import './PostInfo.scss';
+
+import { Post } from '../../types/post';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  post: Post
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    body,
+    user,
+    comments,
+  } = post;
+
+  const noComments = <b data-cy="NoCommentsMessage">No comments yet</b>;
+  const yesComments = (
+    <div className="CommentList">
+      {comments && <CommentList comments={comments} />}
+    </div>
+  );
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        <p>
+          {user && <UserInfo user={user} />}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      {comments && comments.length > 0 ? yesComments : noComments }
+    </div>
+  );
+};
---
+++ src/types/post.tsx
@@ -0,0 +1,11 @@
+import { Comment } from './comment';
+import { User } from './user';
+
+export interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
+  user: User | null,
+  comments: Comment[] | null,
+}
