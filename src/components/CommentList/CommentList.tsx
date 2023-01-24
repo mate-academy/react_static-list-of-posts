34cfa1ad6c3c3057fcacc528359de2c1b2dd1e38
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2023-01-24 23:23:29 +0200

    was moved markup and styles to corresponding components

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import './CommentList.scss';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
+
+type Props = {
+  comments: Comment[];
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  if (comments.length === 0) {
+    return (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    );
+  }
+
+  return (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,91 @@
 import React from 'react';
+import './PostInfo.scss';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import { Post } from '../../types/Post';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    user,
+    comments,
+    body,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        <p>
+          {' Posted by  '}
+
+          {user && (
+            <UserInfo user={user} />
+          )}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <CommentList comments={comments} />
+
+      {/* <div className="CommentList">
+        <div className="CommentInfo">
+          <div className="CommentInfo__title">
+            <strong className="CommentInfo__name">pariatur omnis in</strong>
+
+            {' by '}
+
+            <a
+              className="CommentInfo__email"
+              href="mailto:Telly_Lynch@karl.co.uk"
+            >
+              Telly_Lynch@karl.co.uk
+            </a>
+          </div>
+
+          <div className="CommentInfo__body">
+            dolorum voluptas laboriosam quisquam ab totam beatae et aut
+            aliquid optio assumenda voluptas velit itaque quidem voluptatem
+            tempore cupiditate in itaque sit molestiae minus dolores magni
+          </div>
+        </div>
+
+        <div className="CommentInfo">
+          <div className="CommentInfo__title">
+            <strong className="CommentInfo__name">
+              odio adipisci rerum aut animi
+            </strong>
+
+            {' by '}
+
+            <a
+              className="CommentInfo__email"
+              href="mailto:Nikita@garfield.biz"
+            >
+              Nikita@garfield.biz
+            </a>
+          </div>
+
+          <div className="CommentInfo__body">
+            quia molestiae reprehenderit quasi aspernatur aut expedita
+            occaecati aliquam eveniet laudantium omnis quibusdam delectus
+            saepe quia accusamus maiores nam est cum et ducimus et vero
+            voluptates excepturi deleniti ratione
+          </div>
+        </div>
+      </div> */}
+    </div>
+
+  );
+};
