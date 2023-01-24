d5ef15d69d36e8b564bf1c1c13bf186340e14f24
Iryna Pysanko <malva37@gmail.com>
2023-01-24 08:29:32 +0200

    deleted unnessesary     name,

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -13,25 +13,23 @@ export const CommentInfo: React.FC<Props> = ({ comment }) => {
   } = comment;
 
   return (
-    <>
-      <div className="CommentList">
-        <div className="CommentInfo">
-          <div className="CommentInfo__title">
-            <strong className="CommentInfo__name">{name}</strong>
-            {' by '}
-            <a
-              className="CommentInfo__email"
-              href={`mailto:${email}`}
-            >
-              {email}
-            </a>
-          </div>
+    <div className="CommentList">
+      <div className="CommentInfo">
+        <div className="CommentInfo__title">
+          <strong className="CommentInfo__name">{name}</strong>
+          {' by '}
+          <a
+            className="CommentInfo__email"
+            href={`mailto:${email}`}
+          >
+            {email}
+          </a>
+        </div>
 
-          <div className="CommentInfo__body">
-            {body}
-          </div>
+        <div className="CommentInfo__body">
+          {body}
         </div>
       </div>
-    </>
+    </div>
   );
 };
