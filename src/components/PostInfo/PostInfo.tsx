218d4a70e851f97eda19545157ac95f599c4ace9
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2023-01-25 15:05:47 +0200

    comment lines have been removed

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -38,54 +38,6 @@ export const PostInfo: React.FC<Props> = ({ post }) => {
       </p>
 
       <CommentList comments={comments} />
-
-      {/* <div className="CommentList">
-        <div className="CommentInfo">
-          <div className="CommentInfo__title">
-            <strong className="CommentInfo__name">pariatur omnis in</strong>
-
-            {' by '}
-
-            <a
-              className="CommentInfo__email"
-              href="mailto:Telly_Lynch@karl.co.uk"
-            >
-              Telly_Lynch@karl.co.uk
-            </a>
-          </div>
-
-          <div className="CommentInfo__body">
-            dolorum voluptas laboriosam quisquam ab totam beatae et aut
-            aliquid optio assumenda voluptas velit itaque quidem voluptatem
-            tempore cupiditate in itaque sit molestiae minus dolores magni
-          </div>
-        </div>
-
-        <div className="CommentInfo">
-          <div className="CommentInfo__title">
-            <strong className="CommentInfo__name">
-              odio adipisci rerum aut animi
-            </strong>
-
-            {' by '}
-
-            <a
-              className="CommentInfo__email"
-              href="mailto:Nikita@garfield.biz"
-            >
-              Nikita@garfield.biz
-            </a>
-          </div>
-
-          <div className="CommentInfo__body">
-            quia molestiae reprehenderit quasi aspernatur aut expedita
-            occaecati aliquam eveniet laudantium omnis quibusdam delectus
-            saepe quia accusamus maiores nam est cum et ducimus et vero
-            voluptates excepturi deleniti ratione
-          </div>
-        </div>
-      </div> */}
     </div>
-
   );
 };
