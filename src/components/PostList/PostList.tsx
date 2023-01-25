62c1fa0c34045fa385786aecd38f72070a13332e
maasiimka <mrozisalovec@gmail.com>
2023-01-25 17:44:44 +0200

    Solution

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Post } from '../../types/Post';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+type Props = {
+  posts: Post[]
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <ul className="PostList">
+    {posts.map((post) => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+  </ul>
 );
---
+++ src/types/Post.ts
@@ -0,0 +1,11 @@
+import { Comment } from './Comment';
+import { User } from './User';
+
+export interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
+  user: User | undefined,
+  comments: Comment[] | undefined,
+}
