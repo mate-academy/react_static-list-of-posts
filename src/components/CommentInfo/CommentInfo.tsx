2baf0c0fcf9c7c8493221e2bef251127ad532089
bart6man <igor.musson.55@gmail.com>
2023-01-25 22:14:55 +0200

    Task was solved

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Comment } from '../../types/Comments';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+type Props = {
+  comment: Comment,
+};
+export const CommentInfo: React.FC<Props> = ({ comment }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comment.name}</strong>
+      {' by '}
+      <a
+        className="CommentInfo__email"
+        href={`mailto:${comment.email}`}
+      >
+        {comment.email}
+      </a>
+    </div>
+    <div className="CommentInfo__body">
+      {comment.body}
+    </div>
+  </div>
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import './PostInfo.scss';
+import { FullPost } from '../../types/Posts';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+type Props = {
+  post: FullPost
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+      <p>
+        {' Posted by  '}
+        {post.user === null
+          ? null
+          : (
+            <UserInfo user={post.user} />
+          )}
+      </p>
+    </div>
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+    <hr />
+    {post.comments.length > 0
+      ? (<CommentList comments={post.comments} />)
+      : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+  </div>
 );
