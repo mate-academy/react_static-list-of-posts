4955eecbdecd49fb8a5a9c109625db0f902de487
Andrew Borys <borysandrey1988@gmail.com>
2023-01-27 01:33:54 +0200

    add check for user

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -21,11 +21,13 @@ export const PostInfo: React.FC<Props> = ({ post }) => {
       <div className="PostInfo">
         <div className="PostInfo__header">
           <h3 className="PostInfo__title">{title}</h3>
-          <p>
-            {' Posted by '}
+          {user && (
+            <p>
+              {' Posted by '}
 
-            {user && <UserInfo user={user} />}
-          </p>
+              <UserInfo user={user} />
+            </p>
+          )}
         </div>
 
         <p className="PostInfo__body">{body}</p>
