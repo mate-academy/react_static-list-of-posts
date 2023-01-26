2f555a40616223c4b702391413da4bb0b69a8710
Ivan Stadnytskyy <vanst1@icloud.com>
2023-01-26 20:13:19 +0200

    Posts task FIX

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -5,25 +5,33 @@ import { Comments } from '../../types/Comments';
 interface Props {
   comment: Comments
 }
-export const CommentInfo: React.FC<Props> = ({ comment }) => (
-  <div className="CommentInfo">
-    <div className="CommentInfo__title">
-      <strong className="CommentInfo__name">
-        {comment.name}
-      </strong>
+export const CommentInfo: React.FC<Props> = ({ comment }) => {
+  const {
+    name,
+    email,
+    body,
+  } = comment;
 
-      {' by '}
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">
+          {name}
+        </strong>
 
-      <a
-        className="CommentInfo__email"
-        href={`mailto:${comment.email}`}
-      >
-        {comment.email}
-      </a>
+        {' by '}
+
+        <a
+          className="CommentInfo__email"
+          href={`mailto:${email}`}
+        >
+          {email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">
+        {body}
+      </div>
     </div>
-
-    <div className="CommentInfo__body">
-      {comment.body}
-    </div>
-  </div>
-);
+  );
+}
