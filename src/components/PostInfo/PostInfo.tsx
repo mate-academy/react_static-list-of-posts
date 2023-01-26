d9a0f073de9f7a5182c7f83cf241cf9c4121ace7
Tretiak Serhii <tretyak8936@gmail.com>
2023-01-26 17:30:32 +0200

    changed markup

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -22,11 +22,13 @@ export const PostInfo: React.FC<Props> = ({ post }) => {
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{title}</h3>
 
-        <p>
-          {' Posted by  '}
+        {user && (
+          <p>
+            {' Posted by  '}
 
-          {user && <UserInfo user={user} />}
-        </p>
+            <UserInfo user={user} />
+          </p>
+        )}
       </div>
 
       <p className="PostInfo__body">
