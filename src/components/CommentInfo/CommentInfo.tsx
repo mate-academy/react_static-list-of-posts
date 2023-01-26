90bbaabfed92d475d9644cb1e196aa5007681c92
bart6man <igor.musson.55@gmail.com>
2023-01-26 17:09:50 +0200

    Troubleshooting

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -4,20 +4,24 @@ import { Comment } from '../../types/Comments';
 type Props = {
   comment: Comment,
 };
-export const CommentInfo: React.FC<Props> = ({ comment }) => (
-  <div className="CommentInfo">
-    <div className="CommentInfo__title">
-      <strong className="CommentInfo__name">{comment.name}</strong>
-      {' by '}
-      <a
-        className="CommentInfo__email"
-        href={`mailto:${comment.email}`}
-      >
-        {comment.email}
-      </a>
+export const CommentInfo: React.FC<Props> = ({ comment }) => {
+  const { name, email, body } = comment;
+
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{name}</strong>
+        {' by '}
+        <a
+          className="CommentInfo__email"
+          href={`mailto:${email}`}
+        >
+          {email}
+        </a>
+      </div>
+      <div className="CommentInfo__body">
+        {body}
+      </div>
     </div>
-    <div className="CommentInfo__body">
-      {comment.body}
-    </div>
-  </div>
-);
+  );
+};
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -8,27 +8,34 @@ type Props = {
   post: FullPost
 };
 
-export const PostInfo: React.FC<Props> = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
-      <p>
-        {' Posted by  '}
-        {post.user === null
-          ? null
-          : (
-            <UserInfo user={post.user} />
-          )}
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    user,
+    body,
+    comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+        {user && (
+          <p>
+            {' Posted by  '}
+            <UserInfo user={user} />
+          </p>
+        )}
+      </div>
+      <p className="PostInfo__body">
+        {body}
       </p>
+      <hr />
+      {comments.length
+        ? (<CommentList comments={comments} />)
+        : (
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        )}
     </div>
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-    <hr />
-    {post.comments.length > 0
-      ? (<CommentList comments={post.comments} />)
-      : (
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      )}
-  </div>
-);
+  );
+};
