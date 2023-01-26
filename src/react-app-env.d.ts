c4b58e579287067a86dfa873dfff8876840da660
Andrew Borys <borysandrey1988@gmail.com>
2023-01-26 17:04:15 +0200

    solution_1

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,28 @@
 /// <reference types="react-scripts" />
+
+export interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface FullPost extends Post {
+  user: User | null;
+  comments: Comment[];
+}
