9b0ddaad423d478592a09294d8524daa6ac0d23a
Oleksandr-Maly <117357713+Oleksandr-Maly@users.noreply.github.com>
2023-01-27 10:17:06 +0200

    made changes after commets

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -6,11 +6,18 @@ import './UserInfo.scss';
 type Props = {
   user: User;
 };
-export const UserInfo: React.FC<Props> = ({ user }) => (
-  <a
-    className="UserInfo"
-    href={`mailto:${user.email}`}
-  >
-    {user.name}
-  </a>
-);
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <p>
+      {' Posted by  '}
+      <a
+        className="UserInfo"
+        href={`mailto:${email}`}
+      >
+        {name}
+      </a>
+    </p>
+  );
+};
