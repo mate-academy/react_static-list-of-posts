7b6c77799432a27749a33a7fa7433c0ddaf4416e
Margo <margo@Margos-MBP.home>
2023-01-27 17:37:23 +0000

    Updated solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -19,12 +19,16 @@ export const PostInfo: React.FC<Props> = ({ post }) => {
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{title}</h3>
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
 
-        <p>
-          {' Posted by  '}
-          { user && <UserInfo user={user} />}
-        </p>
+        { user && (
+          <p>
+            {' Posted by  '}
+            <UserInfo user={user} />
+          </p>
+        )}
       </div>
 
       <p className="PostInfo__body">
@@ -33,7 +37,7 @@ export const PostInfo: React.FC<Props> = ({ post }) => {
 
       <hr />
 
-      { comments.length > 0
+      { comments.length
         ? <CommentList comments={comments} />
         : <b data-cy="NoCommentsMessage">No comments yet</b>}
     </div>
