b5934736482c2d9476e8d5b6fe33d3a62ab15547
Anastasiia Kapustiak <anastasiakapustiak@gmail.com>
2023-01-27 15:42:15 +0100

    fixed some mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,13 @@ import { Post } from './types/Post';
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
 
-  // if there is no user with a given userId
   return foundUser || null;
 }
 
 function getCommentById(postId: number): Comment[] {
-  const foundComment = commentsFromServer.filter(
+  return commentsFromServer.filter(
     comment => comment.postId === postId,
   );
-
-  return foundComment;
 }
 
 export const posts: Post[] = postsFromServer.map(post => ({
