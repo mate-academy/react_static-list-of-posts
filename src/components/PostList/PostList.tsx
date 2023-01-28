015452d4362482d41ca447788255309c0efa18d9
Olya Zaiets <zayets.olga@gmail.com>
2023-01-28 03:04:22 +0100

    list of posts done

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Posts } from '../../types/Posts';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+type Props = {
+  posts: Posts[],
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+  </div>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Users } from '../../types/Users';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+import './UserInfo.scss';
+
+type Props = {
+  user : Users,
+};
+
+export const UserInfo: React.FC<Props> = ({ user: { name, email } }) => (
+  <p>
+    {' Posted by  '}
+
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  </p>
 );
