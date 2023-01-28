752d21486247f9ffd364c9192c6510f9888a50ef
maasiimka <mrozisalovec@gmail.com>
2023-01-28 14:25:57 +0200

    Fixed mistakes

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PostList.scss';
 import { Post } from '../../types/Post';
 
 import { PostInfo } from '../PostInfo';
--- src/types/Post.ts
+++ src/types/Post.ts
@@ -7,5 +7,5 @@ export interface Post {
   title: string,
   body: string,
   user: User | undefined,
-  comments: Comment[] | undefined,
+  comments: Comment[],
 }
