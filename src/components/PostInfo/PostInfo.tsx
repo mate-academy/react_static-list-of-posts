fa63a6e30033e47443f528a73d5298ca1615a4d9
andriibilous4 <92803546+andriibilous4@users.noreply.github.com>
2023-01-31 23:17:37 +0200

    Update src/components/PostInfo/PostInfo.tsx
    Co-authored-by: Anastasiia Svintsova <99403198+Anastasiia-Svintsova@users.noreply.github.com>

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -12,12 +12,12 @@ export const PostInfo: React.FC<PostInfoProps> = ({ post }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{post.title}</h3>
-      {post.user ? (
+      {post.user && (
         <p>
           {' Posted by  '}
           <UserInfo user={post.user} />
         </p>
-      ) : null}
+      )}
     </div>
 
     <p className="PostInfo__body">
