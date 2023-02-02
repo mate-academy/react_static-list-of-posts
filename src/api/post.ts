0f97bb49a51a43a5ec40dfb6b61f514d451eb53d
Anton Vasyliev <magisterol963@gmail.com>
2023-02-02 16:03:42 +0200

    solut

---
+++ src/api/post.ts
@@ -0,0 +1,124 @@
+/* eslint-disable max-len */
+
+export default [
+  {
+    userId: 1,
+    id: 1,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'qui est esse',
+    body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+    body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
+  },
+  {
+    userId: 2,
+    id: 11,
+    title: 'et ea vero quia laudantium autem',
+    body: 'delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi',
+  },
+  {
+    userId: 2,
+    id: 12,
+    title: 'in quibusdam tempore odit est dolorem',
+    body: 'itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio',
+  },
+  {
+    userId: 2,
+    id: 20,
+    title: 'doloribus ad provident suscipit at',
+    body: 'qui consequuntur ducimus possimus quisquam amet similique\nsuscipit porro ipsam amet\neos veritatis officiis exercitationem vel fugit aut necessitatibus totam\nomnis rerum consequatur expedita quidem cumque explicabo',
+  },
+  {
+    userId: 3,
+    id: 30,
+    title: 'a quo magni similique perferendis',
+    body: 'alias dolor cumque\nimpedit blanditiis non eveniet odio maxime\nblanditiis amet eius quis tempora quia autem rem\na provident perspiciatis quia',
+  },
+  {
+    userId: 4,
+    id: 31,
+    title: 'ullam ut quidem id aut vel consequuntur',
+    body: 'debitis eius sed quibusdam non quis consectetur vitae\nimpedit ut qui consequatur sed aut in\nquidem sit nostrum et maiores adipisci atque\nquaerat voluptatem adipisci repudiandae',
+  },
+  {
+    userId: 4,
+    id: 32,
+    title: 'doloremque illum aliquid sunt',
+    body: 'deserunt eos nobis asperiores et hic\nest debitis repellat molestiae optio\nnihil ratione ut eos beatae quibusdam distinctio maiores\nearum voluptates et aut adipisci ea maiores voluptas maxime',
+  },
+  {
+    userId: 4,
+    id: 33,
+    title: 'qui explicabo molestiae dolorem',
+    body: 'rerum ut et numquam laborum odit est sit\nid qui sint in\nquasi tenetur tempore aperiam et quaerat qui in\nrerum officiis sequi cumque quod',
+  },
+  {
+    userId: 4,
+    id: 34,
+    title: 'magnam ut rerum iure',
+    body: 'ea velit perferendis earum ut voluptatem voluptate itaque iusto\ntotam pariatur in\nnemo voluptatem voluptatem autem magni tempora minima in\nest distinctio qui assumenda accusamus dignissimos officia nesciunt nobis',
+  },
+  {
+    userId: 4,
+    id: 35,
+    title: 'id nihil consequatur molestias animi provident',
+    body: 'nisi error delectus possimus ut eligendi vitae\nplaceat eos harum cupiditate facilis reprehenderit voluptatem beatae\nmodi ducimus quo illum voluptas eligendi\net nobis quia fugit',
+  },
+  {
+    userId: 4,
+    id: 36,
+    title: 'fuga nam accusamus voluptas reiciendis itaque',
+    body: 'ad mollitia et omnis minus architecto odit\nvoluptas doloremque maxime aut non ipsa qui alias veniam\nblanditiis culpa aut quia nihil cumque facere et occaecati\nqui aspernatur quia eaque ut aperiam inventore',
+  },
+  {
+    userId: 5,
+    id: 41,
+    title: 'non est facere',
+    body: 'molestias id nostrum\nexcepturi molestiae dolore omnis repellendus quaerat saepe\nconsectetur iste quaerat tenetur asperiores accusamus ex ut\nnam quidem est ducimus sunt debitis saepe',
+  },
+  {
+    userId: 6,
+    id: 51,
+    title: 'soluta aliquam aperiam consequatur illo quis voluptas',
+    body: 'sunt dolores aut doloribus\ndolore doloribus voluptates tempora et\ndoloremque et quo\ncum asperiores sit consectetur dolorem',
+  },
+  {
+    userId: 7,
+    id: 61,
+    title: 'voluptatem doloribus consectetur est ut ducimus',
+    body: 'ab nemo optio odio\ndelectus tenetur corporis similique nobis repellendus rerum omnis facilis\nvero blanditiis debitis in nesciunt doloribus dicta dolores\nmagnam minus velit',
+  },
+  {
+    userId: 7,
+    id: 62,
+    title: 'beatae enim quia vel',
+    body: 'enim aspernatur illo distinctio quae praesentium\nbeatae alias amet delectus qui voluptate distinctio\nodit sint accusantium autem omnis\nquo molestiae omnis ea eveniet optio',
+  },
+  {
+    userId: 8,
+    id: 71,
+    title: 'et iusto veniam et illum aut fuga',
+    body: 'occaecati a doloribus\niste saepe consectetur placeat eum voluptate dolorem et\nqui quo quia voluptas\nrerum ut id enim velit est perferendis',
+  },
+  {
+    userId: 9,
+    id: 82,
+    title: 'laudantium voluptate suscipit sunt enim enim',
+    body: 'ut libero sit aut totam inventore sunt\nporro sint qui sunt molestiae\nconsequatur cupiditate qui iste ducimus adipisci\ndolor enim assumenda soluta laboriosam amet iste delectus hic',
+  },
+  {
+    userId: 10,
+    id: 91,
+    title: 'aut amet sed',
+    body: 'libero voluptate eveniet aperiam sed\nsunt placeat suscipit molestias\nsimilique fugit nam natus\nexpedita consequatur consequatur dolores quia eos et placeat',
+  },
+];
