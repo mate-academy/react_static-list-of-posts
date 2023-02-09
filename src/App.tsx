b6c2292170a25e109a784779a318cb0cf0a2f3f7
Skuratovskyi Vladyslav <vladskur0369@gmail.com>
2023-02-09 17:41:30 +0200

    Solution v0.4: remake style

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 import './App.scss';
 import { PostList } from './components/PostList';
 
-// import { PostWithComments } from './components/PostInfo/PostInfo';
-
 import postsFromServer from './api/posts';
-// import commentsFromServer from './api/comments';
-// import usersFromServer from './api/users';
 
 export const App: React.FC = () => (
   <section className="App">
