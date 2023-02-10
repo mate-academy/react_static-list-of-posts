a866ed9294b5c52f0c0239b791b05f73974960f3
Alex Vahnovan <sonyaxent@gmail.com>
2023-02-10 22:30:40 +0100

    ul tags added to the list

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -8,9 +8,11 @@ type Props = {
 };
 
 export const CommentList: React.FC<Props> = ({ comments }) => (
-  <div className="CommentList">
+  <ul className="CommentList">
     {comments.map(comment => (
-      <CommentInfo comment={comment} key={comment.id} />
+      <li>
+        <CommentInfo comment={comment} key={comment.id} />
+      </li>
     ))}
-  </div>
+  </ul>
 );
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Post } from '../../types/post';
 import { PostInfo } from '../PostInfo';
 
+import './PostList.scss';
+
 type Props = {
   posts: Post[];
 };
 
 export const PostList: React.FC<Props> = ({ posts }) => (
-  <div className="PostList">
-    {posts.map(post => <PostInfo post={post} key={post.id} />)}
-  </div>
+  <ul className="PostList">
+    <li>
+      {posts.map(post => <PostInfo post={post} key={post.id} />)}
+    </li>
+  </ul>
 );
