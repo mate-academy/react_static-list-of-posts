b36e386e1c3a0cffb25130cd61bb8f6114554672
Anton Chetveryk <chetverykantonma@gmail.com>
2023-02-13 11:11:38 +0200

    done

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+type CommentListProps = {
+  comments: Comment[],
+};
+
+export const CommentList: React.FC<CommentListProps> = ({ comments }) => (
+  <ul className="CommentList">
+    {comments.map(c => {
+      return (
+        <li key={c.id}>
+          <CommentInfo comment={c} />
+        </li>
+      );
+    })}
+  </ul>
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,54 @@
 import React from 'react';
+import { Post } from '../../types/Post';
+import users from '../../api/users';
+import commentsFromServer from '../../api/comments';
+import { User } from '../../types/User';
+import { Comment } from '../../types/Comment';
+import { CommentList } from '../CommentList';
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+type PostInfoProps = {
+  post: Post,
+};
+
+function getUser(userId: number): User | null {
+  const foundUser = users.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+function getComments(postId: number): Comment[] {
+  const comments = commentsFromServer.filter(c => c.postId === postId);
+
+  return comments;
+}
+
+export const PostInfo: React.FC<PostInfoProps> = ({ post }) => {
+  const { title, body } = post;
+  const user = getUser(post.userId);
+  const comments = getComments(post.id);
+
+  return user && (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        <p>
+          {' Posted by  '}
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+
+      {comments.length === 0
+        ? <b data-cy="NoCommentsMessage">No comments yet</b>
+        : <CommentList comments={comments} />}
+    </div>
+  );
+};
