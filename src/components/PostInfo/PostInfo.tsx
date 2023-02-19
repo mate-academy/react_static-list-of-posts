69b3b7fc75a4c74706c22d204be8b6708deff216
Taras Baran <baranprivat22@gmail.com>
2023-02-19 14:58:52 +0200

    finished

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,45 @@
 import React from 'react';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import './PostInfo.scss';
+
+import { Post } from '../../types/Post';
+
+type Props = {
+  post: Post;
+};
+
+const showNoCommnets = () => (
+  <>
+    <hr />
+
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  </>
+);
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <>
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">
+        {post.title}
+      </h3>
+
+      <p>
+        {' Posted by  '}
+
+        {post.user && <UserInfo user={post.user} />}
+      </p>
+    </div>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    {
+      post.comments.length > 0
+        ? <CommentList comments={post.comments} />
+        : showNoCommnets()
+    }
+  </>
 );
