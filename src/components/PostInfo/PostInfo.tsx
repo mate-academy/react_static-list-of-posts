5ddd11a62e83e9bff2d3163906fcdffa8eb60f49
«ifUride» <ifuride@gmail.com>
2023-03-01 11:08:22 +0100

    Solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,48 @@
 import React from 'react';
+import { Post } from '../../types/Post';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+import './PostInfo.scss';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+const showNoCommnets = () => (
+  <>
+    <hr />
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  </>
 );
+
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    body,
+    comments,
+    user,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+        {
+          user
+            ? <UserInfo user={user} />
+            : <span>anonymous</span>
+        }
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      {
+        comments.length > 0
+          ? <CommentList comments={comments} />
+          : showNoCommnets()
+      }
+    </div>
+  );
+};
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { PostInfo } from '../PostInfo';
+import { Post } from '../../types/Post';
+import './PostList.scss';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  posts: Post[];
+};
+
+export const PostList: React.FC<Props> = ({ posts = [] }) => (
+  <ul className="PostList">
+    {posts.map((post) => (
+      <li key={post.id}>
+        <PostInfo post={post} />
+      </li>
+    ))}
+  </ul>
 );
+
+// prepare posts by adding a user and comments to each post (each comment has a postId);
+// split the App into components based on given CSS blocks;
+// styles from App.scss should be moved to separate files, as well. E.g.: PostList.scss;
+// List components should take an array of corresponding objects and render one Info component per object;
+// Info components should take one object and render its data.
