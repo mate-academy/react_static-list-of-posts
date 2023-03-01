dd066ae11b08a5961297b02fb4b3a53703e5718a
Dmytro <mitrohin21020@gmail.com>
2023-03-01 19:50:23 +0200

    React Post

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { Comment } from '../../types/Comments';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
-);
+type Props = {
+  comment: Comment,
+};
+
+export const CommentInfo: React.FC<Props> = ({ comment }) => {
+  const {
+    email,
+    body,
+    name,
+  } = comment;
+
+  return (
+    <li className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">
+          {name}
+        </strong>
+
+        {' by '}
+
+        <a
+          className="CommentInfo__email"
+          href={`mailto:${email}`}
+        >
+          {email}
+        </a>
+      </div>
+      <div className="CommentInfo__body">
+        {body}
+      </div>
+    </li>
+  );
+};
