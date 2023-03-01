497f7f4cd3e160bea760fcf0a112574f875a0d10
«ifUride» <ifuride@gmail.com>
2023-03-01 11:13:26 +0100

    Comments removal

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -16,9 +16,3 @@ export const PostList: React.FC<Props> = ({ posts = [] }) => (
     ))}
   </ul>
 );
-
-// prepare posts by adding a user and comments to each post (each comment has a postId);
-// split the App into components based on given CSS blocks;
-// styles from App.scss should be moved to separate files, as well. E.g.: PostList.scss;
-// List components should take an array of corresponding objects and render one Info component per object;
-// Info components should take one object and render its data.
