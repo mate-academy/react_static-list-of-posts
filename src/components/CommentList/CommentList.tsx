5788b4e02cead3178d236080068a8ca49fddbab5
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2023-03-09 04:01:24 +0200

    react_static-list-of-posts was done

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+import { CommentInfo } from '../CommentInfo';
+
+import { Comment } from '../../types/Comment';
+
+type Props = {
+  comments: Comment[]
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  return (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
---
+++ src/types/Comment.ts
@@ -0,0 +1,7 @@
+export interface Comment {
+  postId: number;
+  id: number;
+  email: string;
+  name: string;
+  body: string;
+}
