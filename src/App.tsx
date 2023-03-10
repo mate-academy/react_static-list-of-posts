f2f6f92c0dcaebc70ad68e117d5f86ff31d4ea25
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-03-10 22:26:14 +0100

    names of functions changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './App.scss';
 
 import postsFromServer from './api/posts';
@@ -12,18 +11,18 @@ import { Comment } from './types/Comment';
 
 import { PostList } from './components/PostList';
 
-function getUser(userId: number): User | null {
+function getUserById(userId: number): User | null {
   return usersFromServer.find(user => user.id === userId) || null;
 }
 
-function getComments(id: number): Comment[] {
+function getCommentsById(id: number): Comment[] {
   return commentsFromServer.filter(comment => comment.postId === id);
 }
 
 export const posts: Post[] = postsFromServer.map(post => ({
   ...post,
-  user: getUser(post.userId),
-  comments: getComments(post.id),
+  user: getUserById(post.userId),
+  comments: getCommentsById(post.id),
 }));
 
 export const App: React.FC = () => (
