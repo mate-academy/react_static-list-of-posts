08a7366339bfc404e400a88921166320a9af78ea
diamat1997 <diamat1997@gmail.com>
2023-03-13 15:29:43 +0200

    commit message

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
-);
+import { User } from '../../types/User';
+import './UserInfo.scss';
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <p>
+      {' Posted by  '}
+
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
+      </a>
+    </p>
+  );
+};
