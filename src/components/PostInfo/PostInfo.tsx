f6d10075aa5601177bf62adad356069d2918a718
gretoq <dovhun3d@gmail.com>
2023-03-13 16:45:21 +0200

    Did static list of posts in react

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,48 @@
 import React from 'react';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import './PostInfo.scss';
+
+import { Post } from '../../types/Post';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = (
+  {
+    post: {
+      title,
+      body,
+      user,
+      comments,
+    },
+  },
+) => (
+  <>
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        {user && (
+          <UserInfo user={user} />
+        )}
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      {comments.length === 0
+        ? (
+          <>
+            <hr />
+
+            <b data-cy="NoCommentsMessage">No comments yet</b>
+          </>
+        )
+        : <CommentList comments={comments} />}
+    </div>
+  </>
 );
