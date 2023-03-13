a088bb8d1720ff5cb992265fed8e296aaa84bfc3
Ruslan Liapin <joy.esenin@gmail.com>
2023-03-14 01:59:48 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import './App.scss';
 import { PostList } from './components/PostList';
 
-// import postsFromServer from './api/posts';
-// import commentsFromServer from './api/comments';
-// import usersFromServer from './api/users';
-
 export const App: React.FC = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
