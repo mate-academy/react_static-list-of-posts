720202ca6a9549e0dd5cadce3bdb135ef1724f5f
Vladyslav Kanaiev <nordex83@gmail.com>
2023-03-14 16:02:48 +0200

    solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Comment } from '../../types/Comments';
+import { CommentInfo } from '../CommentInfo';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = {
+  comments: Comment[]
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  return (
+    <div className="CommentList">
+      {comments
+        .map(comment => <CommentInfo comment={comment} key={comment.id} />)}
+    </div>
+  );
+};
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import { Post } from '../../types/Posts';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+type Props = {
+  post: Post,
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    body,
+    user,
+    comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+        <p>
+          {' Posted by  '}
+          {user && <UserInfo user={user} />}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+
+      {
+        comments.length === 0
+          ? <b data-cy="NoCommentsMessage">No comments yet</b>
+          : <CommentList comments={comments} />
+      }
+    </div>
+  );
+};
