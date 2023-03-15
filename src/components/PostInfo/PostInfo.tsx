8f5c9b146ff3c59909aa6a52718e521da2de87e3
Степан <stepan12@MacBook-Air-Stepan.local>
2023-03-15 11:50:12 +0200

    list-of-posts done

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,51 @@
 import React from 'react';
+import './PostInfo.scss';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import { FullPosts } from '../../types/FullPosts';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  post: FullPosts;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    body,
+    user,
+    comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+        <p>
+          {' Posted by  '}
+
+          {user
+            && <UserInfo user={user} />}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+
+      {comments.length === 0 ? (
+        <b data-cy="NoCommentsMessage">
+          No comments yet
+        </b>
+      ) : (
+        <CommentList
+          comments={comments}
+        />
+      )}
+    </div>
+  );
+};
