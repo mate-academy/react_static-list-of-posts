0ef04ca87448dea2a28ab77227149f72bbfb7eb1
Sasha Kononenko <nurchiksasha@gmail.com>
2023-03-15 11:08:58 +0200

    static list

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import { Comment } from '../../types/Comment';
+import './CommentList.scss';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+import { CommentInfo } from '../CommentInfo';
+
+type Props = {
+  comments: Comment[];
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  return (
+    <div className="CommentList">
+      {comments.map((comment) => (
+        <CommentInfo
+          comment={comment}
+          key={comment.id}
+        />
+      ))}
+    </div>
+  );
+};
