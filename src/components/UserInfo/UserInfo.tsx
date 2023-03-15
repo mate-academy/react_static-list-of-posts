0ef1763c89aae5ef915335e6bb5f012901ce60e8
Yevhenii Leichenko <eugeniy0536@gmail.com>
2023-03-15 17:08:23 +0200

    fixed

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -12,10 +12,8 @@ export const UserInfo: React.FC<Props> = ({ user }) => {
   const { name, email } = user;
 
   return (
-    <>
-      <a className="UserInfo" href={`mailto:${email}`}>
-        {name}
-      </a>
-    </>
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
   );
 };
