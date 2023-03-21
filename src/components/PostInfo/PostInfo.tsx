faf2ed371b09fdd02cf38158e29302cb6ac2b7b7
timoxa2t <alex2t172@gmail.com>
2023-03-21 18:28:08 +0200

    corrections

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -10,37 +10,31 @@ type Props = {
   post: Post,
 };
 
-export const PostInfo: React.FC<Props> = (
-  {
-    post:
-    {
-      title,
-      body,
-      user,
-      comments,
-    },
-  },
-) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{title}</h3>
-      {user && (
-        <p>
-          {' Posted by  '}
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    body,
+    user,
+    comments,
+  } = post;
 
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+        {user && (
           <UserInfo user={user} />
-        </p>
-      )}
+        )}
+      </div>
+
+      <p className="PostInfo__body">{body}</p>
+
+      <hr />
+
+      {comments.length
+        ? <CommentList comments={comments} />
+        : <CommentsEmpty />}
     </div>
 
-    <p className="PostInfo__body">{body}</p>
-
-    <hr />
-
-    {comments.length > 0
-      ? <CommentList comments={comments} />
-      : <CommentsEmpty />
-    }
-  </div>
-
-);
+  );
+};
