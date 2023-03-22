2e21f303ce9043470184290f098b2f156709aa2f
ctk12 <chetankatwa12@gmail.com>
2023-03-22 12:54:07 +0530

    Added React Static List of Posts

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,36 @@ import React from 'react';
 
 import './App.scss';
 
-// import postsFromServer from './api/posts';
-// import commentsFromServer from './api/comments';
-// import usersFromServer from './api/users';
+import postsFromServer from './api/posts';
+import commentsFromServer from './api/comments';
+import usersFromServer from './api/users';
+
+import { User } from './types/User';
+import { Comment } from './types/Comments';
+import { Post } from './types/Post';
+import { PostList } from './components/PostList';
+
+const getUser = (userId: number): User | null => {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+};
+
+const getComments = (id: number): Comment[] => {
+  return commentsFromServer.filter(comment => comment.postId === id);
+};
+
+export const posts: Post[] = postsFromServer.map(post => ({
+  ...post,
+  user: getUser(post.userId),
+  comments: getComments(post.id),
+}));
 
 export const App: React.FC = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
+
+    <PostList posts={posts} />
 
     <div className="PostList">
       <div className="PostInfo">
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,48 @@
 import React from 'react';
+import { Post } from '../../types/Post';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+type Props = {
+  posts: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ posts }) => {
+  const {
+    title,
+    body,
+    user,
+    comments,
+  } = posts;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        <p>
+          {' Posted by  '}
+
+          {user && (
+            <UserInfo user={user} />
+          )}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+
+      {
+        comments.length === 0
+          ? <b data-cy="NoCommentsMessage">No comments yet</b>
+          : <CommentList comments={comments} />
+      }
+    </div>
+  );
+};
