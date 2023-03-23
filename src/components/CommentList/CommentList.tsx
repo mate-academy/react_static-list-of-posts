0bfe79df05e443649a2549f10cef65c06f30aa57
Anastasia Radchenko <radchenckooo@gmail.com>
2023-03-23 12:59:02 +0200

    commit

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import { Posts } from '../../types/Posts';
+import { CommentInfo } from '../CommentInfo';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = Pick<Posts, 'comments'>;
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  return (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo comment={comment} key={comment.id} />
+      ))}
+    </div>
+  );
+};
