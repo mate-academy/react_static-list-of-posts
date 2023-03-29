b4ef143cd71cbecd0afd5db43a96048428fa67d8
Anna Akhalmosulishvili <akhala33@gmail.com>
2023-03-29 17:18:59 +0300

    solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
+import { Post } from '../../types/Post';
+import { User } from '../../types/User';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+type Props = {
+  post: Post;
+  users: User[];
+  comments: Comment[];
+};
+
+export const PostInfo: React.FC<Props> = ({ post, users, comments }) => {
+  const user: User | undefined
+  = users.find(person => person.id === post.userId);
+  let commentSection: Comment[] | undefined
+ = comments.filter(comment => comment.postId === post.id);
+
+  if (commentSection.length === 0) {
+    commentSection = undefined;
+  }
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        {user ? <UserInfo user={user} /> : undefined}
+
+        <p className="PostInfo__body">
+          {post.body}
+        </p>
+
+        {commentSection
+          ? <CommentList comments={commentSection} />
+          : (
+            <>
+              <hr />
+              <b data-cy="NoCommentsMessage">No comments yet</b>
+            </>
+          )}
+      </div>
+    </div>
+  );
+};
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
+import { Post } from '../../types/Post';
+import { User } from '../../types/User';
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  posts: Post[];
+  users: User[];
+  comments: Comment[];
+};
+
+export const PostList: React.FC<Props> = ({ posts, users, comments }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} users={users} comments={comments} />
+    ))}
+  </div>
 );
