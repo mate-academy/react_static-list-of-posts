20df9bf321015fb6158ffcd6c4bd19b9243641f8
Nazar <nazar378378379379@gmail.com>
2023-04-02 19:59:20 +0300

    develop

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,43 @@
 import React from 'react';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import { Posts } from '../../types/Posts';
+import { UserInfo } from '../UserInfo';
+
+import './PostInfo.scss';
+
+import { CommentList } from '../CommentList';
+
+type Props = {
+  post: Posts;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    user,
+    comments,
+    title,
+    body,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        {user && (
+          <UserInfo user={user} />
+        )}
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+
+      {!comments.length
+        ? <b data-cy="NoCommentsMessage">No comments yet</b>
+        : <CommentList comments={comments} />}
+    </div>
+  );
+};
---
+++ src/types/Posts.ts
@@ -0,0 +1,10 @@
+import { User } from "./User";
+import { Comments } from "./Comments";
+
+export interface Posts {
+  user: User | null;
+  id: number;
+  title: string;
+  body: string;
+  comments: Comments[];
+}
