cbb45adc87c00e50edfaaf0adceb566c40d5d331
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-04-05 11:28:46 +0200

    complete task

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { Comment } from '../../types/CommentInfo';
+import { CommentInfo } from '../CommentInfo';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import './CommentList.scss';
+
+type Props = {
+  comments: Comment[]
+};
+
+const commentListSending = (comments: Comment[]) => {
+  if (comments.length === 0) {
+    return <b data-cy="NoCommentsMessage">No comments yet</b>;
+  }
+
+  return comments.map((comment: Comment) => (
+    comment
+    && (
+      <CommentInfo
+        comment={comment}
+      />
+    )
+  ));
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <div className="CommentList">
+    {commentListSending(comments)}
+  </div>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { User } from '../../types/UserInfo';
+import './UserInfo.scss';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
-);
+type Props = {
+  user: User
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const {
+    id,
+    name,
+    email,
+  } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`} id={`${id}`}>
+      {name}
+    </a>
+  );
+};
