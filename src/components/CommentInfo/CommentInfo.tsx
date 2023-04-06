f913a338c595b6f2998f5f1e466c8e5e4835f656
Daria <hamdsad8@gmail.com>
2023-04-06 20:04:55 +0300

    created a list of posts func

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
+interface CommentInfoProps {
+  comment: Comment;
+}
+
+export const CommentInfo: React.FC<CommentInfoProps> = ({ comment }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comment.name}</strong>
+
+      {' by '}
+
+      <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+        {comment.email}
+      </a>
+    </div>
+
+    <div className="CommentInfo__body">{comment.body}</div>
+  </div>
 );
