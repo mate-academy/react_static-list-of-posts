b2521fdd88b1424ba7acfe5169f1f24a8d3448e3
yana-kotsulym <yanak7170@gmail.com>
2023-04-06 20:06:39 +0200

    Solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import './CommentList.scss';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
+
+type Props = {
+  comments: Comment[];
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <div className="CommentList">
+    <ul>
+      {comments.map(comment => (
+        <li key={comment.id}>
+          <CommentInfo comment={comment} />
+        </li>
+      ))}
+    </ul>
+  </div>
 );
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Post } from '../../types/Post';
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  posts: Post[]
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <div className="PostList">
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>
+          <PostInfo post={post} />
+        </li>
+      ))}
+    </ul>
+  </div>
 );
