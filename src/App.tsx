1cd4e0a5b616ca1769252d75b87b151a85623ac8
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2023-04-07 14:58:31 +0100

    key props are added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 import './App.scss';
@@ -8,6 +9,7 @@ import postsFromServer from './api/posts';
 import commentsFromServer from './api/comments';
 import usersFromServer from './api/users';
 import { PostList } from './components/PostList';
+import { Comment } from './types/Comment';
 
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(
@@ -18,7 +20,7 @@ function getUser(userId: number): User | null {
 }
 
 function getComments(postId: number): Comment[] {
-  return commentsFromServer.filter(comment => comment.postId === postId);
+  return commentsFromServer.filter((comment): Comment => comment.postId === postId);
 }
 
 const posts: Post[] = postsFromServer.map(post => ({
