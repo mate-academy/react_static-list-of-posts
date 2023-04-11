2af82445ad57c03681fbcacc44b573d93bc09d7e
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-04-11 23:26:00 +0300

    rendering list of posts

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../../types/Comment';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+type CommentsProps = {
+  comments: Comment[];
+};
+
+export const CommentList: React.FC<CommentsProps> = ({ comments }) => (
+  <ul className="CommentList">
+    {comments.map((comment: Comment) => (
+      <CommentInfo key={comment.id} comment={comment} />
+    ))}
+  </ul>
 );
