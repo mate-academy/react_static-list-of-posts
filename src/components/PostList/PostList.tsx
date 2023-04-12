08456a334eede1223f43ed4cb04904da1bcf1ff0
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-04-12 22:39:04 +0300

    Solution

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { PreparedPost } from '../../types/PreparedPost';
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  preparedPosts: PreparedPost[];
+};
+
+export const PostList: React.FC<Props> = ({ preparedPosts }) => (
+  <div className="PostList">
+    {preparedPosts.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+  </div>
 );
