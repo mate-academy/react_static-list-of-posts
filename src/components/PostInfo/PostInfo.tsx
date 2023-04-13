936404b34168cfdb268ff1c3a62c82ab17317484
Goto-Light <vadim.tkachenko.1997@gmail.com>
2023-04-13 12:25:19 +0300

    create list of posts

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,49 @@
 import React from 'react';
+import { PreparedPost } from '../../types/PreparedPost';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+import './PostInfo.scss';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+interface Props {
+  post: PreparedPost;
+}
+
+const NoComments = () => (
+  <>
+    <hr />
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  </>
 );
+
+export const PostInfo: React.FC<Props> = ({
+  post,
+}) => {
+  const {
+    comments,
+    title,
+    body,
+    user,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      {post.comments.length > 0
+        ? (<CommentList comments={comments} />)
+        : (<NoComments />)}
+    </div>
+  );
+};
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { PreparedPost } from '../../types/PreparedPost';
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+interface Props {
+  preparedPosts: PreparedPost[];
+}
+
+export const PostList: React.FC<Props> = ({
+  preparedPosts,
+}) => (
+  <div className="PostList">
+    {preparedPosts.map((post) => {
+      return (
+        <PostInfo post={post} key={post.id} />
+      );
+    })}
+  </div>
 );
