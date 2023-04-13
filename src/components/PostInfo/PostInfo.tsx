ee213a01f50335014329bdf53efe83b7b32cccf6
kukla1989 <kukla.1989@ukr.net>
2023-04-13 14:29:19 +0300

    solve react_static-list-of-posts task

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { Post } from '../../types/Post';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+type PostInfoProp = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<PostInfoProp> = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by ' }
+        <UserInfo user={post.user} />
+      </p>
+    </div>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    <hr />
+
+    <CommentList comments={post.comments} />
+  </div>
 );
