3ddab4134e2f53f5536ca6c243c83976b68208d7
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-04-14 13:45:54 +0100

    implemented UserInfo component, added props with interface

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+import { User } from '../../types/User';
+
+interface UserInfoProps {
+  user: User;
+}
+
+export const UserInfo: React.FC<UserInfoProps> = ({ user }) => (
+  <p>
+    {`Posted by ${user.name}`}
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  </p>
+
 );
