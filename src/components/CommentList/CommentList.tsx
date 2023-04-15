e8dfea1beff7cb46a1919d89be41b6a7ff5d8417
Vyacheslav Stolyarchuk <haunt.beta@gmail.com>
2023-04-15 14:19:07 +0300

    static list of posts solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+interface Props {
+  comments: Comment[];
+}
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  const isComments: boolean = comments.length > 0;
+
+  return (
+    <ul className="CommentList">
+      {isComments ? (
+        comments.map(comment => (
+          <CommentInfo comment={comment} key={Math.random()} />
+        ))
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </ul>
+  );
+};
