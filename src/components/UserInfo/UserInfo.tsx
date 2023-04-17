43c355c354ed14edc114f291193f3a0ffd5ff6d2
Rage1502 <karaulchenkoilja@gmail.com>
2023-04-17 20:14:12 +0300

    some fix

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -3,7 +3,7 @@ import { User } from '../../types/User';
 import './UserInfo.scss';
 
 type Props = {
-  user: User;
+  user: User | null;
 };
 export const UserInfo: React.FC<Props> = ({ user }) => (
   <a className="UserInfo" href={`mailto:${user?.email}`}>
