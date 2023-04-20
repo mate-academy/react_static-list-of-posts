6243d8068bc1489b3c2c1606e7ab2cb0b28f484a
Oleksiy Uchaev <corob.dir@gmail.com>
2023-04-21 00:06:38 +0300

    solution_for_list_of_posts

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import { User } from '../../type/User';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
-);
+type Props = {
+  user: User,
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { email, name } = user;
+
+  return (
+    <a
+      className="UserInfo"
+      href={`mailto:${email}`}
+    >
+      {name}
+    </a>
+  );
+};
