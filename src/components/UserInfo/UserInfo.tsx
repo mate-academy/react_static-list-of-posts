d613619c6158edd02207e24d268fcac04738b462
Paweł Smoliński <ski1993@o2.pl>
2023-04-26 16:11:49 +0200

    added destructuring to User component

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -7,9 +7,11 @@ type UserInfoProps = {
 };
 
 export const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
+  const { name, email } = user;
+
   return (
-    <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.name}
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
     </a>
   );
 };
