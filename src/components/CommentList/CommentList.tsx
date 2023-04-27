870b5c4f6c2612b1c903cea82a1d26d93989f4c1
Daniel <henio09@wp.pl>
2023-04-27 23:25:51 +0200

    solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../../types/Comment';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+interface CommentsProps {
+  comments: Comment[]
+}
+
+export const CommentList: React.FC<CommentsProps> = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map((comment) => (
+      <CommentInfo comment={comment} key={comment.id} />
+    ))}
+  </div>
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+import { PostWithUserAndComments } from '../../types/PostWithUserAndComments';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+interface PostProps {
+  post: PostWithUserAndComments
+}
+
+export const PostInfo: React.FC<PostProps> = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <UserInfo user={post.user} />
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    {post.comments.length > 0
+      ? <CommentList comments={post.comments} />
+      : (
+        <>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )}
+  </div>
 );
