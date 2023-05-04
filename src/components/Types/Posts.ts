8787981f11b6a4b58a3db0cad021bce28f647445
aliksandr porakh <sashapohh@gmail.com>
2023-05-04 21:09:58 +0200

    solution v3

---
+++ src/components/Types/Posts.ts
@@ -0,0 +1,11 @@
+import { Comment } from './Comment';
+import { User } from './User';
+
+export interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+  user: User | null;
+  comments: Comment[] | [];
+}
