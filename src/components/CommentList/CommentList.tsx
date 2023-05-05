9f9ef8957ebdaf40b977dea28a36f17ec8e8a55f
PufAnimalsDev <93289768+PufAnimalsDev@users.noreply.github.com>
2023-05-05 08:33:19 +0200

    add solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+import './CommentList.scss';
+
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
+
+interface CommentListProps {
+  comments: Comment[];
+}
+
+export const CommentList: React.FC<CommentListProps> = ({ comments }) => {
+  return (
+    <div className="CommentList">
+      {comments
+        .map(comment => <CommentInfo comment={comment} key={comment.id} />)}
+    </div>
+  );
+};
