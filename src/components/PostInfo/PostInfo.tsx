f0850fa604442ccb0a50d9a9f095912aea796fe6
catgode <105614630+catgode@users.noreply.github.com>
2023-05-07 22:05:28 +0200

    start building PostInfo

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import { Post } from '../../types/Post';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+interface PostInfoProps {
+  postInfo: Post
+}
+
+export const PostInfo: React.FC<PostInfoProps> = ({
+  postInfo: { title, body },
+}) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          Leanne Graham
+        </a>
+      </p>
+    </div>
+
+    <p className="PostInfo__body">
+      {body}
+    </p>
+
+    <hr />
+
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  </div>
 );
