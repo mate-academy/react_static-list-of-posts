7e130c2e4844ae5967c5f824d77c4f3f038afc14
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-05-08 12:21:11 +0200

    initial solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+import './PostInfo.scss';
+import { Post } from '../../types/post';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+interface PostInfoProps {
+  post: Post;
+}
+
+export const PostInfo: React.FC<PostInfoProps> = ({ post }) => {
+  const {
+    title, user, body, comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        <p>
+          {' Posted by  '}
+
+          {user && <UserInfo user={user} />}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      {(comments.length > 0)
+        ? <CommentList comments={comments} />
+        : <b data-cy="NoCommentsMessage">No comments yet</b>}
+    </div>
+  );
+};
