10ba5423b795d724f401f27b17439dae5e2779e3
Iryna Konovalova <iruna191020@gmail.com>
2023-05-12 18:48:49 +0200

    Solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,57 @@
 import React from 'react';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import './PostInfo.scss';
+import { Post } from '../../types/Post';
+import { Comment } from '../../types/Comment';
+import { User } from '../../types/User';
+
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  post: Post & {
+    user: User;
+    comments: Comment[]
+  },
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    user,
+    comments,
+    title,
+    body,
+  } = post;
+
+  return (
+    <>
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        {user && <UserInfo user={user} />}
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+
+      {(comments.length > 0)
+        ? (
+          <div className="CommentList">
+            <CommentList
+              comments={comments}
+            />
+          </div>
+        )
+        : (
+          <b data-cy="NoCommentsMessage">
+            No comments yet
+          </b>
+        )}
+    </>
+  );
+};
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,44 @@
 import React from 'react';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+import { Post } from '../../types/Post';
+import { User } from '../../types/User';
+import { Comment } from '../../types/Comment';
+
+type Props = {
+  posts: Post[],
+  comments: Comment[],
+  users: User[],
+};
+
+export const PostList: React.FC<Props> = ({
+  posts,
+  users,
+  comments,
+}) => (
+  <div className="PostList">
+    {posts.map(postItem => {
+      const user = users.find(
+        person => person.id === postItem.userId,
+      );
+
+      const postComments = comments.filter(item => postItem.id === item.postId);
+
+      if (!user) {
+        return null;
+      }
+
+      const postWithComments = {
+        ...postItem,
+        user,
+        comments: postComments,
+      };
+
+      return (
+        <div className="PostInfo" key={postItem.id}>
+          <PostInfo post={postWithComments} />
+        </div>
+      );
+    })}
+  </div>
 );
