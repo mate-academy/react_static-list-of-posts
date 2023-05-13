c23fd272112ff8722b3c1534a812d2e45288cbf6
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2023-05-13 12:49:46 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,40 @@ import React from 'react';
 
 import './App.scss';
 
-// import postsFromServer from './api/posts';
-// import commentsFromServer from './api/comments';
-// import usersFromServer from './api/users';
+import postsFromServer from './api/posts';
+import commentsFromServer from './api/comments';
+import usersFromServer from './api/users';
+
+import { User } from './types/user';
+import { Comment } from './types/comment';
+import { Post } from './types/post';
+
+import { PostList } from './components/PostList';
+
+const getUser = (userId: number): User | null => {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+};
+
+const getComments = (postId: number): Comment[] | null => {
+  const foundComment = commentsFromServer
+    .filter(comment => comment.postId === postId);
+
+  return foundComment || null;
+};
+
+const posts: Post[] = postsFromServer.map(post => ({
+  ...post,
+  user: getUser(post.userId),
+  comment: getComments(post.id),
+}));
 
 export const App: React.FC = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
+
+    <PostList posts={posts} />
 
     <div className="PostList">
       <div className="PostInfo">
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { User } from '../../types/user';
 
-export const UserInfo: React.FC = () => (
-  <>Put the user here</>
+type Props = {
+  user: User | null,
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <>
+    {user && (
+      <a className="UserInfo" href={`mailto:${user.email}`}>
+        {user.name}
+      </a>
+    )}
+  </>
 );
