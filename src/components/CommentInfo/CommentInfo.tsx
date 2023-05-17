65e6f6bb05b8d34ec940c35c9621764f972bdac3
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-05-17 18:18:27 +0300

    correct mentor

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -16,9 +16,7 @@ export const CommentInfo: React.FC<Props> = ({
   <div className="CommentInfo">
     <div className="CommentInfo__title">
       <strong className="CommentInfo__name">{name}</strong>
-
-      {' by '}
-
+      <span> by </span>
       <a
         className="CommentInfo__email"
         href={`mailto:${email}`}
