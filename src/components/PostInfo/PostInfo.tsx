51eb5f203413accf1e5ab3bd4d88f258aaabd4ef
Denys Didenko <ddidenko441@gmail.com>
2023-05-24 11:40:36 +0300

    fix code style

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -25,14 +25,13 @@ export const PostInfo: React.FC<Props> = ({ post }) => {
           {title}
         </h3>
 
-        {user
-          && (
-            <p>
-              {' Posted by '}
+        {user && (
+          <p>
+            {' Posted by '}
 
-              <UserInfo user={user} />
-            </p>
-          )}
+            <UserInfo user={user} />
+          </p>
+        )}
       </div>
 
       <p className="PostInfo__body">
