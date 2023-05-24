eac80970cde51c1e34f9dfcec81653cfee920b7d
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2023-05-24 23:20:24 +0200

    task solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -10,9 +10,11 @@ interface CommentListProps {
 export const CommentList: React.FC <CommentListProps> = ({ comments }) => (
   <>
     <div className="CommentList">
-      {comments && comments.map((comment) => (
-        <CommentInfo comment={comment} key={comment.id} />
-      ))}
+      { (!comments || comments.length === 0)
+        ? <b data-cy="NoCommentsMessage">No comments yet</b>
+        : comments?.map((comment) => (
+          <CommentInfo comment={comment} key={comment.id} />
+        ))}
     </div>
   </>
 );
