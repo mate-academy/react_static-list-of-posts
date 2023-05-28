6b999a17aef8c0a85fe63d3aefbb43e983020334
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-05-28 20:49:10 +0300

    Initial commit: tests with comments do not pass, do not know how to fix it

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../../types/Comment';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import './CommentList.scss';
+
+type Props = {
+  comment: Comment;
+};
+
+export const CommentList: React.FC<Props> = ({ comment }) => (
+  <div className="CommentList">
+    <CommentInfo comment={comment} />
+  </div>
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { Post } from '../../types/Post';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      {post.user ? <UserInfo user={post.user} /> : null}
+    </div>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    {post.comment ? (
+      <CommentList comment={post.comment} />
+    )
+      : (
+        <>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )}
+  </div>
 );
