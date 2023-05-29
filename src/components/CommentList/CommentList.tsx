bfb9bb30d45a847f3f9bbea91becae6fb31531fb
Maksym Nemera <nickmitchell@ukr.net>
2023-05-29 15:00:32 +0300

    add task solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import './CommentList.scss';
+
+interface Props {
+  comments: Comment[];
+}
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map((comment) => (
+      <CommentInfo commentInfo={comment} key={comment.id} />
+    ))}
+  </div>
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,50 @@
 import React from 'react';
+import { Post } from '../../types/Post';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import './PostInfo.scss';
+
+interface Props {
+  postInfo: Post;
+}
+
+export const PostInfo: React.FC<Props> = ({ postInfo }) => {
+  const {
+    title,
+    body,
+    user,
+    comment,
+  } = postInfo;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        {user && (
+          <p>
+            {' Posted by  '}
+
+            <UserInfo user={user} />
+          </p>
+        )}
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+
+      {comment.length
+        ? (
+          <CommentList comments={comment} />
+        ) : (
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        )}
+    </div>
+  );
+};
