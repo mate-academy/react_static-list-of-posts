84bd6c4e3e6fe48fda509a931814f9def5f6fcbc
Katya Zaremska <unnarummel@gmail.com>
2023-06-03 09:27:14 +0300

    add task solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import { Post } from '../../types/Post';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+import './PostInfo.scss';
+
+interface PostInfoProps {
+  post: Post;
+}
+
+export const PostInfo: React.FC<PostInfoProps> = ({ post }) => {
+  const {
+    userId,
+    title,
+    body,
+    user,
+    comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        {user && <UserInfo user={user} key={userId} />}
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      {!comments.length
+        ? <b data-cy="NoCommentsMessage">No comments yet</b>
+        : <CommentList comments={comments} />}
+    </div>
+  );
+};
