e8908196d2056e2d7f96e227e40bf9d04dbc8f78
Yulia Shvets <juls.shvets@gmail.com>
2023-06-06 19:21:43 +0300

    fixes after code review

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -4,25 +4,15 @@ import { CommentInfo } from '../CommentInfo';
 import './CommentList.scss';
 
 type Props = {
-  comments: Comments[] | null
+  comments: Comments[];
 };
 
-export const CommentList: React.FC <Props> = ({ comments }) => {
-  if (!comments) {
-    return (
-      <>
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      </>
-    );
-  }
-
-  return (
-    <ul className="CommentList">
-      {comments.map(comment => (
-        <li key={comment.id}>
-          <CommentInfo comment={comment} />
-        </li>
-      ))}
-    </ul>
-  );
-};
+export const CommentList: React.FC <Props> = ({ comments }) => (
+  <ul className="CommentList">
+    {comments.map(comment => (
+      <li key={comment.id}>
+        <CommentInfo comment={comment} />
+      </li>
+    ))}
+  </ul>
+);
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -8,26 +8,35 @@ type Props = {
   post: Post,
 };
 
-export const PostInfo: React.FC <Props> = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
-      {post.user && (
-        <UserInfo user={post.user} />
-      )}
+export const PostInfo: React.FC <Props> = ({ post }) => {
+  const {
+    title,
+    user,
+    body,
+    comments,
+  } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+        {user && (
+          <UserInfo user={user} />
+        )}
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+      {post.comments.length > 0
+        ? (<CommentList comments={comments} />)
+        : (
+          <b data-cy="NoCommentsMessage">
+            No comments yet
+          </b>
+        )}
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-
-    <hr />
-    {post.comments.length > 0
-      ? (<CommentList comments={post.comments} />)
-      : (
-        <>
-          <b data-cy="NoCommentsMessage">No comments yet</b>
-        </>
-      )}
-  </div>
-);
+  );
+};
