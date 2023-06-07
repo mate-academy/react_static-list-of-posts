d2a772df1eea5333883282cbc5cb1d71038fc3e7
Anna Peretiatko <tobossanna@gmail.com>
2023-06-08 01:15:37 +0200

    React static list of posts solution

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import './PostList.scss';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+import { Post } from '../../types/Post';
+import { PostInfo } from '../PostInfo';
+
+type Props = {
+  posts: Post[];
+};
+
+export const PostList: React.FC<Props> = ({ posts = [] }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo
+        key={post.id}
+        title={post.title}
+        body={post.body}
+        comments={post.comments}
+        user={post.user}
+        id={post.userId}
+      />
+    ))}
+  </div>
 );
