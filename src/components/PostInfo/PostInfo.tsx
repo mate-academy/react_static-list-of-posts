b0f96dbf7c863bbede44a656fa92224ef5848652
Ivan Khoroshylov <host513@gmail.com>
2023-06-07 19:59:11 +0300

    add solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
-);
+import './PostInfo.scss';
+import { PostPrepared } from '../../types/postsPrepared';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+interface Props {
+  post: PostPrepared,
+}
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  if (!post.user) {
+    throw new Error('The author of the post was not found');
+  }
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo key={post.userId} user={(post.user)} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {post.body}
+      </p>
+
+      <hr />
+
+      {(post.comments && post.comments?.length > 0)
+        ? <CommentList comments={post.comments} />
+        : <b data-cy="NoCommentsMessage">No comments yet</b>}
+
+    </div>
+
+  );
+};
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { PostInfo } from '../PostInfo';
+import { PostPrepared } from '../../types/postsPrepared';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+interface Props{
+  posts: PostPrepared[];
+}
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+  </div>
 );
