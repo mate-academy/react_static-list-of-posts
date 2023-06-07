340f9c8603fde07238aedc3696c267995cce2b2c
Vitalii Drabynko <vpdrabynko@gmail.com>
2023-06-07 12:48:54 +0300

    solution

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { Comments } from '../../types/Comments';
 
-export const CommentInfo: React.FC = () => (
-  <>Put the comment here</>
-);
+type Props = {
+  comments: Comments;
+};
+
+export const CommentInfo: React.FC<Props> = ({ comments }) => {
+  const {
+    name,
+    email,
+    body,
+  } = comments;
+
+  return (
+    <>
+      <div className="CommentInfo">
+        <div className="CommentInfo__title">
+          <strong className="CommentInfo__name">{name}</strong>
+          <span>{' by '}</span>
+          <a
+            className="CommentInfo__email"
+            href={`mailto:${email}`}
+          >
+            Telly_Lynch@karl.co.uk
+          </a>
+        </div>
+
+        <div className="CommentInfo__body">
+          {body}
+        </div>
+      </div>
+    </>
+  );
+};
