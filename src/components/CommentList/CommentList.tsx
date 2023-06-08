14600c52028ba8fb15ea74173ce27d6dfa231539
holytimy4 <ez.lvl.faceit@gmail.com>
2023-06-08 16:44:18 +0300

    added task solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../types/comment';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+import './CommentList.scss';
+
+interface Props {
+  comments: Comment[],
+}
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => (
+      <CommentInfo key={comment.id} comment={comment} />
+    ))}
+  </div>
 );
