65c85711e1df1c89176dcb2fe240a42c9fa680fa
Vania Yarovyi <yarovyi@gmail.com>
2023-06-08 14:32:33 +0300

    used destructuring assignment in PastInfo and CommentInfo

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -8,26 +8,35 @@ interface Props {
   post: FullPost;
 }
 
-export const PostInfo: React.FC<Props> = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const {
+    title,
+    user,
+    body,
+    comments,
+  } = post;
 
-      <p>
-        {' Posted by  '}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
 
-        <UserInfo user={post.user} />
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
       </p>
+
+      <hr />
+
+      {comments.length === 0
+        ? <b data-cy="NoCommentsMessage">No comments yet</b>
+        : <CommentList comments={comments} />}
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-
-    <hr />
-
-    {post.comments.length === 0
-      ? <b data-cy="NoCommentsMessage">No comments yet</b>
-      : <CommentList comments={post.comments} />}
-  </div>
-);
+  );
+};
