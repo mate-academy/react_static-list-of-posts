944d59f2f03b1d33895e68d3f0c190a865d531b8
Alla Gutsul <allagutsul1990@gmail.com>
2023-06-10 12:02:33 +0300

    add list of posts

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import './PostInfo.scss';
+import { Post } from '../../types/Post';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+type Props = {
+  post: Post
+};
+
+export const PostInfo: React.FC<Props> = ({
+  post: {
+    title,
+    body,
+    user,
+    comments,
+  },
+}) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">
+        {title}
+      </h3>
+
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </div>
+
+    <p className="PostInfo__body">
+      {body}
+    </p>
+
+    {(comments && comments.length > 0) ? (
+      <CommentList comments={comments} />
+    ) : ((
+      <>
+        <hr />
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      </>
+    ))}
+  </div>
 );
