d4e948c7454f1eca41c0e5a4dc19f914f373bf9f
Oleksii Lysenko <nano77@ukr.net>
2023-06-11 16:36:41 +0300

    fix

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -21,13 +21,7 @@ export const PostInfo: React.FC<Props> = ({ post }) => {
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{title}</h3>
 
-        {user && (
-          <p>
-            {' Posted by  '}
-
-            <UserInfo user={user} />
-          </p>
-        )}
+        {user && <UserInfo user={user} />}
       </div>
 
       <p className="PostInfo__body">{body}</p>
