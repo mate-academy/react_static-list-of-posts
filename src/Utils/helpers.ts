25eb413c85680557aa13fd701f4d1880972e8af3
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-06-12 19:06:51 +0300

    added additional functions

--- src/Utils/helpers.ts
+++ src/Utils/helpers.ts
@@ -9,16 +9,23 @@ interface PreparePostsArgs {
   comments: Comment[];
 }
 
+const getUserById = (users: User[], id: number): User | undefined => (
+  users.find(user => user.id === id)
+);
+
+const getCommentsByPostId = (
+  comments: Comment[],
+  postId: number,
+): Comment[] => (
+  comments.filter(comment => comment.postId === postId)
+);
+
 export const preparePosts = (args: PreparePostsArgs): FullPost[] => {
   const { posts, users, comments } = args;
 
   return posts.map((post) => {
-    const foundUser = users.find((user) => (
-      post.userId === user.id
-    ));
-    const FoundComments = comments.filter(comment => (
-      comment.postId === post.id
-    ));
+    const foundUser = getUserById(users, post.userId);
+    const FoundComments = getCommentsByPostId(comments, post.id);
 
     return {
       ...post,
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -14,8 +14,12 @@ export const UserInfo: React.FC<Props> = ({ user }) => {
   } = user;
 
   return (
-    <a className="UserInfo" href={`mailto:${email}`}>
-      {name}
-    </a>
+    <p>
+      {' Posted by '}
+
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
+      </a>
+    </p>
   );
 };
