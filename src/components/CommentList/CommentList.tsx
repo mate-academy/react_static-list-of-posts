e0c460fc911ea8b0aeeb0a386795207319f0195b
Vladyslav Fedenko <vadondrummer@gmail.com>
2023-06-16 00:29:00 +0300

    Solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  comments: Comment[] | string
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => (
+  <>
+    {typeof comments === 'string' ? (
+      <>
+        <hr />
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      </>
+    ) : (
+      <div className="CommentList">
+        {comments.map(comment => (
+          <CommentInfo comment={comment} key={comment.id} />
+        ))}
+      </div>
+    )}
+  </>
 );
