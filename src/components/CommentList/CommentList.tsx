ce9176b93c07e194df4cc64a8f90e8b436726ec4
Hishchak Tetiana <tanaiosered@ukr.net>
2023-06-16 21:54:42 +0300

    staticListPosts

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../../type/Commet';
+import './CommentList.scss';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = {
+  comments: Comment[];
+};
+
+export const CommentList: React.FC<Props> = ({ comments }) => {
+  if (comments.length === 0) {
+    return (<b data-cy="NoCommentsMessage">No comments yet</b>);
+  }
+
+  return (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo comment={comment} />
+      ))}
+    </div>
+  );
+};
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Post } from '../../type/Post';
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  posts: Post[];
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo
+        post={post}
+        key={post.id}
+      />
+    ))}
+  </div>
 );
