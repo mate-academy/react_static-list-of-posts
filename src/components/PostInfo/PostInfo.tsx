f7245cd28185db06096480c7c78fc5c9adf7ea28
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-06-25 19:18:30 +0300

    task done

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,44 @@
 import React from 'react';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+import commentsFromServer from '../../api/comments';
+
+import './PostInfo.scss';
+import { Post } from '../../types/Post';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList';
+
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <>
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">
+        {post.title}
+      </h3>
+
+      {post.user !== null && (
+        <UserInfo user={post.user} />
+      )}
+    </div>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    {!commentsFromServer.some(comment => comment.postId === post.id)
+      ? (
+        <>
+          <hr />
+
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )
+      : (
+        <CommentList comments={commentsFromServer
+          .filter(comment => comment.postId === post.id)}
+        />
+      )}
+  </>
 );
