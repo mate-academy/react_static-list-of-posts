9ed4ee96362d5b2b801085a830d36a9b0d016b98
unknown <vorobetst777@gmail.com>
2023-07-02 18:00:14 +0300

    Ready

---
+++ src/utils/posts.ts
@@ -0,0 +1,28 @@
+import { User } from '../types/User';
+import { Comment } from '../types/Comment';
+import { Post } from '../types/Post';
+
+import postsFromServer from '../api/posts';
+import commentsFromServer from '../api/comments';
+import usersFromServer from '../api/users';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+function getComments(postId: number): Comment[] {
+  const comments = commentsFromServer
+    .filter(comment => comment.postId === postId);
+
+  return comments;
+}
+
+export const posts:Post[] = postsFromServer.map(post => (
+  {
+    ...post,
+    user: getUser(post.userId),
+    comments: getComments(post.id),
+  }
+));
