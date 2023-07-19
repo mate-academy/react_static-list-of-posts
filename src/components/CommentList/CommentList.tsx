cc684f0985041b70478400aecc913f9807c0b41f
Oleksandr Marchenko <sasha@mate.academy>
2023-07-19 17:41:57 +0300

    Solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo';
+import { Comment } from '../../types/Comment';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
+type CommentListItem = {
+  comments: Comment[];
+};
+
+export const CommentList: React.FC<CommentListItem> = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => (
+      <CommentInfo comment={comment} key={comment.id} />
+    ))}
+  </div>
 );
