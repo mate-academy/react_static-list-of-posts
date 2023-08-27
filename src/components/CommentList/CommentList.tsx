020f496d259b6624f28d0a108cbd7fd4749f4142
arturwisnia <wisnia3008@interia.pl>
2023-08-27 19:37:04 +0200

    add task solution

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Comment } from '../../types/api.types';
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
 
-export const CommentList: React.FC = () => (
-  <>Put the list here</>
-);
+type CommentListProp = {
+  comments: Comment[] | []
+};
+
+export const CommentList: React.FC<CommentListProp> = ({ comments }) => {
+  return (
+    <div className="CommentList">
+      {comments.map(comment => <CommentInfo comment={comment} />)}
+    </div>
+  );
+};
