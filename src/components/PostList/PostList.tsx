9061b2730204b322cf3e4e592e515ca649651252
KarolinaWiktoria <130801871+KarolinaWiktoria@users.noreply.github.com>
2023-09-14 22:52:34 +0200

    Update PostList.tsx

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,12 +1,12 @@
+import React from 'react';
 import { PostInfo } from '../PostInfo';
-
 import { Post } from '../../types/Post';
 
-type Props = {
-  posts: Post[]
-};
+interface Props {
+  posts: Post[];
+}
 
-export const PostList = ({ posts }: Props) => (
+export const PostList: React.FC<Props> = ({ posts }) => (
   <div className="PostList">
     {posts.map(post => <PostInfo key={post.id} post={post} />)}
   </div>
