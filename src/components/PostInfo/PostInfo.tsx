1972cc55c402293b158aa218c3bde02f358c1437
KarolinaWiktoria <130801871+KarolinaWiktoria@users.noreply.github.com>
2023-09-14 22:58:20 +0200

    Update PostInfo.tsx

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -13,17 +13,14 @@ export const PostInfo = ({ post }: Props) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{post.title}</h3>
-
       <p>
         {'Posted by  '}
         {post.user && <UserInfo user={post.user} />}
       </p>
     </div>
-
     <p className="PostInfo__body">
       {post.body}
     </p>
-
     <hr />
 
     {post.comments.length === 0 ? (
