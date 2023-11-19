9b50446e3a0c9f88f228baa5169cfd322a12d044
Natalia Netzel <140259327+netzelnatalia@users.noreply.github.com>
2023-11-19 23:11:58 +0100

    Update CommentInfo.tsx

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,31 +1,27 @@
 import React from 'react';
-import { Comment } from '../../types/Comment';
+import { Comment } from '../../types/api.types';
 
-type CommentProp = {
-  comment: Comment,
+type CommentInfoProp = {
+  comment: Comment
 };
 
-export const CommentInfo: React.FC<CommentProp> = ({ comment }) => {
-  return (
+export const CommentInfo: React.FC<CommentInfoProp> = ({ comment }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comment.name}</strong>
 
-    <div className="CommentInfo">
-      <div className="CommentInfo__title">
-        <strong className="CommentInfo__name">{comment.name}</strong>
+      {' by '}
 
-        {' by '}
-
-        <a
-          className="CommentInfo__email"
-          href={`mailto:${comment.email}`}
-        >
-          {comment.email}
-        </a>
-      </div>
-
-      <div className="CommentInfo__body">
-        {comment.body}
-      </div>
+      <a
+        className="CommentInfo__email"
+        href={`mailto:${comment.email}`}
+      >
+        {comment.email}
+      </a>
     </div>
 
-  );
-};
+    <div className="CommentInfo__body">
+      {comment.body}
+    </div>
+  </div>
+);
