9b43446f4d51ad8507290da96cb54c429228d4dd
Marcin Szymanski <mszymanski1709@outlook.com>
2023-12-04 14:17:29 +0100

    task

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Post } from '../../types/Post';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
 
-export const PostInfo: React.FC = () => (
-  <>Put the post here</>
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => (
+  <>
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        {post.user && <UserInfo key={post.id} user={post.user} />}
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+      {post.comments.length === 0
+        ? (<b data-cy="NoCommentsMessage">No comments yet</b>)
+        : (<CommentList key={post.id} comments={post.comments} />)}
+    </div>
+  </>
 );
