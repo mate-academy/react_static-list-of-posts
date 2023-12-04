29b8801a790757ecbfab1474f5c2cbaff5dfee0b
Marcin Szymanski <mszymanski1709@outlook.com>
2023-12-04 16:50:34 +0100

    fixed PostInfo

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -13,7 +13,7 @@ export const PostInfo: React.FC<Props> = ({ post }) => (
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{post.title}</h3>
 
-        {post.user && <UserInfo key={post.id} user={post.user} />}
+        {post.user && <UserInfo user={post.user} />}
       </div>
 
       <p className="PostInfo__body">{post.body}</p>
