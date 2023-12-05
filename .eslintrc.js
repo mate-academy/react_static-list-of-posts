1c0ce240e6df66924f4a5cb4bc6771f17a54f303
aurelia <aurelia.gorzna.dev@gmail.com>
2023-12-05 20:07:51 +0100

    tests passing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,5 @@
-const path = require('path');
-
 module.exports = {
   extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
-  settings: {
-    'import/resolver': {
-      webpack: {
-        config: {
-          resolve: {
-            alias: {
-              '~': path.join(__dirname, 'src/app'),
-            },
-            extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
-          },
-        },
-      },
-    },
-  },
   rules: {
     'max-len': ['error', {
       ignoreTemplateLiterals: true,
