546aa43dcbd8c3a07f141f970c742408b7240193
Wiktoria Bielec <wiktoriabielec3@gmail.com>
2023-12-05 17:25:19 +0100

    Requested changes 2

--- src/api/data.ts
+++ src/api/data.ts
@@ -6,17 +6,17 @@ import { Post, Comment, User } from '../types';
 
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
-  
+
   return foundUser || null;
 }
-  
+
 function getCommentsByPostId(id: number): Comment[] | null {
   const foundComment = commentsFromServer
     .filter(comment => comment.postId === id);
-  
+
   return foundComment || null;
 }
-  
+
 export const posts: Post[] = postsFromServer.map(post => ({
   user: getUser(post.userId),
   comments: getCommentsByPostId(post.id),
