c81eaf82b55dc68938e29149053d116c0175f436
Dominika Karmel <dkarmel@op.pl>
2023-12-19 13:33:22 +0100

    fixes as menthors comment said

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -6,23 +6,21 @@ type Props = {
   comment: Comment;
 };
 
-export const CommentInfo: React.FC<Props> = ({ comment }) => (
+export const CommentInfo:
+React.FC<Props> = ({ comment: { name, email, body } }) => (
   <div className="CommentInfo">
     <div className="CommentInfo__title">
-      <strong className="CommentInfo__name">{comment.name}</strong>
+      <strong className="CommentInfo__name">{name}</strong>
 
       {' by '}
 
-      <a
-        className="CommentInfo__email"
-        href={`mailto:${comment.email}`}
-      >
-        {comment.email}
+      <a className="CommentInfo__email" href={`mailto:${email}`}>
+        {email}
       </a>
     </div>
 
     <div className="CommentInfo__body">
-      {comment.body}
+      {body}
     </div>
   </div>
 );
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -8,24 +8,28 @@ type Props = {
   post: Post;
 };
 
-export const PostInfo: React.FC<Props> = ({ post }) => (
+export const PostInfo:
+React.FC<Props> = ({
+  post: {
+    title, user, body, comments,
+  },
+}) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+      <h3 className="PostInfo__title">{title}</h3>
 
       <p>
         {' Posted by  '}
-        {post.user && <UserInfo user={post.user} />}
-
+        {user && <UserInfo user={user} />}
       </p>
     </div>
 
     <p className="PostInfo__body">
-      {post.body}
+      {body}
     </p>
 
     <hr />
 
-    <CommentList comments={post.comments} />
+    <CommentList comments={comments} />
   </div>
 );
