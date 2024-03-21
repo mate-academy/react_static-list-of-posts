7e05ab71565be52169beab89be842f475639457c
Rafał Górski <158020641+itsXenthis@users.noreply.github.com>
2024-03-21 11:45:38 +0100

    Update UserInfo.tsx

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,3 +1,21 @@
 import React from 'react';
+import { User } from '../User';
 
-export const UserInfo: React.FC = () => <>Put the user here</>;
+import './UserInfo.scss';
+
+interface Props {
+  user: User;
+}
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <p>
+      {' Posted by  '}
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
+      </a>
+    </p>
+  );
+};
