28dc4f2673c333f7578553f5e1f3a95b591e108b
Rafał Górski <158020641+itsXenthis@users.noreply.github.com>
2024-03-21 11:43:47 +0100

    Update PostInfo.tsx

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,35 @@
 import React from 'react';
+import { Post } from '../Post';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
 
-export const PostInfo: React.FC = () => <>Put the post here</>;
+import './PostInfo.scss';
+
+interface Props {
+  post: Post;
+}
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  const { title, body, user, comments } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        {user && <UserInfo user={user} />}
+      </div>
+
+      <p className="PostInfo__body">{body}</p>
+
+      <hr />
+
+      {comments.length ? (
+        <CommentList comments={comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
+
