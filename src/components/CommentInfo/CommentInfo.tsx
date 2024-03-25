b8697e6fc12d8ff3f7b7a57657ebc627c3215b5c
Bartlomiej Karbownik <bartlomiej.karbownik56@gmail.com>
2024-03-25 14:08:44 +0100

    Task resolved

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -1,3 +1,18 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
+import './CommentInfo.scss';
 
-export const CommentInfo: React.FC = () => <>Put the comment here</>;
+export const CommentInfo: React.FC<{ comment: Comment }> = ({ comment }) => {
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{comment.name}</strong>
+        {' by '}
+        <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+          {comment.email}
+        </a>
+      </div>
+      <div className="CommentInfo__body">{comment.body}</div>
+    </div>
+  );
+};
