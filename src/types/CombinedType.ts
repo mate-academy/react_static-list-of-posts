24ef3b40ea310d5f877d7b595bab1422bb9faf69
Wojciech <weswojciech@gmail.com>
2024-03-25 16:23:52 +0100

    fix CombinedType to be optional instead of '| null

--- src/types/CombinedType.ts
+++ src/types/CombinedType.ts
@@ -3,6 +3,6 @@ import { Post } from './Post';
 import { User } from './User';
 
 export type CombinedType = Post & {
-  user: User | undefined;
-  comments: Comment[] | [];
+  user?: User;
+  comments?: Comment[];
 };
