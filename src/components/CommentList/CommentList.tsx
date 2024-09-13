1eb6f6de0537f72e322fe4a777ec87bf6dcf1fac
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-13 13:07:01 +0200

    refactor: extract CommentList from PostInfo

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,3 +1,26 @@
 import React from 'react';
+import { Comment } from '../../types/Comment';
 
-export const CommentList: React.FC = () => <>Put the list here</>;
+export const CommentList: React.FC<{ commentListComments: Comment[] }> = ({
+  commentListComments,
+}) => {
+  return (
+    <div className="CommentList">
+      {commentListComments.map(comment => (
+        <div key={comment.id} className="CommentInfo">
+          <div className="CommentInfo__title">
+            <strong className="CommentInfo__name">{comment.name}</strong>
+
+            {' by '}
+
+            <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+              {comment.email}
+            </a>
+          </div>
+
+          <div className="CommentInfo__body">{comment.body}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,47 +1,31 @@
 import React from 'react';
 import { Post } from '../../types/Post';
 import './PostInfo.scss';
+import { CommentList } from '../CommentList';
 
-export const PostInfo: React.FC<{ post: Post }> = ({ post }) => {
+export const PostInfo: React.FC<{ postInfoPost: Post }> = ({
+  postInfoPost: { title, user, body, comments },
+}) => {
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+        <h3 className="PostInfo__title">{title}</h3>
 
         <p>
           {' Posted by  '}
 
           <a className="UserInfo" href="mailto:Sincere@april.biz">
-            {post.user?.name}
+            {user?.name}
           </a>
         </p>
       </div>
 
-      <p className="PostInfo__body">{post.body}</p>
+      <p className="PostInfo__body">{body}</p>
 
       <hr />
 
-      {post.comments ? (
-        <div className="CommentList">
-          {post.comments.map(comment => (
-            <div key={comment.id} className="CommentInfo">
-              <div className="CommentInfo__title">
-                <strong className="CommentInfo__name">{comment.name}</strong>
-
-                {' by '}
-
-                <a
-                  className="CommentInfo__email"
-                  href={`mailto:${comment.email}`}
-                >
-                  {comment.email}
-                </a>
-              </div>
-
-              <div className="CommentInfo__body">{comment.body}</div>
-            </div>
-          ))}
-        </div>
+      {comments ? (
+        <CommentList commentListComments={comments} />
       ) : (
         <b data-cy="NoCommentsMessage">No comments yet</b>
       )}
