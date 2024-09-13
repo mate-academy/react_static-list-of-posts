10acd9ded56b8e2fee49605e9721bf84702f148d
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-13 13:18:48 +0200

    refactor: Comment List to destructure coment properties in map

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -7,19 +7,19 @@ export const CommentList: React.FC<{ commentListComments: Comment[] }> = ({
 }) => {
   return (
     <div className="CommentList">
-      {commentListComments.map(comment => (
-        <div key={comment.id} className="CommentInfo">
+      {commentListComments.map(({ id, name, email, body }) => (
+        <div key={id} className="CommentInfo">
           <div className="CommentInfo__title">
-            <strong className="CommentInfo__name">{comment.name}</strong>
+            <strong className="CommentInfo__name">{name}</strong>
 
             {' by '}
 
-            <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
-              {comment.email}
+            <a className="CommentInfo__email" href={`mailto:${email}`}>
+              {email}
             </a>
           </div>
 
-          <div className="CommentInfo__body">{comment.body}</div>
+          <div className="CommentInfo__body">{body}</div>
         </div>
       ))}
     </div>
