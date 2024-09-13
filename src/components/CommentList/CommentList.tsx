51010b084d51e64586300354559781da29b7ac86
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-13 13:27:37 +0200

    refactor: create CommentInfo component

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import { Comment } from '../../types/Comment';
 import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo';
 
 export const CommentList: React.FC<{ commentListComments: Comment[] }> = ({
   commentListComments,
 }) => {
   return (
     <div className="CommentList">
-      {commentListComments.map(({ id, name, email, body }) => (
-        <div key={id} className="CommentInfo">
-          <div className="CommentInfo__title">
-            <strong className="CommentInfo__name">{name}</strong>
-
-            {' by '}
-
-            <a className="CommentInfo__email" href={`mailto:${email}`}>
-              {email}
-            </a>
-          </div>
-
-          <div className="CommentInfo__body">{body}</div>
-        </div>
+      {commentListComments.map(comment => (
+        <CommentInfo key={comment.id} commentInfoComment={comment} />
       ))}
     </div>
   );
