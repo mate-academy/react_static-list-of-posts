5fe11874e8135b1f4ac1a6141512082191d0564a
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-13 13:09:05 +0200

    refactor: change CommentList styles directory

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Comment } from '../../types/Comment';
+import './CommentList.scss';
 
 export const CommentList: React.FC<{ commentListComments: Comment[] }> = ({
   commentListComments,
