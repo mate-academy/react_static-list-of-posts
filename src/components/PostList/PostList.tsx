0f4ddb68a6c5a12beefe05837d7c8128fb02fcc4
Mykyta-snacj <nikitamal4ik@gmail.com>
2026-08-05 22:55:10 +0200

    add a task solution

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,3 +1,20 @@
+//react
 import React from 'react';
+//types
+import { Post } from '../../types/types';
+//components
+import { PostInfo } from '../PostInfo';
 
-export const PostList: React.FC = () => <>Put the list here</>;
+type Props = {
+  posts: Post[];
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => {
+  return (
+    <>
+      {posts.map(post => (
+        <PostInfo post={post} key={post.id} />
+      ))}
+    </>
+  );
+};
